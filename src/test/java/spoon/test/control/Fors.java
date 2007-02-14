478aff93e745a1d8772e56fecae0e8ca8c8d684f
renaud <renaud@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2007-02-14 21:14:18 +0000

    added more tests for special loops
    git-svn-id: file:///svnroot/spoon/trunk/spoon-core@1731 b1f50995-73fd-43b8-8ca2-fbc405a04e82

---
+++ src/test/java/spoon/test/control/Fors.java
@@ -0,0 +1,22 @@
+package spoon.test.control;
+
+public class Fors {
+
+	void multipleInit() {
+		for (int i = 0, j = 0; i < 2; j++) {
+
+		}
+	}
+
+	void empty1() {
+		int i = 0;
+		for (i = 0;; i++) {
+		}
+	}
+
+	void empty2() {
+		int i = 0;
+		for (;; i++) {
+		}
+	}
+}
