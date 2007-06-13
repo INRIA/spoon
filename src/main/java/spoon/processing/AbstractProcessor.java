5a4bbee0324a88598d13a5e46ce5ef17db777b09
dwayneb <dwayneb@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2007-06-13 17:11:34 +0000

    keep in sync the Factory of ByteCodeOutputProcessor and JavaOuputProcessor
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@1847 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/main/java/spoon/processing/AbstractProcessor.java
+++ src/main/java/spoon/processing/AbstractProcessor.java
@@ -1,16 +1,16 @@
-/* 
+/*
  * Spoon - http://spoon.gforge.inria.fr/
  * Copyright (C) 2006 INRIA Futurs <renaud.pawlak@inria.fr>
- * 
+ *
  * This software is governed by the CeCILL-C License under French law and
- * abiding by the rules of distribution of free software. You can use, modify 
- * and/or redistribute the software under the terms of the CeCILL-C license as 
- * circulated by CEA, CNRS and INRIA at http://www.cecill.info. 
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT 
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * abiding by the rules of distribution of free software. You can use, modify
+ * and/or redistribute the software under the terms of the CeCILL-C license as
+ * circulated by CEA, CNRS and INRIA at http://www.cecill.info.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the CeCILL-C License for more details.
- *  
+ *
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C license and that you accept its terms.
  */
@@ -34,163 +34,163 @@ import spoon.support.util.RtHelper;
  * defining new processors.
  */
 public abstract class AbstractProcessor<E extends CtElement> implements
-		Processor<E> {
+        Processor<E> {
 
-	Factory factory;
+    Factory factory;
 
-	Set<Class<? extends CtElement>> processedElementTypes = new HashSet<Class<? extends CtElement>>();
+    Set<Class<? extends CtElement>> processedElementTypes = new HashSet<Class<? extends CtElement>>();
 
-	/**
-	 * Empty constructor only for all processors (invoked by Spoon).
-	 */
-	@SuppressWarnings("unchecked")
-	public AbstractProcessor() {
-		super();
-		for (Method m : getClass().getMethods()) {
-			if (m.getName().equals("process")
-					&& m.getParameterTypes().length == 1) {
-				Class c = m.getParameterTypes()[0];
-				if (CtElement.class != c) {
-					addProcessedElementType((Class<? extends CtElement>) c);
-				}
-			}
-		}
-		if (processedElementTypes.isEmpty()) {
-			addProcessedElementType(CtElement.class);
-		}
-	}
+    /**
+     * Empty constructor only for all processors (invoked by Spoon).
+     */
+    @SuppressWarnings("unchecked")
+    public AbstractProcessor() {
+        super();
+        for (Method m : getClass().getMethods()) {
+            if (m.getName().equals("process")
+                    && (m.getParameterTypes().length == 1)) {
+                Class c = m.getParameterTypes()[0];
+                if (CtElement.class != c) {
+                    addProcessedElementType(c);
+                }
+            }
+        }
+        if (processedElementTypes.isEmpty()) {
+            addProcessedElementType(CtElement.class);
+        }
+    }
 
-	/**
-	 * Adds a processed element type. This method is typically invoked in
-	 * subclasses' constructors.
-	 */
-	protected void addProcessedElementType(
-			Class<? extends CtElement> elementType) {
-		processedElementTypes.add(elementType);
-	}
+    /**
+     * Adds a processed element type. This method is typically invoked in
+     * subclasses' constructors.
+     */
+    protected void addProcessedElementType(
+            Class<? extends CtElement> elementType) {
+        processedElementTypes.add(elementType);
+    }
 
-	/**
-	 * Clears the processed element types.
-	 */
-	protected void clearProcessedElementType() {
-		processedElementTypes.clear();
-	}
+    /**
+     * Clears the processed element types.
+     */
+    protected void clearProcessedElementType() {
+        processedElementTypes.clear();
+    }
 
-	public Environment getEnvironment() {
-		return getFactory().getEnvironment();
-	}
+    public Environment getEnvironment() {
+        return getFactory().getEnvironment();
+    }
 
-	public Factory getFactory() {
-		return this.factory;
-	}
+    public Factory getFactory() {
+        return this.factory;
+    }
 
-	public Set<Class<? extends CtElement>> getProcessedElementTypes() {
-		return processedElementTypes;
-	}
+    public Set<Class<? extends CtElement>> getProcessedElementTypes() {
+        return processedElementTypes;
+    }
 
-	/**
-	 * Helper method to load the properties of the given processor (uses
-	 * {@link Environment#getProcessorProperties(String)}).
-	 */
-	public static ProcessorProperties loadProperties(Processor p) {
-		ProcessorProperties props = null;
-		try {
-			props = p.getFactory().getEnvironment().getProcessorProperties(
-					p.getClass().getName());
-		} catch (FileNotFoundException e) {
-			p.getFactory().getEnvironment().debugMessage(
-					"property file not found for processor '"
-							+ p.getClass().getName()+"'");
-		} catch (IOException e) {
-			p.getFactory().getEnvironment().report(
-					p,
-					Severity.ERROR,
-					"wrong properties file format for processor '"
-							+ p.getClass().getName()+"'");
-			e.printStackTrace();
-		} catch (Exception e) {
-			p.getFactory().getEnvironment().report(
-					p,
-					Severity.ERROR,
-					"unable to get properties for processor '"
-							+ p.getClass().getName() + "': " + e.getMessage());
-			e.printStackTrace();
-		}
-		return props;
-	}
+    /**
+     * Helper method to load the properties of the given processor (uses
+     * {@link Environment#getProcessorProperties(String)}).
+     */
+    public static ProcessorProperties loadProperties(Processor p) {
+        ProcessorProperties props = null;
+        try {
+            props = p.getFactory().getEnvironment().getProcessorProperties(
+                    p.getClass().getName());
+        } catch (FileNotFoundException e) {
+            p.getFactory().getEnvironment().debugMessage(
+                    "property file not found for processor '"
+                            + p.getClass().getName()+"'");
+        } catch (IOException e) {
+            p.getFactory().getEnvironment().report(
+                    p,
+                    Severity.ERROR,
+                    "wrong properties file format for processor '"
+                            + p.getClass().getName()+"'");
+            e.printStackTrace();
+        } catch (Exception e) {
+            p.getFactory().getEnvironment().report(
+                    p,
+                    Severity.ERROR,
+                    "unable to get properties for processor '"
+                            + p.getClass().getName() + "': " + e.getMessage());
+            e.printStackTrace();
+        }
+        return props;
+    }
 
-	public TraversalStrategy getTraversalStrategy() {
-		return TraversalStrategy.PRE_ORDER;
-	}
+    public TraversalStrategy getTraversalStrategy() {
+        return TraversalStrategy.PRE_ORDER;
+    }
 
-	public void init() {
-		// loadProperties();
-	}
+    public void init() {
+        // loadProperties();
+    }
 
-	public final void initProperties(ProcessorProperties properties) {
-		initProperties(this, properties);
-	}
+    public final void initProperties(ProcessorProperties properties) {
+        initProperties(this, properties);
+    }
 
-	public boolean isToBeProcessed(E candidate) {
-		if (candidate instanceof CtClass) {
-			if (factory.Template().getAll().containsKey(
-					((CtClass) candidate).getQualifiedName())) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public boolean isToBeProcessed(E candidate) {
+        if (candidate instanceof CtClass) {
+            if (factory.Template().getAll().containsKey(
+                    ((CtClass) candidate).getQualifiedName())) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	/**
-	 * Helper method to initialize the properties of a given processor.
-	 */
-	public static void initProperties(Processor p,
-			ProcessorProperties properties) {
-		if (properties != null) {
-			for (Field f : RtHelper.getAllFields(p.getClass())) {
-				if (f.isAnnotationPresent(Property.class)) {
-					Object obj = properties.get(f.getType(), f.getName());
-					if (obj != null) {
-						f.setAccessible(true);
-						try {
-							f.set(p, obj);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					} else {
-						p.getFactory().getEnvironment().report(
-								p,
-								Severity.WARNING,
-								"No value found for property '" + f.getName()
-										+ "' in processor "
-										+ p.getClass().getName());
-					}
-				}
-			}
-		}
-	}
+    /**
+     * Helper method to initialize the properties of a given processor.
+     */
+    public static void initProperties(Processor p,
+            ProcessorProperties properties) {
+        if (properties != null) {
+            for (Field f : RtHelper.getAllFields(p.getClass())) {
+                if (f.isAnnotationPresent(Property.class)) {
+                    Object obj = properties.get(f.getType(), f.getName());
+                    if (obj != null) {
+                        f.setAccessible(true);
+                        try {
+                            f.set(p, obj);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        p.getFactory().getEnvironment().report(
+                                p,
+                                Severity.WARNING,
+                                "No value found for property '" + f.getName()
+                                        + "' in processor "
+                                        + p.getClass().getName());
+                    }
+                }
+            }
+        }
+    }
 
-	/**
-	 * The manual meta-model processing cannot be overriden (use
-	 * {@link AbstractManualProcessor}) to do so.
-	 */
-	public final void process() {
-	}
+    /**
+     * The manual meta-model processing cannot be overriden (use
+     * {@link AbstractManualProcessor}) to do so.
+     */
+    public final void process() {
+    }
 
-	public void processingDone() {
-		// do nothing by default
-	}
+    public void processingDone() {
+        // do nothing by default
+    }
 
-	/**
-	 * Removes a processed element type.
-	 */
-	protected void removeProcessedElementType(
-			Class<? extends CtElement> elementType) {
-		processedElementTypes.remove(elementType);
-	}
+    /**
+     * Removes a processed element type.
+     */
+    protected void removeProcessedElementType(
+            Class<? extends CtElement> elementType) {
+        processedElementTypes.remove(elementType);
+    }
 
-	public final void setFactory(Factory factory) {
-		this.factory = factory;
-	}
+    public void setFactory(Factory factory) {
+        this.factory = factory;
+    }
 
 }
--- src/main/java/spoon/support/ByteCodeOutputProcessor.java
+++ src/main/java/spoon/support/ByteCodeOutputProcessor.java
@@ -27,6 +27,7 @@ import org.eclipse.jdt.internal.compiler.env.ICompilationUnit;
 
 import spoon.processing.AbstractProcessor;
 import spoon.processing.FileGenerator;
+import spoon.reflect.Factory;
 import spoon.reflect.declaration.CtSimpleType;
 import spoon.support.util.ClassFileUtil;
 import spoon.support.util.JDTCompiler;
@@ -35,116 +36,121 @@ import spoon.support.util.JDTCompiler;
  * This class defines a processor that generates class files from the metamodel.
  */
 public class ByteCodeOutputProcessor extends AbstractProcessor<CtSimpleType<?>>
-		implements FileGenerator<CtSimpleType<?>> {
+        implements FileGenerator<CtSimpleType<?>> {
 
-	/**
-	 * Extension for class files (.class).
-	 */
-	public static final String CLASS_EXT = ".class";
+    /**
+     * Extension for class files (.class).
+     */
+    public static final String CLASS_EXT = ".class";
 
-	private File outputDir;
+    private File outputDir;
 
-	private List<ICompilationUnit> units = new ArrayList<ICompilationUnit>();
+    private List<ICompilationUnit> units = new ArrayList<ICompilationUnit>();
 
-	private List<File> printed = new ArrayList<File>();
+    private List<File> printed = new ArrayList<File>();
 
-	private List<String> printedTypes = new ArrayList<String>();
+    private List<String> printedTypes = new ArrayList<String>();
 
-	private JavaOutputProcessor javaPrinter;
+    private JavaOutputProcessor javaPrinter;
 
-	/**
-	 * Creates a new processor for generating Java source files.
-	 *
-	 * @param outputDirectory
-	 *            the root output directory
-	 */
-	public ByteCodeOutputProcessor(JavaOutputProcessor javaPrinter,
-			File outputDirectory) {
-		outputDir = outputDirectory;
-		this.javaPrinter = javaPrinter;
-	}
+    /**
+     * Creates a new processor for generating Java source files.
+     *
+     * @param outputDirectory
+     *            the root output directory
+     */
+    public ByteCodeOutputProcessor(JavaOutputProcessor javaPrinter,
+            File outputDirectory) {
+        outputDir = outputDirectory;
+        this.javaPrinter = javaPrinter;
+    }
 
-	public List<File> getCreatedFiles() {
-		return printed;
-	}
+    public List<File> getCreatedFiles() {
+        return printed;
+    }
 
-	public File getOutputDirectory() {
-		return outputDir;
-	}
+    public File getOutputDirectory() {
+        return outputDir;
+    }
 
-	/**
-	 * Tells if the source is Java 1.4 or lower.
-	 */
-	public long getJavaCompliance() {
-		switch (getFactory().getEnvironment().getComplianceLevel()) {
-		case 1:
-			return ClassFileConstants.JDK1_1;
-		case 2:
-			return ClassFileConstants.JDK1_2;
-		case 3:
-			return ClassFileConstants.JDK1_3;
-		case 4:
-			return ClassFileConstants.JDK1_4;
-		case 5:
-			return ClassFileConstants.JDK1_5;
-		case 6:
-			return ClassFileConstants.JDK1_6;
-		}
-		return ClassFileConstants.JDK1_5;
-	}
+    /**
+     * Tells if the source is Java 1.4 or lower.
+     */
+    public long getJavaCompliance() {
+        switch (getFactory().getEnvironment().getComplianceLevel()) {
+        case 1:
+            return ClassFileConstants.JDK1_1;
+        case 2:
+            return ClassFileConstants.JDK1_2;
+        case 3:
+            return ClassFileConstants.JDK1_3;
+        case 4:
+            return ClassFileConstants.JDK1_4;
+        case 5:
+            return ClassFileConstants.JDK1_5;
+        case 6:
+            return ClassFileConstants.JDK1_6;
+        }
+        return ClassFileConstants.JDK1_5;
+    }
 
-	public void process(CtSimpleType<?> element) {
-		if (!element.isTopLevel()) {
+    public void process(CtSimpleType<?> element) {
+        if (!element.isTopLevel()) {
             return;
         }
-		// Create Java code and create ICompilationUnit
-		javaPrinter.getCreatedFiles().clear();
-		javaPrinter.createJavaFile(element);
+        // Create Java code and create ICompilationUnit
+        javaPrinter.getCreatedFiles().clear();
+        javaPrinter.createJavaFile(element);
 
-		for (File f : javaPrinter.getCreatedFiles()) {
-			try {
-				units.add(JDTCompiler.getUnit(element.getQualifiedName(), f));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+        for (File f : javaPrinter.getCreatedFiles()) {
+            try {
+                units.add(JDTCompiler.getUnit(element.getQualifiedName(), f));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-	}
+    }
 
-	@Override
-	public void processingDone() {
-		try {
-			// Do compilation
-			JDTCompiler compiler = new JDTCompiler();
-			compiler.getCompilerOption().sourceLevel = getJavaCompliance();
-			compiler.getCompilerOption().complianceLevel = getJavaCompliance();
-			compiler.compile(units.toArray(new ICompilationUnit[0]));
+    @Override
+    public void processingDone() {
+        try {
+            // Do compilation
+            JDTCompiler compiler = new JDTCompiler();
+            compiler.getCompilerOption().sourceLevel = getJavaCompliance();
+            compiler.getCompilerOption().complianceLevel = getJavaCompliance();
+            compiler.compile(units.toArray(new ICompilationUnit[0]));
 
-			getOutputDirectory().mkdirs();
+            getOutputDirectory().mkdirs();
 
-			for (ClassFile f : compiler.getClassFiles()) {
-				String fileName = new String(f.fileName()).replace('/',
-						File.separatorChar)
-						+ CLASS_EXT;
+            for (ClassFile f : compiler.getClassFiles()) {
+                String fileName = new String(f.fileName()).replace('/',
+                        File.separatorChar)
+                        + CLASS_EXT;
 //				System.out.println("--- adjusting "+fileName);
-				ClassFileUtil.adjustLineNumbers(f.getBytes(), f.headerOffset
-						+ f.methodCountOffset - 1, javaPrinter
-						.getLineNumberMappings().get(
-								new String(f.fileName()).replace('/', '.')));
-				ClassFileUtil.writeToDisk(true, getOutputDirectory()
-						.getAbsolutePath(), fileName, f.getBytes());
+                ClassFileUtil.adjustLineNumbers(f.getBytes(), f.headerOffset
+                        + f.methodCountOffset - 1, javaPrinter
+                        .getLineNumberMappings().get(
+                                new String(f.fileName()).replace('/', '.')));
+                ClassFileUtil.writeToDisk(true, getOutputDirectory()
+                        .getAbsolutePath(), fileName, f.getBytes());
 
-				printed.add(new File(getOutputDirectory(), fileName));
-				printedTypes.add(new String(f.fileName()).replace('/', '.'));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+                printed.add(new File(getOutputDirectory(), fileName));
+                printedTypes.add(new String(f.fileName()).replace('/', '.'));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public void setOutputDirectory(File directory) {
-		outputDir = directory;
-	}
+    public void setOutputDirectory(File directory) {
+        outputDir = directory;
+    }
 
+    @Override
+    public void setFactory(Factory factory) {
+        super.setFactory(factory);
+        javaPrinter.setFactory(factory);
+    }
 }
