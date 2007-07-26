c9a25ad8c87848d45986cf7f5ccf9fb7ecd3eb8b
renaud <renaud@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2007-07-26 19:33:53 +0000

    added test to ckeck for null types
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@1922 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/test/java/spoon/test/processing/TestProcessor.java
+++ src/test/java/spoon/test/processing/TestProcessor.java
@@ -4,13 +4,21 @@ import spoon.processing.AbstractProcessor;
 import spoon.processing.Severity;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtPackage;
+import spoon.reflect.declaration.CtTypedElement;
 
 public class TestProcessor extends AbstractProcessor<CtElement> {
 
 	public void process(CtElement element) {
-		if((!(element instanceof CtPackage)) && element.getParent()==null) {
-			getEnvironment().report(this, Severity.ERROR, element, "Element's parent is null ("+element+")");
+		if ((!(element instanceof CtPackage)) && element.getParent() == null) {
+			getEnvironment().report(this, Severity.ERROR, element,
+					"Element's parent is null (" + element + ")");
 			throw new RuntimeException("null parent detected");
+		}
+		if (element instanceof CtTypedElement) {
+			if (((CtTypedElement) element).getType() == null) {
+				getEnvironment().report(this, Severity.WARNING, element,
+						"Element's type is null (" + element + ")");
+			}
 		}
 	}
 
---
+++ src/test/java/spoon/test/strings/Main.java
@@ -0,0 +1,11 @@
+package spoon.test.strings;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("a"+"b");
+		
+	}
+	
+}
