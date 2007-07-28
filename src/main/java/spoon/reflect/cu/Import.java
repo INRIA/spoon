0d258e12b7ce363e4fcbe08502cf67467f7354fe
renaud <renaud@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2007-07-28 18:10:15 +0000

    javadoc
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@1927 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/main/java/spoon/reflect/cu/Import.java
+++ src/main/java/spoon/reflect/cu/Import.java
@@ -5,8 +5,13 @@ import spoon.reflect.reference.CtReference;
 /**
  * This interface represents imports in a compilation unit. Imports are not part
  * of the AST and are generated automatically. However, when the auto-import
- * feature of a compilation unit is turned of, the programmer can manually
+ * feature of a compilation unit is turned off, the programmer can manually
  * specify the imports to be done.
+ * 
+ * @see CompilationUnit#isAutoImport()
+ * @see CompilationUnit#setAutoImport(boolean)
+ * @see CompilationUnit#getManualImports()
+ * @see CompilationUnit#setManualImports(java.util.Set)
  */
 public interface Import {
 	
