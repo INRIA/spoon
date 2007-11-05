e9a5ed4acc5fcbd681de91649cb091baf99f4482
dwayneb <dwayneb@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2007-11-05 17:28:54 +0000

    fix generic signature (to allow override)
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@1958 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/main/java/spoon/support/StandardEnvironment.java
+++ src/main/java/spoon/support/StandardEnvironment.java
@@ -1,16 +1,16 @@
-/* 
+/*
  * Spoon - http://spoon.gforge.inria.fr/
  * Copyright (C) 2006 INRIA Futurs <renaud.pawlak@inria.fr>
- * 
+ *
  * This software is governed by the CeCILL-C License under French law and
- * abiding by the rules of distribution of free software. You can use, modify 
- * and/or redistribute the software under the terms of the CeCILL-C license as 
- * circulated by CEA, CNRS and INRIA at http://www.cecill.info. 
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT 
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * abiding by the rules of distribution of free software. You can use, modify
+ * and/or redistribute the software under the terms of the CeCILL-C license as
+ * circulated by CEA, CNRS and INRIA at http://www.cecill.info.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the CeCILL-C License for more details.
- *  
+ *
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C license and that you accept its terms.
  */
@@ -48,305 +48,316 @@ import spoon.support.processing.XmlProcessorProperties;
  */
 public class StandardEnvironment implements Serializable, Environment {
 
-	/**
-	 * The processors' properties files extension (.xml)
-	 */
-	public static final String PROPERTIES_EXT = ".xml";
+    /**
+     * The processors' properties files extension (.xml)
+     */
+    public static final String PROPERTIES_EXT = ".xml";
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private boolean debug = false;
+    private boolean debug = false;
 
-	private FileGenerator<?> defaultFileGenerator;
+    private FileGenerator<? extends CtElement> defaultFileGenerator;
 
-	private int errorCount = 0;
+    private int errorCount = 0;
 
-	private transient Factory factory;
+    private transient Factory factory;
 
-	ProcessingManager manager;
+    ProcessingManager manager;
 
-	private boolean processingStopped = false;
+    private boolean processingStopped = false;
 
-	private boolean verbose = false;
+    private boolean verbose = false;
 
-	private int warningCount = 0;
+    private int warningCount = 0;
 
-	private File xmlRootFolder;
+    private File xmlRootFolder;
 
-	/**
-	 * Creates a new environment with a <code>null</code> default file
-	 * generator.
-	 */
-	public StandardEnvironment() {
-	}
+    /**
+     * Creates a new environment with a <code>null</code> default file
+     * generator.
+     */
+    public StandardEnvironment() {
+    }
 
-	/**
-	 * Creates a new environment.
-	 */
-	public StandardEnvironment(FileGenerator<?> defaultFileGenerator) {
-		this.defaultFileGenerator = defaultFileGenerator;
-	}
+    /**
+     * Creates a new environment.
+     */
+    public StandardEnvironment(FileGenerator<? extends CtElement> defaultFileGenerator) {
+        this.defaultFileGenerator = defaultFileGenerator;
+    }
 
-	public void debugMessage(String message) {
-		if (isDebug())
-			System.out.println(message);
-	}
+    public void debugMessage(String message) {
+        if (isDebug()) {
+            System.out.println(message);
+        }
+    }
 
-	public FileGenerator<?> getDefaultFileGenerator() {
-		return defaultFileGenerator;
-	}
+    public FileGenerator<? extends CtElement> getDefaultFileGenerator() {
+        return defaultFileGenerator;
+    }
 
-	public Factory getFactory() {
-		return factory;
-	}
+    public Factory getFactory() {
+        return factory;
+    }
 
-	public ProcessingManager getManager() {
-		return manager;
-	}
+    public ProcessingManager getManager() {
+        return manager;
+    }
 
-	Map<String, ProcessorProperties> processorProperties = new TreeMap<String, ProcessorProperties>();
+    Map<String, ProcessorProperties> processorProperties = new TreeMap<String, ProcessorProperties>();
 
-	public ProcessorProperties getProcessorProperties(String processorName)
-			throws FileNotFoundException, IOException, SAXException {
-		if (processorProperties.containsKey(processorName))
-			return processorProperties.get(processorName);
+    public ProcessorProperties getProcessorProperties(String processorName)
+            throws FileNotFoundException, IOException, SAXException {
+        if (processorProperties.containsKey(processorName)) {
+            return processorProperties.get(processorName);
+        }
 
-		InputStream in = getPropertyStream(processorName);
-		XmlProcessorProperties prop =null;
-		try {
-			prop = new XmlProcessorProperties(getFactory(),
-					processorName, in);
-		} catch(SAXException e) {
-			throw new RuntimeException(e);
-		}
-		processorProperties.put(processorName, prop);
-		return prop;
-	}
+        InputStream in = getPropertyStream(processorName);
+        XmlProcessorProperties prop =null;
+        try {
+            prop = new XmlProcessorProperties(getFactory(),
+                    processorName, in);
+        } catch(SAXException e) {
+            throw new RuntimeException(e);
+        }
+        processorProperties.put(processorName, prop);
+        return prop;
+    }
 
-	private InputStream getPropertyStream(String processorName)
-			throws FileNotFoundException {
-		for (File child : getXmlRootFolder().listFiles()) {
-			if (child.getName().equals(processorName + PROPERTIES_EXT)) {
-				return new FileInputStream(child);
-			}
-		}
-		throw new FileNotFoundException();
-	}
+    private InputStream getPropertyStream(String processorName)
+            throws FileNotFoundException {
+        for (File child : getXmlRootFolder().listFiles()) {
+            if (child.getName().equals(processorName + PROPERTIES_EXT)) {
+                return new FileInputStream(child);
+            }
+        }
+        throw new FileNotFoundException();
+    }
 
-	/**
-	 * Gets the root folder where the processors' XML configuration files are
-	 * located.
-	 */
-	public File getXmlRootFolder() {
-		if (xmlRootFolder == null) {
-			xmlRootFolder = new File(".");
-		}
-		return xmlRootFolder;
-	}
+    /**
+     * Gets the root folder where the processors' XML configuration files are
+     * located.
+     */
+    public File getXmlRootFolder() {
+        if (xmlRootFolder == null) {
+            xmlRootFolder = new File(".");
+        }
+        return xmlRootFolder;
+    }
 
-	public boolean isDebug() {
-		return debug;
-	}
+    public boolean isDebug() {
+        return debug;
+    }
 
-	/**
-	 * Tells if the processing is stopped, generally because one of the
-	 * processors called {@link #setProcessingStopped(boolean)} after reporting
-	 * an error.
-	 */
-	public boolean isProcessingStopped() {
-		return processingStopped;
-	}
+    /**
+     * Tells if the processing is stopped, generally because one of the
+     * processors called {@link #setProcessingStopped(boolean)} after reporting
+     * an error.
+     */
+    public boolean isProcessingStopped() {
+        return processingStopped;
+    }
 
-	/**
-	 * Returns true if Spoon is in verbose mode.
-	 */
-	public boolean isVerbose() {
-		return verbose;
-	}
+    /**
+     * Returns true if Spoon is in verbose mode.
+     */
+    public boolean isVerbose() {
+        return verbose;
+    }
 
-	private void prefix(StringBuffer buffer, Severity severity) {
-		// Prefix message
-		switch (severity) {
-		case ERROR:
-			buffer.append("error: ");
-			errorCount++;
-			break;
-		case WARNING:
-			buffer.append("warning: ");
-			warningCount++;
-			break;
-		case MESSAGE:
-			break;
-		}
-	}
+    private void prefix(StringBuffer buffer, Severity severity) {
+        // Prefix message
+        switch (severity) {
+        case ERROR:
+            buffer.append("error: ");
+            errorCount++;
+            break;
+        case WARNING:
+            buffer.append("warning: ");
+            warningCount++;
+            break;
+        case MESSAGE:
+            break;
+        }
+    }
 
-	private void print(StringBuffer buffer, Severity severity) {
-		switch (severity) {
-		case ERROR:
-		case WARNING:
-			System.out.println(buffer.toString());
-			break;
-		default:
-			if (isVerbose())
-				System.out.println(buffer.toString());
-		}
-	}
+    private void print(StringBuffer buffer, Severity severity) {
+        switch (severity) {
+        case ERROR:
+        case WARNING:
+            System.out.println(buffer.toString());
+            break;
+        default:
+            if (isVerbose()) {
+                System.out.println(buffer.toString());
+            }
+        }
+    }
 
-	public void report(Processor<?> processor, Severity severity,
-			CtElement element, String message) {
-		StringBuffer buffer = new StringBuffer();
+    public void report(Processor<?> processor, Severity severity,
+            CtElement element, String message) {
+        StringBuffer buffer = new StringBuffer();
 
-		prefix(buffer, severity);
+        prefix(buffer, severity);
 
-		// Adding message
-		buffer.append(message);
+        // Adding message
+        buffer.append(message);
 
-		// Add sourceposition (javac format)
-		CtSimpleType<?> type = (element instanceof CtSimpleType) ? (CtSimpleType<?>) element
-				: element.getParent(CtSimpleType.class);
-		SourcePosition sp = element.getPosition();
+        // Add sourceposition (javac format)
+        CtSimpleType<?> type = (element instanceof CtSimpleType) ? (CtSimpleType<?>) element
+                : element.getParent(CtSimpleType.class);
+        SourcePosition sp = element.getPosition();
 
-		if (sp == null) {
-			buffer.append(" (Unknown Source)");
-		} else {
-			buffer.append(" at " + type.getQualifiedName() + ".");
-			CtExecutable<?> exe = (element instanceof CtExecutable) ? (CtExecutable<?>) element
-					: element.getParent(CtExecutable.class);
-			if (exe != null)
-				buffer.append(exe.getSimpleName());
-			buffer.append("(" + sp.getFile().getName() + ":" + sp.getLine()
-					+ ")");
-		}
+        if (sp == null) {
+            buffer.append(" (Unknown Source)");
+        } else {
+            buffer.append(" at " + type.getQualifiedName() + ".");
+            CtExecutable<?> exe = (element instanceof CtExecutable) ? (CtExecutable<?>) element
+                    : element.getParent(CtExecutable.class);
+            if (exe != null) {
+                buffer.append(exe.getSimpleName());
+            }
+            buffer.append("(" + sp.getFile().getName() + ":" + sp.getLine()
+                    + ")");
+        }
 
-		print(buffer, severity);
-	}
+        print(buffer, severity);
+    }
 
-	public void report(Processor<?> processor, Severity severity, String message) {
-		StringBuffer buffer = new StringBuffer();
+    public void report(Processor<?> processor, Severity severity, String message) {
+        StringBuffer buffer = new StringBuffer();
 
-		prefix(buffer, severity);
-		// Adding message
-		buffer.append(message);
-		print(buffer, severity);
-	}
+        prefix(buffer, severity);
+        // Adding message
+        buffer.append(message);
+        print(buffer, severity);
+    }
 
-	/**
-	 * This method should be called to report the end of the processing.
-	 */
-	public void reportEnd() {
-		if(!isVerbose()) return;
-		System.out.print("end of processing: ");
-		if (warningCount > 0) {
-			System.out.print(warningCount + " warning");
-			if (warningCount > 1)
-				System.out.print("s");
-			if (errorCount > 0)
-				System.out.print(", ");
-		}
-		if (errorCount > 0) {
-			System.out.print(errorCount + " error");
-			if (errorCount > 1)
-				System.out.print("s");
-		}
-		if ((errorCount + warningCount) > 0) {
-			System.out.print("\n");
-		} else {
-			System.out.println("no errors, no warnings");
-		}
-	}
+    /**
+     * This method should be called to report the end of the processing.
+     */
+    public void reportEnd() {
+        if(!isVerbose()) {
+            return;
+        }
+        System.out.print("end of processing: ");
+        if (warningCount > 0) {
+            System.out.print(warningCount + " warning");
+            if (warningCount > 1) {
+                System.out.print("s");
+            }
+            if (errorCount > 0) {
+                System.out.print(", ");
+            }
+        }
+        if (errorCount > 0) {
+            System.out.print(errorCount + " error");
+            if (errorCount > 1) {
+                System.out.print("s");
+            }
+        }
+        if ((errorCount + warningCount) > 0) {
+            System.out.print("\n");
+        } else {
+            System.out.println("no errors, no warnings");
+        }
+    }
 
-	public void reportProgressMessage(String message) {
-		if(!isVerbose()) return;
-		System.out.println(message);
-	}
+    public void reportProgressMessage(String message) {
+        if(!isVerbose()) {
+            return;
+        }
+        System.out.println(message);
+    }
 
-	public void setDebug(boolean debug) {
-		this.debug = debug;
-	}
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
 
-	public void setDefaultFileGenerator(
-			FileGenerator<? extends CtElement> defaultFileGenerator) {
-		this.defaultFileGenerator = defaultFileGenerator;
-		defaultFileGenerator.setFactory(getFactory());
-	}
+    public void setDefaultFileGenerator(
+            FileGenerator<? extends CtElement> defaultFileGenerator) {
+        this.defaultFileGenerator = defaultFileGenerator;
+        defaultFileGenerator.setFactory(getFactory());
+    }
 
-	public void setFactory(Factory factory) {
-		this.factory = factory;
-	}
+    public void setFactory(Factory factory) {
+        this.factory = factory;
+    }
 
-	public void setManager(ProcessingManager manager) {
-		this.manager = manager;
-	}
+    public void setManager(ProcessingManager manager) {
+        this.manager = manager;
+    }
 
-	public void setProcessingStopped(boolean processingStopped) {
-		this.processingStopped = processingStopped;
-	}
+    public void setProcessingStopped(boolean processingStopped) {
+        this.processingStopped = processingStopped;
+    }
 
-	public void setVerbose(boolean verbose) {
-		this.verbose = verbose;
-	}
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
 
-	/**
-	 * Sets the root folder where the processors' XML configuration files are
-	 * located.
-	 */
-	public void setXmlRootFolder(File xmlRootFolder) {
-		this.xmlRootFolder = xmlRootFolder;
-	}
+    /**
+     * Sets the root folder where the processors' XML configuration files are
+     * located.
+     */
+    public void setXmlRootFolder(File xmlRootFolder) {
+        this.xmlRootFolder = xmlRootFolder;
+    }
 
-	int complianceLevel = 5;
+    int complianceLevel = 5;
 
-	public int getComplianceLevel() {
-		return complianceLevel;
-	}
+    public int getComplianceLevel() {
+        return complianceLevel;
+    }
 
-	public void setComplianceLevel(int level) {
-		this.complianceLevel = level;
-	}
+    public void setComplianceLevel(int level) {
+        complianceLevel = level;
+    }
 
-	public void setProcessorProperties(String processorName,
-			ProcessorProperties prop) {
-		processorProperties.put(processorName, prop);
-	}
+    public void setProcessorProperties(String processorName,
+            ProcessorProperties prop) {
+        processorProperties.put(processorName, prop);
+    }
 
-	public void report(Processor<?> processor, Severity severity,
-			CtElement element, String message, ProblemFixer<?>... fix) {
-		// Fix not (yet) used in command-line mode
-		report(processor, severity, element, message);
-	}
+    public void report(Processor<?> processor, Severity severity,
+            CtElement element, String message, ProblemFixer<?>... fix) {
+        // Fix not (yet) used in command-line mode
+        report(processor, severity, element, message);
+    }
 
-	public boolean isUsingSourceCodeFragments() {
-		return useSourceCodeFragments;
-	}
+    public boolean isUsingSourceCodeFragments() {
+        return useSourceCodeFragments;
+    }
 
-	boolean useSourceCodeFragments=false;
-	
-	public void useSourceCodeFragments(boolean b) {
-		useSourceCodeFragments=b;
-	}
+    boolean useSourceCodeFragments=false;
 
-	boolean useTabulations=false;
+    public void useSourceCodeFragments(boolean b) {
+        useSourceCodeFragments=b;
+    }
 
-	public boolean isUsingTabulations() {
-		return useTabulations;
-	}
+    boolean useTabulations=false;
 
-	public void useTabulations(boolean tabulation) {
-		this.useTabulations = tabulation;
-	}
-	
-	int tabulationSize=4;
+    public boolean isUsingTabulations() {
+        return useTabulations;
+    }
 
-	public int getTabulationSize() {
-		return tabulationSize;
-	}
+    public void useTabulations(boolean tabulation) {
+        useTabulations = tabulation;
+    }
 
-	public void setTabulationSize(int tabulationSize) {
-		this.tabulationSize = tabulationSize;
-	}
+    int tabulationSize=4;
 
-	public String getSourcePath() {
-		return ".";
-	}
+    public int getTabulationSize() {
+        return tabulationSize;
+    }
+
+    public void setTabulationSize(int tabulationSize) {
+        this.tabulationSize = tabulationSize;
+    }
+
+    public String getSourcePath() {
+        return ".";
+    }
 
 }
