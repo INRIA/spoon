b3a79c5373b320b04f07354df9b52596afbdc305
renaud <renaud@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2007-11-09 20:07:32 +0000

    more support
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@1965 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/main/java/spoon/support/reflect/eval/VisitorSymbolicEvaluator.java
+++ src/main/java/spoon/support/reflect/eval/VisitorSymbolicEvaluator.java
@@ -1,16 +1,16 @@
-/*
+/* 
  * Spoon - http://spoon.gforge.inria.fr/
  * Copyright (C) 2006 INRIA Futurs <renaud.pawlak@inria.fr>
- *
+ * 
  * This software is governed by the CeCILL-C License under French law and
- * abiding by the rules of distribution of free software. You can use, modify
- * and/or redistribute the software under the terms of the CeCILL-C license as
- * circulated by CEA, CNRS and INRIA at http://www.cecill.info.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * abiding by the rules of distribution of free software. You can use, modify 
+ * and/or redistribute the software under the terms of the CeCILL-C license as 
+ * circulated by CEA, CNRS and INRIA at http://www.cecill.info. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
  * FITNESS FOR A PARTICULAR PURPOSE. See the CeCILL-C License for more details.
- *
+ *  
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C license and that you accept its terms.
  */
@@ -771,6 +771,14 @@ public class VisitorSymbolicEvaluator implements CtVisitor, SymbolicEvaluator {
 	public <T> void visitCtLiteral(CtLiteral<T> literal) {
 		if (literal.getValue() == null) {
 			result = SymbolicInstance.NULL;
+		} else if(literal.getValue().equals(true)) {
+			result = SymbolicInstance.TRUE;
+		} else if(literal.getValue().equals(false)) {
+			result = SymbolicInstance.FALSE;
+		} else if((literal.getValue() instanceof Number)) {
+			if(((Number)literal.getValue()).intValue()==0) {
+				result = SymbolicInstance.ZERO;
+			}
 		} else {
 			result = new SymbolicInstance<T>(this, literal.getType(), false);
 		}
