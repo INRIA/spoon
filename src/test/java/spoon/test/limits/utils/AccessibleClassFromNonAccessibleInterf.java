e9745da758263b13c8f642306313b0c5974285ec
bcornu <bcornu@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2012-11-28 15:10:02 +0000

    adding unit tests
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@2074 b1f50995-73fd-43b8-8ca2-fbc405a04e82

---
+++ src/test/java/spoon/test/limits/utils/AccessibleClassFromNonAccessibleInterf.java
@@ -0,0 +1,7 @@
+package spoon.test.limits.utils;
+
+public class AccessibleClassFromNonAccessibleInterf implements NonAccessibleInterf {
+
+	public void method(NonAccessibleInterf tmp) {
+	}
+}
---
+++ src/test/java/spoon/test/limits/utils/ContainInternalClass.java
@@ -0,0 +1,10 @@
+package spoon.test.limits.utils;
+
+public class ContainInternalClass {
+	
+	public class InternalClass{
+		public class InsideInternalClass{
+			
+		}
+	}
+}
