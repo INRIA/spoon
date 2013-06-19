4de1d920f5388722f204c9590f0521a9195cfdbf
martin <martin@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2013-06-19 08:00:31 +0000

    renamed class Compiler to TreeBuilderCompil
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@2140 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/main/java/spoon/support/builder/TreeBuilderCompiler.java
+++ src/main/java/spoon/support/builder/TreeBuilderCompiler.java
@@ -10,43 +10,52 @@ import spoon.eclipse.jdt.internal.compiler.ast.CompilationUnitDeclaration;
 import spoon.eclipse.jdt.internal.compiler.batch.CompilationUnit;
 import spoon.eclipse.jdt.internal.compiler.env.INameEnvironment;
 
-class Compiler extends spoon.eclipse.jdt.internal.compiler.Compiler {
+class TreeBuilderCompiler extends spoon.eclipse.jdt.internal.compiler.Compiler {
 
-  @SuppressWarnings("deprecation")
-  public Compiler(INameEnvironment environment,
-      IErrorHandlingPolicy policy, Map<?,?> settings,
-      ICompilerRequestor requestor, IProblemFactory problemFactory,
-      PrintWriter out, boolean statementsRecovery) {
-    super(environment, policy, settings, requestor, problemFactory,
-        statementsRecovery);
-  }
-  
-  public CompilationUnitDeclaration[] compileUnits(
-      CompilationUnit[] sourceUnits) {
-    CompilationUnitDeclaration unit = null;
-    int i = 0;
-    // build and record parsed units
-    beginToCompile(sourceUnits);
-    // process all units (some more could be injected in the loop by
-    // the lookup environment)
-    for (; i < this.totalUnits; i++) {
-      unit = unitsToProcess[i];
-      this.parser.getMethodBodies(unit);
+	@SuppressWarnings("deprecation")
+	public TreeBuilderCompiler(INameEnvironment environment,
+			IErrorHandlingPolicy policy, Map<?,?> settings,
+			ICompilerRequestor requestor, IProblemFactory problemFactory,
+			PrintWriter out, boolean statementsRecovery) {
+		super(environment, policy, settings, requestor, problemFactory,
+				statementsRecovery);
+	}
+	
+	public CompilationUnitDeclaration[] compileUnits(
+			CompilationUnit[] sourceUnits) {
+	  
+	  // //////////////////////////////////////////////////////////////////////////
+	  // This code is largely inspired from JDT's CompilationUnitResolver.resolve
+	  
+	  
+		CompilationUnitDeclaration unit = null;
+		int i = 0;
+		// build and record parsed units
+		beginToCompile(sourceUnits);
+		
+		// process all units (some more could be injected in the loop by
+		// the lookup environment)
+		for (; i < this.totalUnits; i++) {
+			unit = unitsToProcess[i];
+			//System.err.println(unit);
+			this.parser.getMethodBodies(unit);
 
-      // fault in fields & methods
-      if (unit.scope != null)
-        unit.scope.faultInTypes();
-      // verify inherited methods
-      if (unit.scope != null)
-        unit.scope
-            .verifyMethods(lookupEnvironment.methodVerifier());
-      // type checking
-      unit.resolve();
-      // flow analysis
-      unit.analyseCode();
+			// fault in fields & methods
+			if (unit.scope != null)
+              unit.scope.faultInTypes();
+			
+			// verify inherited methods
+			if (unit.scope != null)
+				unit.scope.verifyMethods(lookupEnvironment.methodVerifier());
+			
+			// type checking
+			unit.resolve();
+			// flow analysis
+			unit.analyseCode();
 
-      requestor.acceptResult(unit.compilationResult.tagAsAccepted());
-    }
-    return this.unitsToProcess;
-  }
+			requestor.acceptResult(unit.compilationResult);
+		}
+		
+		return this.unitsToProcess;
+	}
 }
