INRIA/spoon

commit 59bc4f693fe6fc0178d5327a2afb4db77011d1a7

path bug fix
git-svn-id: file:///svnroot/spoon/trunk/spoon-core@2179 b1f50995-73fd-43b8-8ca2-fbc405a04e82

src/main/java/spoon/support/builder/SpoonCompiler.java
@@ -75,8 +75,9 @@ public class SpoonCompiler  extends Main {
 		}
 		
 		// args.add("-nowarn");
-		String srcPath = f.getEnvironment().getSourcePath();
-		args.add(srcPath);
+		for (SpoonFile file : files) {
+			args.add(file.getPath());
+		}
 
 //		JDTCompiler compiler = new JDTCompiler(new PrintWriter(System.out),
 //				new PrintWriter(System.err));

src/main/java/spoon/support/StandardEnvironment.java
@@ -366,6 +366,7 @@ public class StandardEnvironment implements Serializable, Environment {
 		this.tabulationSize = tabulationSize;
 	}
 
+	@Deprecated
 	public String getSourcePath() {
 		return ".";
 	}

commit 4879051479ea13d800cf52b9f96302b832027442

fixed bug introduced by r2179 (broke snippet compilation)
git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@2200 b1f50995-73fd-43b8-8ca2-fbc405a04e82

src/main/java/spoon/support/builder/SpoonCompiler.java
@@ -23,7 +23,9 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import spoon.eclipse.jdt.core.compiler.CategorizedProblem;
 import spoon.eclipse.jdt.internal.compiler.CompilationResult;
@@ -103,9 +105,17 @@ public class SpoonCompiler  extends Main {
 		}
 		
 		// args.add("-nowarn");
+		// method configure JDT of JDT requires at least one file or one directory
+		Set<String> paths = new HashSet<String>();
 		for (SpoonFile file : files) {
-			args.add(file.getPath());
+		  // We can not use file.getPath() because when using in-memory code (e.g. snippets)
+		  // there is no real file on the disk
+		  // In this case, the virtual parent of the virtual file is "." (by convention)
+		  // and we are sure it exists
+		  // However, if . contains a lot of subfolders and Java files, it will take a lot of time
+			paths.add(file.getParent().getPath());
 		}
+		args.addAll(paths);
 
 //		JDTCompiler compiler = new JDTCompiler(new PrintWriter(System.out),
 //				new PrintWriter(System.err));

src/main/java/spoon/support/builder/support/VirtualFolder.java
@@ -77,7 +77,8 @@ public class VirtualFolder implements SpoonFolder {
 	}
 
 	public String getPath() {
-		return "Virtual folder";
+		// it has to be real path for snippet building 
+		return ".";
 	}
 	
 