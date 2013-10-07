5bb089bed1ecfab6d440dc3ef2bdf5378ca2c40f
martin <martin@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2013-10-07 04:13:57 +0000

    adds tests for insertBefore and insertAfter
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@2204 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/test/java/spoon/test/intercession/IntercessionTest.java
+++ src/test/java/spoon/test/intercession/IntercessionTest.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 
 import spoon.reflect.Factory;
 import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtCodeSnippetStatement;
 import spoon.reflect.code.CtReturn;
+import spoon.reflect.code.CtStatement;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
 import spoon.support.DefaultCoreFactory;
@@ -37,4 +39,62 @@ public class IntercessionTest {
 		assertEquals(2, body.getStatements().size());
 		assertSame(returnStmt, body.getStatements().get(0));
 	}
+
+
+	@Test
+	public void testInsertBefore() {
+		CtClass clazz = (CtClass) factory.Code().createCodeSnippetStatement(
+				""
+				+ "class X {"
+				+ "public void foo() {"
+				+ " int x=0;"
+				+ " int y=0;"
+				+ " int z=x+y;"
+				+ "}"
+				+ "};"
+		).compile();
+		CtMethod foo = (CtMethod) clazz.getMethods().toArray()[0];
+
+		CtBlock body = foo.getBody();
+		assertEquals(3, body.getStatements().size());
+		
+		CtStatement s = (CtStatement) body.getStatements().get(2);
+		assertEquals("int z = x + y", s.toString());
+		
+		// adding a new statement;
+		CtCodeSnippetStatement stmt = factory.Core().createCodeSnippetStatement();
+		stmt.setValue("System.out.println(x);");
+		s.insertBefore(stmt);
+		assertEquals(4, body.getStatements().size());
+		assertSame(stmt, body.getStatements().get(2));
+	}
+
+	@Test
+	public void testInsertAfter() {
+		CtClass clazz = (CtClass) factory.Code().createCodeSnippetStatement(
+				""
+				+ "class X {"
+				+ "public void foo() {"
+				+ " int x=0;"
+				+ " int y=0;"
+				+ " int z=x+y;"
+				+ "}"
+				+ "};"
+		).compile();
+		CtMethod foo = (CtMethod) clazz.getMethods().toArray()[0];
+
+		CtBlock body = foo.getBody();
+		assertEquals(3, body.getStatements().size());
+		
+		CtStatement s = (CtStatement) body.getStatements().get(2);
+		assertEquals("int z = x + y", s.toString());
+		
+		// adding a new statement;
+		CtCodeSnippetStatement stmt = factory.Core().createCodeSnippetStatement();
+		stmt.setValue("System.out.println(x);");
+		s.insertAfter(stmt);
+		assertEquals(4, body.getStatements().size());
+		assertSame(stmt, body.getStatements().get(3));
+	}
+
 }
