9a413f8b3a9065a8a1368a367f3a668e8081c167
martin <martin@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2013-10-10 04:25:05 +0000

    adds tests for if-related intercession
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@2208 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/test/java/spoon/test/replace/Foo.java
+++ src/test/java/spoon/test/replace/Foo.java
@@ -2,6 +2,12 @@ package spoon.test.replace;
 
 class Foo {
 	int i;
+	void foo() {
+		int x = 3;
+		int z;
+		z= x+1;
+		System.out.println(z);
+	}
 }
 
 class Bar {
--- src/test/java/spoon/test/replace/ReplaceTest.java
+++ src/test/java/spoon/test/replace/ReplaceTest.java
@@ -7,13 +7,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import spoon.reflect.Factory;
+import spoon.reflect.code.CtAssignment;
+import spoon.reflect.code.CtExpression;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtField;
-import spoon.reflect.declaration.CtSimpleType;
-import spoon.reflect.reference.CtTypeParameterReference;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.support.DefaultCoreFactory;
 import spoon.support.StandardEnvironment;
 import spoon.support.builder.SpoonCompiler;
@@ -22,14 +25,19 @@ import spoon.support.builder.support.FileSystemFile;
 
 public class ReplaceTest {
 
-	@Test 
-	public void testReplace() throws Exception {		
+	Factory factory;
+
+	@Before
+	public void setup() throws Exception {
+		factory = new Factory(new DefaultCoreFactory(), new StandardEnvironment());
 		SpoonCompiler comp = new SpoonCompiler();
 		List<SpoonFile> files = new ArrayList();
 		SpoonFile file = new FileSystemFile(new File("./src/test/java/spoon/test/replace/Foo.java"));
 		files.add(file);
-		Factory factory = new Factory(new DefaultCoreFactory(), new StandardEnvironment());
-		comp.compileSrc(factory, files);
+		comp.compileSrc(factory, files);		
+	}
+	@Test 
+	public void testReplaceSet() throws Exception {		
 		
 		CtClass foo = (CtClass) factory.Package().get("spoon.test.replace").getType("Foo");
 		assertEquals("Foo", foo.getSimpleName());
@@ -50,6 +58,31 @@ public class ReplaceTest {
 		i2.replace(i1);		
 		assertSame(i1,foo.getField("i"));
 		assertEquals("int",foo.getField("i").getType().getSimpleName());		
+	}
+
+	@Test 
+	public void testReplaceBlock() throws Exception {		
+		CtClass foo = (CtClass) factory.Package().get("spoon.test.replace").getType("Foo");
+		CtMethod m = foo.getElements(new TypeFilter<CtMethod>(CtMethod.class)).get(0);
+		assertEquals("foo", m.getSimpleName());
+		
+		CtAssignment assignment = (CtAssignment)m.getBody().getStatements().get(2);
+
+		CtExpression s1 = assignment.getAssignment();		
+		CtExpression s2 = factory.Code().createLiteral(3);
+		
+		assertEquals("z = x + 1",assignment.toString());
+		assertEquals("x + 1",s1.toString());
+
+		// do
+		s1.replace(s2);		
+		assertSame(s2,assignment.getAssignment());
+		assertEquals("z = 3",assignment.toString());
+
+		// undo
+		s2.replace(s1);		
+		assertSame(s1,assignment.getAssignment());
+		assertEquals("z = x + 1",assignment.toString());
 	}
 
 	
