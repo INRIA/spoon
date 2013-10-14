c978af812d03ddca1ee4c5cb8418117f7383a8f7
martin <martin@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2013-10-14 04:54:00 +0000

    fixes a bug: an access to "this" or "super" is not a field access.
    We may consider adding CtThisAccess and CtSuperAccess in the future (under CtVariableAccess)
    
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@2217 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/test/java/spoon/test/fieldaccesses/FieldAccessTest.java
+++ src/test/java/spoon/test/fieldaccesses/FieldAccessTest.java
@@ -21,15 +21,12 @@ public class FieldAccessTest {
 		CtMethod meth1 = (CtMethod) type.getElements(new NameFilter("meth1")).get(0);
 		CtMethod meth1b = (CtMethod) type.getElements(new NameFilter("meth1b")).get(0);
 
-		assertEquals(2, meth1.getElements(new TypeFilter(CtFieldAccess.class)).size());
+		assertEquals(3, meth1.getElements(new TypeFilter(CtFieldAccess.class)).size());
 		
-		// here we seen that an explicit this counts as a field access
-		// I'm not sure this is correct
-		// we should add a ThisAccess and maybe super access
-		assertEquals(3, meth1b.getElements(new TypeFilter(CtFieldAccess.class)).size());
+		assertEquals(2, meth1b.getElements(new TypeFilter(CtFieldAccess.class)).size());
 
 		CtMethod meth2 = (CtMethod) type.getElements(new NameFilter("meth2")).get(0);
-		assertEquals(3, meth2.getElements(new TypeFilter(CtFieldAccess.class)).size());
+		assertEquals(2, meth2.getElements(new TypeFilter(CtFieldAccess.class)).size());
 		
 		CtMethod meth3 = (CtMethod) type.getElements(new NameFilter("meth3")).get(0);
 		assertEquals(3, meth3.getElements(new TypeFilter(CtFieldAccess.class)).size());
