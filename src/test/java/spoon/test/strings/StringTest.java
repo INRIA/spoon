1753e6577355f6de093e4233c06f73f5b7b5ffb9
martin <martin@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2013-10-14 19:12:21 +0000

    adds a test for operator "+" on strings
    git-svn-id: file:///svnroot/spoon/trunk/spoon-core@2223 b1f50995-73fd-43b8-8ca2-fbc405a04e82

---
+++ src/test/java/spoon/test/strings/StringTest.java
@@ -0,0 +1,29 @@
+package spoon.test.strings;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static spoon.test.TestUtils.build;
+
+import org.junit.Test;
+
+import spoon.reflect.code.BinaryOperatorKind;
+import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.code.CtLiteral;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+public class StringTest {
+
+	@Test 
+	public void testModelBuildingInitializer() throws Exception {
+		CtClass type = (CtClass)build ("spoon.test.strings",  "Main");
+		assertEquals("Main", type.getSimpleName());
+		
+		CtBinaryOperator op = type.getElements(new TypeFilter<CtBinaryOperator>(CtBinaryOperator.class)).get(0);
+		assertEquals("(\"a\" + \"b\")", op.toString());
+		assertEquals(BinaryOperatorKind.PLUS, op.getKind());
+		assertTrue(op.getLeftHandOperand() instanceof CtLiteral);
+		assertTrue(op.getRightHandOperand() instanceof CtLiteral);
+		
+	}
+}
