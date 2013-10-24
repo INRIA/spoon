d63a921fcf84548f47445b0d2bf1ee3ff0d3a4c0
martin <martin@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2013-10-24 16:37:30 +0000

    adds a test for "diamond" init
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@2253 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/test/java/spoon/test/generics/GenericsTest.java
+++ src/test/java/spoon/test/generics/GenericsTest.java
@@ -7,14 +7,19 @@ import java.util.List;
 
 import org.junit.Test;
 
+import spoon.reflect.Factory;
+import spoon.reflect.code.CtNewClass;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtConstructor;
+import spoon.reflect.declaration.CtField;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtNamedElement;
 import spoon.reflect.declaration.CtParameter;
 import spoon.reflect.reference.CtTypeParameterReference;
 import spoon.reflect.visitor.filter.NameFilter;
 import spoon.reflect.visitor.filter.TypeFilter;
+import spoon.support.DefaultCoreFactory;
+import spoon.support.StandardEnvironment;
 
 public class GenericsTest {
 
@@ -44,6 +49,22 @@ public class GenericsTest {
 		assertEquals("E", generic.getSimpleName());
 		CtParameter param = (CtParameter) ((CtMethod)methods.get(0)).getParameters().get(0);
 		assertEquals("E", param.getType().toString());
+	}
+	
+	@Test
+	public void testDiamond() {
+		Factory factory = new Factory(new DefaultCoreFactory(), new StandardEnvironment());		
+		CtClass clazz = (CtClass) factory.Code().createCodeSnippetStatement(
+				"class Diamond {\n" + 
+				"	java.util.List<String> f = new java.util.ArrayList<>();\n" + 
+				"}"
+		).compile();
+		CtField f = (CtField) clazz.getFields().toArray()[0];
+		CtNewClass val = (CtNewClass)f.getDefaultExpression();
+		
+		// the diamond is resolved to String 
+		assertEquals("java.lang.String", val.getType().getActualTypeArguments().get(0).toString());
+		assertEquals("new java.util.ArrayList<java.lang.String>()", val.toString());		
 	}
 	
 }
