8fa6afeb36cc94788fb157a757bca545fc5e4366
renaud <renaud@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2013-11-02 16:41:03 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@2262 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/test/java/spoon/test/TestUtils.java
+++ src/test/java/spoon/test/TestUtils.java
@@ -1,30 +1,23 @@
 package spoon.test;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
+import spoon.Spoon;
+import spoon.compiler.SpoonCompiler;
+import spoon.compiler.SpoonFile;
+import spoon.compiler.SpoonResourceHelper;
 import spoon.reflect.Factory;
 import spoon.reflect.declaration.CtSimpleType;
-import spoon.support.DefaultCoreFactory;
-import spoon.support.StandardEnvironment;
-import spoon.support.builder.SpoonCompiler;
-import spoon.support.builder.SpoonFile;
-import spoon.support.builder.support.FileSystemFile;
 
 public class TestUtils {
 
 	public static <T extends CtSimpleType<?>> T build(String packageName,
 			String className) throws Exception {
-		SpoonCompiler comp = new SpoonCompiler();
-		List<SpoonFile> files = new ArrayList<SpoonFile>();
-		SpoonFile file = new FileSystemFile(new File("./src/test/java/"
-				+ packageName.replace('.', '/') + "/" + className + ".java"));
-		files.add(file);
-		Factory factory = new Factory(new DefaultCoreFactory(),
-				new StandardEnvironment());
-		comp.compileSrc(factory, files);
+		SpoonCompiler comp = Spoon.createCompiler();
+		Factory factory = Spoon.createFactory();
+		List<SpoonFile> files = SpoonResourceHelper.files("./src/test/java/"
+				+ packageName.replace('.', '/') + "/" + className + ".java");
+		comp.build(factory, files);
 		return factory.Package().get(packageName).getType(className);
 	}
-
 }
