472373ab41abced7e02dde99622f19942cbf86da
martin <martin@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2013-11-27 01:57:28 +0000

    specifies CtArrayTypeReference
    git-svn-id: file:///svnroot/spoon/trunk/spoon-core@2323 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/test/java/spoon/test/arrays/ArraysTest.java
+++ src/test/java/spoon/test/arrays/ArraysTest.java
@@ -1,19 +1,27 @@
 package spoon.test.arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static spoon.test.TestUtils.build;
 
 import org.junit.Test;
 
+import spoon.reflect.declaration.CtField;
 import spoon.reflect.declaration.CtSimpleType;
+import spoon.reflect.reference.CtArrayTypeReference;
 
 public class ArraysTest {
 
 	@Test
-	public void testModelBuildingArrays() throws Exception {
+	public void testArrayReferences() throws Exception {
 		CtSimpleType<?> type = build("spoon.test.arrays", "ArrayClass");
 		assertEquals("ArrayClass", type.getSimpleName());
 		assertEquals("int[][][]", type.getField("i").getType().toString());
-	}
+        assertEquals(3,((CtArrayTypeReference)type.getField("i").getType()).getDimensionCount());
+
+        CtField<?> x = type.getField("x");
+        assertTrue(x.getType() instanceof CtArrayTypeReference);
+        assertTrue(((CtArrayTypeReference)x.getType()).getComponentType().getActualClass().equals(int.class));
+    }
 
 }
