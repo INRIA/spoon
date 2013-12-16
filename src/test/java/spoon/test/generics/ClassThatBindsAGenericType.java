9688fd9c9a2cc6a852b6c0fc4040f4ebff739ef4
renaud <renaud@b1f50995-73fd-43b8-8ca2-fbc405a04e82>
2013-12-16 09:03:15 +0000

    do not call spoon API
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/spoon/trunk/spoon-core@2342 b1f50995-73fd-43b8-8ca2-fbc405a04e82

--- src/test/java/spoon/test/generics/ClassThatBindsAGenericType.java
+++ src/test/java/spoon/test/generics/ClassThatBindsAGenericType.java
@@ -1,17 +1,11 @@
 package spoon.test.generics;
 
-import spoon.Launcher;
-
 import java.io.File;
 import java.util.ArrayList;
 
 public class ClassThatBindsAGenericType extends ArrayList<File> {
 
 	public static void main(String[] args) throws Exception {
-		Launcher.main(new String[] {
-				"-i", "src/test/java/spoon/test/generics",
-				"-g", "--no"
-		});
 	}
 	
 }
