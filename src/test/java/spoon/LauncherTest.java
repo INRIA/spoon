d252820c64ea47585dc2b9d48186b212bf52fb5f
Gerard <gerard.paligot@gmail.com>
2014-10-03 15:46:54 +0200

    fix #38 Fix test case of the initEnvironment method
    Remove useless attribute of test class and change default
    value for properties file and output directory.

--- src/test/java/spoon/LauncherTest.java
+++ src/test/java/spoon/LauncherTest.java
@@ -6,42 +6,54 @@ import junit.framework.TestCase;
 
 import org.junit.Assert;
 
+import org.junit.Test;
+import spoon.compiler.Environment;
 import spoon.reflect.visitor.FragmentDrivenJavaPrettyPrinter;
 import spoon.support.JavaOutputProcessor;
 import spoon.support.StandardEnvironment;
 
 public class LauncherTest extends TestCase {
 
-    private Launcher classUnderTest;
+	@Test
+	public void testInitEnvironment() throws Exception {
+		// Arguments given in command line but hardcoded for the test.
+		File properties = new File(".");
+		int complianceLevel = 5;
+		boolean verbose = true;
+		boolean debug = true;
+		boolean autoImports = true;
+		int tabulationSize = 42;
+		boolean useTabulations = true;
+		boolean useSourceCodeFragments = true;
+		boolean preserveLineNumbers = true;
 
-    public void setUp() throws Exception {
-        classUnderTest = new Launcher();
-    }
+		// Main class of Spoon who contain initEnvironment method.
+		final Launcher launcher = new Launcher();
 
-    public void testInitEnvironment() throws Exception {
-        File properties = new File("/");
-        int complianceLevel = 5;
-        boolean verbose = true;
-        boolean debug = true;
-        boolean autoImports = true;
-        int tabulationSize = 42;
-        boolean useTabulations = true;
-        boolean useSourceCodeFragments = true;
-        boolean preserveLineNumbers = true;
+		// The attribute factory isn't public in Launcher class so we create a
+		// new one and specify his implementation as type to have an access to
+		// getXmlRootFolder method.
+		final StandardEnvironment environment = new StandardEnvironment();
 
-        StandardEnvironment environment = new StandardEnvironment();
-        classUnderTest.initEnvironment(environment, complianceLevel, verbose, debug, properties, autoImports, tabulationSize, useTabulations, useSourceCodeFragments, preserveLineNumbers, new File("/home"));
+		launcher.initEnvironment(environment, complianceLevel, verbose,
+				debug, properties, autoImports, tabulationSize, useTabulations,
+				useSourceCodeFragments, preserveLineNumbers, new File("target/spooned"));
 
-        Assert.assertEquals(properties, environment.getXmlRootFolder());
-        Assert.assertTrue(environment.isVerbose());
-        Assert.assertTrue(environment.isDebug());
-        Assert.assertTrue(environment.isAutoImports());
-        Assert.assertTrue(environment.isUsingTabulations());
-        Assert.assertTrue(environment.isUsingSourceCodeFragments());
-        Assert.assertTrue(environment.isPreserveLineNumbers());
-        Assert.assertEquals(tabulationSize, environment.getTabulationSize());
+		// Verify if the environment is correct.
+		Assert.assertEquals(properties, environment.getXmlRootFolder());
+		Assert.assertTrue(environment.isVerbose());
+		Assert.assertTrue(environment.isDebug());
+		Assert.assertTrue(environment.isAutoImports());
+		Assert.assertTrue(environment.isUsingTabulations());
+		Assert.assertTrue(environment.isUsingSourceCodeFragments());
+		Assert.assertTrue(environment.isPreserveLineNumbers());
+		Assert.assertEquals(tabulationSize, environment.getTabulationSize());
 
-        JavaOutputProcessor processor = (JavaOutputProcessor) environment.getDefaultFileGenerator();
-        Assert.assertTrue(processor.getPrinter() instanceof FragmentDrivenJavaPrettyPrinter);
-    }
+		// Check if the processor of the output is a FragmentDrivenJavaPrettyPrinter
+		//
+		JavaOutputProcessor processor = (JavaOutputProcessor) environment
+				.getDefaultFileGenerator();
+		Assert.assertTrue(processor
+				.getPrinter() instanceof FragmentDrivenJavaPrettyPrinter);
+	}
 }
