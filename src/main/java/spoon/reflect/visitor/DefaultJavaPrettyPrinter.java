6a91516d1ec5e7b84c95be282537fffc0cf6e7c0
Nicolas Petitprez <nicolas.petitprez@gmail.com>
2014-10-08 15:27:43 +0200

    Fix visibility on static fields in interface include in class. Print target for field access only if needed

--- src/main/java/spoon/reflect/visitor/DefaultJavaPrettyPrinter.java
+++ src/main/java/spoon/reflect/visitor/DefaultJavaPrettyPrinter.java
@@ -172,6 +172,12 @@ public class DefaultJavaPrettyPrinter implements CtVisitor, PrettyPrinter {
 		}
 
 		@Override
+		public <T> void visitCtInvocation(CtInvocation<T> invocation) {
+			// For a ctinvocation, we don't have to import declaring type
+			scan(invocation.getTarget());
+		}
+
+		@Override
 		public <T> void visitCtTypeReference(CtTypeReference<T> reference) {
 			if (!(reference instanceof CtArrayTypeReference)) {
 				if (reference.getDeclaringType() == null) {
@@ -1146,8 +1152,8 @@ public class DefaultJavaPrettyPrinter implements CtVisitor, PrettyPrinter {
 		boolean printType = true;
 
 		if (reference.isFinal() && reference.isStatic()) {
+			CtTypeReference<?> declTypeRef = reference.getDeclaringType();
 			if (context.currentTopLevel != null) {
-				CtTypeReference<?> declTypeRef = reference.getDeclaringType();
 				CtTypeReference<?> ref2;
 				if (context.currentThis != null
 						&& context.currentThis.size() > 0) {
@@ -1157,10 +1163,10 @@ public class DefaultJavaPrettyPrinter implements CtVisitor, PrettyPrinter {
 				}
 				// print type if not annonymous class ref and not within the
 				// current scope
-				printType = !declTypeRef.getSimpleName().equals("")
+				printType = !"".equals(declTypeRef.getSimpleName())
 						&& !(declTypeRef.equals(ref2));
 			} else {
-				printType = true;
+				printType = !"".equals(declTypeRef.getSimpleName());
 			}
 		}
 
@@ -1299,7 +1305,7 @@ public class DefaultJavaPrettyPrinter implements CtVisitor, PrettyPrinter {
 	public <T> void visitCtInvocation(CtInvocation<T> invocation) {
 		enterCtStatement(invocation);
 		enterCtExpression(invocation);
-//BCUTAG ??? 
+//BCUTAG ???
 //		if (invocation.getExecutable() ==null || invocation.getExecutable().getSimpleName() == null){
 //			exitCtExpression(invocation);
 //			return;
---
+++ src/test/java/spoon/test/staticFieldAccess/internal/Extends.java
@@ -0,0 +1,8 @@
+package spoon.test.staticFieldAccess.internal;
+
+/**
+ * Created by nicolas on 05/09/2014.
+ */
+public class Extends extends Origin {
+	public static final String MY_OTHER_STATIC_VALUE = "Value2";
+}
