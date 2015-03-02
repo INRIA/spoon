c87c1c789e5b6c8c1bf9aa3ded9254a94966a73c
Nicolas Petitprez <nicolas.petitprez@gmail.com>
2015-03-02 16:47:14 +0100

    move symbolic evaluator to a standalone project

--- src/main/java/spoon/reflect/factory/EvalFactory.java
+++ src/main/java/spoon/reflect/factory/EvalFactory.java
@@ -17,20 +17,8 @@
 
 package spoon.reflect.factory;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
-import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.eval.PartialEvaluator;
-import spoon.reflect.eval.SymbolicEvaluationPath;
-import spoon.reflect.eval.SymbolicEvaluator;
-import spoon.reflect.eval.SymbolicEvaluatorObserver;
-import spoon.reflect.eval.SymbolicInstance;
-import spoon.reflect.eval.observer.SymbolicEvaluationPathsMaker;
-import spoon.reflect.reference.CtTypeReference;
 import spoon.support.reflect.eval.VisitorPartialEvaluator;
-import spoon.support.reflect.eval.VisitorSymbolicEvaluator;
 
 /**
  * A factory to create some evaluation utilities on the Spoon metamodel.
@@ -51,50 +39,4 @@ public class EvalFactory extends SubFactory {
 		return new VisitorPartialEvaluator();
 	}
 
-	/**
-	 * Creates a symbolic evaluator on the Spoon meta-model.
-	 * 
-	 * @param observers
-	 *            the observers to be notified of the the evaluation progress
-	 */
-	public SymbolicEvaluator createSymbolicEvaluator(
-			SymbolicEvaluatorObserver... observers) {
-		return new VisitorSymbolicEvaluator(observers);
-	}
-
-	/**
-	 * Creates a new symbolic instance.
-	 * 
-	 * @param evaluator
-	 *            the evaluator
-	 * @param concreteType
-	 *            the type of the instance
-	 * @param isType
-	 *            tells if it is a type instance or a regular instance
-	 */
-	public <T> SymbolicInstance<T> createSymbolicInstance(
-			SymbolicEvaluator evaluator, CtTypeReference<T> concreteType,
-			boolean isType) {
-		return new SymbolicInstance<T>(evaluator, concreteType, isType);
-	}
-
-	/**
-	 * Gets the symbolic evaluation paths of the program, as calculated by
-	 * {@link spoon.reflect.eval.SymbolicEvaluator}.
-	 * 
-	 * @param entryPoints
-	 *            the entry point methods
-	 * @return a map containing the paths for each entry point
-	 */
-	public Map<CtMethod<?>, Collection<SymbolicEvaluationPath>> createSymbolicEvaluationPaths(
-			Collection<CtMethod<?>> entryPoints) {
-		Map<CtMethod<?>, Collection<SymbolicEvaluationPath>> results = new HashMap<CtMethod<?>, Collection<SymbolicEvaluationPath>>();
-		for (CtMethod<?> m : entryPoints) {
-			SymbolicEvaluationPathsMaker pathsMaker = new SymbolicEvaluationPathsMaker();
-			SymbolicEvaluator evaluator = createSymbolicEvaluator(pathsMaker);
-			evaluator.invoke(m);
-			results.put(m, pathsMaker.getPaths());
-		}
-		return results;
-	}
 }
