be014b15661aef2dafff7cc9f6073cf118fcafda
Nicolas Petitprez <nicolas.petitprez@gmail.com>
2015-03-25 17:02:55 +0100

    Fix CtExecutableReference.getActualMethod() when method is inherited

--- src/test/java/spoon/test/executable/ExecutableRefTestSource.java
+++ src/test/java/spoon/test/executable/ExecutableRefTestSource.java
@@ -1,12 +1,16 @@
 package spoon.test.executable;
 
-public class ExecutableRefTestSource {
-    
-    public void testMethod() {
-        String.valueOf("Hello World");
-    }
-    
-    public void testConstructor() {
-        new String("Hello World");
-    }
+public class ExecutableRefTestSource implements MyIntf {
+
+	public void testMethod() {
+		String.valueOf("Hello World");
+	}
+
+	public void testConstructor() {
+		new String("Hello World");
+	}
+
+	@Override
+	public void myMethod() {
+	}
 }
