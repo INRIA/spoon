59afe5984b862a29d6ce8363e5824e871cb51fd9
Lionel Seinturier <Lionel.Seinturier@univ-lille1.fr>
2015-04-19 21:26:09 +0200

    Test and bug fix for CtTypeImpl#getUsedTypes that throws a NPE when inner types are used.

--- src/test/java/spoon/test/model/TypeTest.java
+++ src/test/java/spoon/test/model/TypeTest.java
@@ -8,21 +8,28 @@ import java.util.Collection;
 import org.junit.Test;
 
 import spoon.reflect.declaration.CtType;
-import spoon.reflect.declaration.CtType;
 import spoon.reflect.reference.CtExecutableReference;
 
 public class TypeTest {
 
 	@Test
-	public void test() throws Exception {
+	public void testGetAllExecutables() throws Exception {
 		CtType<?> type = build("spoon.test.model", "Foo");
 		assertEquals(1, type.getDeclaredFields().size());
-		assertEquals(2, ((CtType) type).getMethods().size());
-		assertEquals(3, type.getDeclaredExecutables().size());
+		assertEquals(3, type.getMethods().size());
+		assertEquals(4, type.getDeclaredExecutables().size());
 		assertEquals(2, type.getAllFields().size());
 		
-		// we have 4  methods + one explicit constructor + two implicit constructors for Bar and Baz
+		// we have 4  methods + one explicit constructor + two implicit
+		// constructors for Bar, Baz and Baz.Inner
 		Collection<CtExecutableReference<?>> allExecutables = type.getAllExecutables();
-		assertEquals(7, allExecutables.size());
+		assertEquals(8, allExecutables.size());
+	}
+
+	@Test
+	public void testGetUsedTypes() throws Exception {
+		CtType<?> type = build("spoon.test.model", "Foo");
+		assertEquals(3, type.getUsedTypes(true).size());
+		assertEquals(0, type.getUsedTypes(false).size());
 	}
 }
