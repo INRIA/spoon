c0b4dc72fec68155c1d60e325ef01229b73089db
GerardPaligot <gerard.paligot@gmail.com>
2015-08-10 16:07:54 +0200

    feat(enum): Enables annotations on fields of enum.

--- src/test/java/spoon/test/enums/EnumsTest.java
+++ src/test/java/spoon/test/enums/EnumsTest.java
@@ -5,17 +5,38 @@ import static spoon.test.TestUtils.build;
 
 import org.junit.Test;
 
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtEnum;
+import spoon.reflect.visitor.filter.FieldAccessFilter;
+import spoon.test.annotation.AnnotationTest;
+import spoon.test.enums.testclasses.Foo;
 
 public class EnumsTest {
 
-	@Test 
+	@Test
 	public void testModelBuildingEnum() throws Exception {
-		CtEnum<Regular> enumeration = build ("spoon.test.enums",  "Regular");
+		CtEnum<Regular> enumeration = build("spoon.test.enums", "Regular");
 		assertEquals("Regular", enumeration.getSimpleName());
 		assertEquals(3, Regular.values().length);
 		assertEquals(3, enumeration.getValues().size());
 		assertEquals("A", enumeration.getValues().get(0).getSimpleName());
 		assertEquals(5, enumeration.getFields().size());
+	}
+
+	@Test
+	public void testAnnotationsOnEnum() throws Exception {
+		final Launcher launcher = new Launcher();
+		launcher.run(new String[] {
+				"-i", "./src/test/java/spoon/test/enums/testclasses",
+				"-o", "./target/spooned"
+		});
+
+		final CtEnum<?> foo = (CtEnum) launcher.getFactory().Type().get(Foo.class);
+		assertEquals(1, foo.getFields().size());
+		assertEquals(1, foo.getFields().get(0).getAnnotations().size());
+		assertEquals(Deprecated.class, AnnotationTest.getActualClassFromAnnotation(
+				foo.getFields().get(0).getAnnotations().get(0)));
+		assertEquals("public enum Foo {\n@java.lang.Deprecated\n    Bar;}", foo.toString());
 	}
 }
