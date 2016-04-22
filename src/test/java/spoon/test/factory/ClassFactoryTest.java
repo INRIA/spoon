4c63d096faabff1647664000b2e2646e964334d5
GerardPaligot <gerard.paligot@gmail.com>
2016-04-22 13:34:18 +0200

    test(factory): Create a class in a package.

--- src/test/java/spoon/test/factory/ClassFactoryTest.java
+++ src/test/java/spoon/test/factory/ClassFactoryTest.java
@@ -2,6 +2,7 @@ package spoon.test.factory;
 
 import org.junit.Test;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtPackage;
 import spoon.reflect.factory.Factory;
 
 import static org.junit.Assert.assertEquals;
@@ -18,5 +19,17 @@ public class ClassFactoryTest {
 
 		assertEquals("Inner", inner.getSimpleName());
 		assertEquals(declaringClass, inner.getDeclaringType());
+	}
+
+	@Test
+	public void testTopLevelClass() throws Exception {
+		final Factory factory = createFactory();
+		final CtPackage aPackage = factory.Core().createPackage();
+		aPackage.setSimpleName("spoon");
+
+		final CtClass<Object> topLevel = factory.Class().create(aPackage, "TopLevel");
+
+		assertEquals("TopLevel", topLevel.getSimpleName());
+		assertEquals(aPackage, topLevel.getPackage());
 	}
 }
