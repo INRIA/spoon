c7949f73ee3ba7d16779f5cb404588c719679238
GerardPaligot <gerard.paligot@gmail.com>
2016-06-16 14:49:56 +0200

    fix(deque): Order to iterate with a Deque and Stack are differents.

---
+++ src/test/java/spoon/test/reference/testclasses/Pozole.java
@@ -0,0 +1,12 @@
+package spoon.test.reference.testclasses;
+
+public class Pozole {
+	void m() {
+		double[] repair = new double[45];
+	}
+
+	void m2() {
+		final double[] repair = new double[1];
+		repair[0] = 5d;
+	}
+}
