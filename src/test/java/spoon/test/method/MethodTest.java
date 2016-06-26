36786b9ef027244547045e989d0a0bece1fdc68f
Lionel Seinturier <Lionel.Seinturier@univ-lille1.fr>
2016-06-26 14:34:50 +0200

    Test to illustrate bug with setVisibility when cloning methods.

---
+++ src/test/java/spoon/test/method/MethodTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2006-2015 INRIA and contributors
+ * Spoon - http://spoon.gforge.inria.fr/
+ *
+ * This software is governed by the CeCILL-C License under French law and
+ * abiding by the rules of distribution of free software. You can use, modify
+ * and/or redistribute the software under the terms of the CeCILL-C license as
+ * circulated by CEA, CNRS and INRIA at http://www.cecill.info.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the CeCILL-C License for more details.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
+
+package spoon.test.method;
+
+import static org.junit.Assert.assertEquals;
+import static spoon.testing.utils.ModelUtils.build;
+
+import org.junit.Test;
+
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.ModifierKind;
+import spoon.reflect.factory.Factory;
+import spoon.test.delete.testclasses.Adobada;
+
+public class MethodTest {
+	
+	@Test
+	public void testClone() throws Exception {
+		
+		final Factory factory = build(Adobada.class);
+		final CtClass<Adobada> adobada = factory.Class().get(Adobada.class);
+		final CtMethod<?> m2 = adobada.getMethod("m2");
+		
+		CtMethod<?> clone = m2.clone();
+		m2.setVisibility(ModifierKind.PRIVATE);
+		
+		assertEquals(ModifierKind.PUBLIC, m2.getModifiers().iterator().next());
+	}
+}
