d867a19cd454fffeaa8b3e536661bfcebace41a8
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2016-07-22 23:04:20 +0200

    Resolve Literal#constant if required. Fixes #755.
    In some cases (an example is given in #755) Literal#constant may
    be `null` and, thus, causes a NullPointerException when calling
    `localDeclaration.initialization.traverse(this, scope);`. This
    patch fixes the issue by resolving Literal#constant if required.

--- src/main/java/spoon/support/compiler/jdt/JDTTreeBuilder.java
+++ src/main/java/spoon/support/compiler/jdt/JDTTreeBuilder.java
@@ -61,6 +61,7 @@ import org.eclipse.jdt.internal.compiler.ast.InstanceOfExpression;
 import org.eclipse.jdt.internal.compiler.ast.IntLiteral;
 import org.eclipse.jdt.internal.compiler.ast.LabeledStatement;
 import org.eclipse.jdt.internal.compiler.ast.LambdaExpression;
+import org.eclipse.jdt.internal.compiler.ast.Literal;
 import org.eclipse.jdt.internal.compiler.ast.LocalDeclaration;
 import org.eclipse.jdt.internal.compiler.ast.LongLiteral;
 import org.eclipse.jdt.internal.compiler.ast.MarkerAnnotation;
@@ -1601,6 +1602,15 @@ public class JDTTreeBuilder extends ASTVisitor {
 
 		if (localDeclaration.initialization != null) {
 			context.arguments.push(v);
+			// resolve Literal#constant if null (by calling `resolveType`). Otherwise,
+			// `localDeclaration.initialization.traverse(this, scope);` throws a
+			// NullPointerException. Fixes #755.
+			if (localDeclaration.initialization instanceof Literal) {
+				final Literal literal = (Literal) localDeclaration.initialization;
+				if (literal.constant == null) {
+					literal.resolveType(scope);
+				}
+			}
 			localDeclaration.initialization.traverse(this, scope);
 			context.arguments.pop();
 		}
