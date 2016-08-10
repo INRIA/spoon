36d1b5d59fdf8942e9f2a4263f489a09487b0333
bdanglot <bdanglot@gmail.com>
2016-08-10 10:32:28 +0200

    test fix names of anonymous and internal classes

--- src/test/java/spoon/test/limits/utils/InternalTest.java
+++ src/test/java/spoon/test/limits/utils/InternalTest.java
@@ -1,14 +1,20 @@
 package spoon.test.limits.utils;
 
 import org.junit.Test;
+import spoon.reflect.code.CtConstructorCall;
+import spoon.reflect.code.CtNewClass;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtNamedElement;
 import spoon.reflect.visitor.filter.NameFilter;
 import spoon.reflect.visitor.filter.TypeFilter;
 
 import java.util.List;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static spoon.testing.utils.ModelUtils.build;
 
 public class InternalTest {
@@ -55,5 +61,60 @@ public class InternalTest {
 		assertEquals(1, fields.size());
 
 	}
+
+	@Test
+	public void testInternalClassNameWithinMethod() throws Exception {
+		final String nl = System.lineSeparator();
+
+		CtClass<?> type = build("spoon.test.limits.utils",
+				"ContainInternalClass");
+		assertEquals("ContainInternalClass", type.getSimpleName());
+
+		//creating and inserting a new class within the method
+		CtMethod method = type.getMethod("method");
+		CtClass internal = type.getFactory().Core().createClass();
+		internal.setSimpleName("InternalMethodClass");
+		method.getBody().insertBegin(internal);
+		assertEquals("void method() {" + nl + "    class InternalMethodClass {    }" + nl + "}", method.toString());
+
+		CtConstructorCall call = type.getFactory().Code().createConstructorCall(internal.getReference());
+		internal.insertAfter(call);
+
+		CtClass classInserted = method.getElements(new TypeFilter<CtClass>(CtClass.class)).get(0);
+		assertTrue(classInserted.isLocalType());
+		assertEquals("InternalMethodClass", classInserted.getSimpleName());
+	}
+
+	@Test
+	public void testAnonymousClassNames() throws Exception {
+		final String nl = System.lineSeparator();
+
+		CtClass<?> type = build("spoon.test.limits.utils",
+				"ContainInternalClass");
+		assertEquals("ContainInternalClass", type.getSimpleName());
+
+		List<CtNewClass> newClasses = type.getElements(new TypeFilter<CtNewClass>(CtNewClass.class));
+		for (CtNewClass newClass : newClasses) {
+			assertNotNull(newClass.getAnonymousClass());
+			assertEquals(String.valueOf(newClasses.indexOf(newClass) + 1), newClass.getAnonymousClass().getSimpleName());
+		}
+
+		CtClass<?> anonymous = type.getFactory().Core().createClass();
+		CtNewClass<?> newAnonymous = type.getFactory().Code().createNewClass(type.getReference(), anonymous);
+		CtMethod<Object> method = type.getMethod("method");
+		newClasses = method.getElements(new TypeFilter<CtNewClass>(CtNewClass.class));
+		assertTrue(newClasses.isEmpty());
+		method.getBody().insertBegin(newAnonymous);
+		assertEquals("void method() {" + nl + "    new spoon.test.limits.utils.ContainInternalClass() {    };" + nl + "}", method.toString());
+
+		newClasses = method.getElements(new TypeFilter<CtNewClass>(CtNewClass.class));
+		assertFalse(newClasses.isEmpty());
+		assertNotNull(newClasses.get(0).getAnonymousClass());
+
+		Integer simpleNameOfAddedAnonymousClass = Integer.parseInt(newClasses.get(0).getAnonymousClass().getSimpleName());
+		assertTrue(simpleNameOfAddedAnonymousClass > 1000);
+		assertTrue(simpleNameOfAddedAnonymousClass < Integer.MAX_VALUE);
+	}
+
 
 }
