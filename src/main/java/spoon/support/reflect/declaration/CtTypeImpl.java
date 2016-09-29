8ab5f543972a7264c75968bbda8b4f0cf74a8539
arnobl <blouin.arno@gmail.com>
2016-09-29 18:52:51 +0200

    feat(type): a method to know whether a given type has a given method

--- src/main/java/spoon/support/reflect/declaration/CtTypeImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtTypeImpl.java
@@ -16,6 +16,17 @@
  */
 package spoon.support.reflect.declaration;
 
+import static spoon.reflect.ModelElementContainerDefaultCapacities.TYPE_TYPE_PARAMETERS_CONTAINER_DEFAULT_CAPACITY;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import spoon.reflect.code.CtBlock;
 import spoon.reflect.cu.CompilationUnit;
 import spoon.reflect.declaration.CtAnnotation;
@@ -739,6 +750,47 @@ public abstract class CtTypeImpl<T> extends CtNamedElementImpl implements CtType
 		}
 		return result;
 	}
+
+
+	@Override
+	public boolean hasMethod(CtMethod<?> method) {
+		if(method==null)
+			return false;
+
+		// Checking whether the parent is the calling type.
+		try {
+			if(method.getParent()==this) {
+				return true;
+			}
+		}catch(ParentNotInitializedException ex) {
+			return false;
+		}
+
+		// If the method is not the type and, however, a method with its signature exists, this means
+		// that 'method' is overriden here. So, we ignore this overriden method since only leaf methods are considered.
+		final String over = method.getSignature();
+		for(CtMethod<?> m : getMethods()) {
+			if(m.getSignature().equals(over)) {
+				return false;
+			}
+		}
+
+		// Checking whether a super class has the method.
+		final CtTypeReference<?> superCl = getSuperclass();
+		if(superCl!=null && superCl.getDeclaration().hasMethod(method)) {
+			return true;
+		}
+
+		// Finally, checking whether an interface has the method (a default method).
+		for(CtTypeReference<?> interf : getSuperInterfaces()) {
+			if(interf.getDeclaration().hasMethod(method)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 
 	@Override
 	public String getQualifiedName() {
