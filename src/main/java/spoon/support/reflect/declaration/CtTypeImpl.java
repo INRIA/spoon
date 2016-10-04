dfd42eba0af794ff46957c20d123847dc34cbc1f
arnobl <blouin.arno@gmail.com>
2016-10-04 16:51:17 +0200

    fixed(api): changing the behaviour of CtType::hasMethod

--- src/main/java/spoon/support/reflect/declaration/CtTypeImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtTypeImpl.java
@@ -27,6 +27,8 @@ import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import spoon.SpoonException;
 import spoon.reflect.code.CtBlock;
 import spoon.reflect.cu.CompilationUnit;
 import spoon.reflect.declaration.CtAnnotation;
@@ -60,17 +62,6 @@ import spoon.support.reflect.reference.SpoonClassNotFoundException;
 import spoon.support.util.QualifiedNameBasedSortedSet;
 import spoon.support.util.SignatureBasedSortedSet;
 
-import java.lang.annotation.Annotation;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static spoon.reflect.ModelElementContainerDefaultCapacities.TYPE_TYPE_PARAMETERS_CONTAINER_DEFAULT_CAPACITY;
 
 /**
  * The implementation for {@link spoon.reflect.declaration.CtType}.
@@ -764,19 +755,28 @@ public abstract class CtTypeImpl<T> extends CtNamedElementImpl implements CtType
 				return true;
 			}
 		} catch (ParentNotInitializedException ex) {
-			return false;
+			// No matter, trying something else
 		}
 
 		// Checking whether a super class has the method.
 		final CtTypeReference<?> superCl = getSuperclass();
-		if (superCl != null && superCl.getTypeDeclaration().hasMethod(method)) {
-			return true;
+
+		try {
+			if (superCl != null && superCl.getTypeDeclaration().hasMethod(method)) {
+				return true;
+			}
+		} catch (SpoonException ex) {
+			// No matter, trying something else
 		}
 
 		// Finally, checking whether an interface has the method.
 		for (CtTypeReference<?> interf : getSuperInterfaces()) {
-			if (interf.getDeclaration().hasMethod(method)) {
-				return true;
+			try {
+				if (interf.getTypeDeclaration().hasMethod(method)) {
+					return true;
+				}
+			} catch (SpoonException ex) {
+				// No matter, trying something else
 			}
 		}
 
