cbb99eb05f7f6096ffc3ba9d513fa3b8c3cfaafa
Thomas Durieux <tdurieux@users.noreply.github.com>
2016-10-12 14:23:48 +0200

    fix(literal): fix literal type

--- src/test/java/spoon/test/literal/LiteralTest.java
+++ src/test/java/spoon/test/literal/LiteralTest.java
@@ -1,19 +1,24 @@
 package spoon.test.literal;
 
-import static org.junit.Assert.assertEquals;
-import static spoon.testing.utils.ModelUtils.canBeBuilt;
-
-import java.util.TreeSet;
-
 import org.junit.Test;
-
 import spoon.Launcher;
 import spoon.reflect.code.CtLiteral;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtType;
 import spoon.reflect.factory.CodeFactory;
 import spoon.reflect.factory.Factory;
+import spoon.reflect.factory.TypeFactory;
 import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.support.comparator.DeepRepresentationComparator;
+import spoon.test.literal.testclasses.Tacos;
+
+import java.util.TreeSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static spoon.testing.utils.ModelUtils.buildClass;
+import static spoon.testing.utils.ModelUtils.canBeBuilt;
 
 public class LiteralTest {
 	@Test
@@ -34,6 +39,7 @@ public class LiteralTest {
 			}
 		}));
 
+		assertTrue(ts.last().getType().isPrimitive());
 		assertEquals(':', ts.last().getValue());
 		canBeBuilt("./target/literal", 8, true);
 	}
@@ -44,6 +50,58 @@ public class LiteralTest {
 		runLaunch.getEnvironment().setNoClasspath(true);
 		runLaunch.addInputResource("./src/test/resources/noclasspath/LiteralInForEach.java");
 		runLaunch.buildModel();
+	}
+	@Test
+	public void testBuildLiternal() throws Exception {
+		CtType<Tacos> ctType = buildClass(Tacos.class);
+		TypeFactory typeFactory = ctType.getFactory().Type();
+
+		CtLiteral<?> literal = (CtLiteral<?>) ctType.getField("a").getDefaultExpression();
+		assertEquals(0, literal.getValue());
+		assertTrue(literal.getType().isPrimitive());
+		assertEquals(typeFactory.INTEGER_PRIMITIVE, literal.getType());
+
+
+		literal = (CtLiteral<?>) ctType.getField("b").getDefaultExpression();
+		assertEquals(0x0, literal.getValue());
+		assertTrue(literal.getType().isPrimitive());
+		assertEquals(typeFactory.INTEGER_PRIMITIVE, literal.getType());
+
+
+		literal = (CtLiteral<?>) ctType.getField("c").getDefaultExpression();
+		assertEquals(0f, literal.getValue());
+		assertTrue(literal.getType().isPrimitive());
+		assertEquals(typeFactory.FLOAT_PRIMITIVE, literal.getType());
+
+
+		literal = (CtLiteral<?>) ctType.getField("d").getDefaultExpression();
+		assertEquals(0l, literal.getValue());
+		assertTrue(literal.getType().isPrimitive());
+		assertEquals(typeFactory.LONG_PRIMITIVE, literal.getType());
+
+
+		literal = (CtLiteral<?>) ctType.getField("e").getDefaultExpression();
+		assertEquals(0d, literal.getValue());
+		assertTrue(literal.getType().isPrimitive());
+		assertEquals(typeFactory.DOUBLE_PRIMITIVE, literal.getType());
+
+
+		literal = (CtLiteral<?>) ctType.getField("f").getDefaultExpression();
+		assertEquals('0', literal.getValue());
+		assertTrue(literal.getType().isPrimitive());
+		assertEquals(typeFactory.CHARACTER_PRIMITIVE, literal.getType());
+
+
+		literal = (CtLiteral<?>) ctType.getField("g").getDefaultExpression();
+		assertEquals("0", literal.getValue());
+		assertFalse(literal.getType().isPrimitive());
+		assertEquals(typeFactory.STRING, literal.getType());
+
+		literal = (CtLiteral<?>) ctType.getField("h").getDefaultExpression();
+		assertEquals(null, literal.getValue());
+		assertFalse(literal.getType().isPrimitive());
+		assertEquals(typeFactory.NULL_TYPE, literal.getType());
+
 	}
 
 	@Test
