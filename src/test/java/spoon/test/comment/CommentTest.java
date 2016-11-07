08ac40cf57cab184bf9dd058cd629d075fcfb444
Pavel Vojtechovsky <pvojtechovsky@users.noreply.github.com>
2016-11-07 22:45:09 +0100

    test: remove unused imports (#941)

--- src/test/java/spoon/test/comment/CommentTest.java
+++ src/test/java/spoon/test/comment/CommentTest.java
@@ -31,7 +31,6 @@ import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.factory.Factory;
 import spoon.reflect.factory.FactoryImpl;
 import spoon.reflect.visitor.filter.AbstractFilter;
-import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.support.DefaultCoreFactory;
 import spoon.support.StandardEnvironment;
@@ -48,7 +47,6 @@ import java.util.regex.Pattern;
 import static org.apache.commons.io.IOUtils.write;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class CommentTest {
