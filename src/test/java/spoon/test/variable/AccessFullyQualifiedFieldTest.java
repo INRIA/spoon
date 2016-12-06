0b5371da356b8e63383d5da1e92bdc0fa7314a3c
Simon Urli <simon.urli@inria.fr>
2016-12-06 10:27:39 +0100

    Revert "Merge branch 'bug-static-import' of https://github.com/monperrus/spoon into martin"
    This reverts commit 26092840339a89a5db7db11836f95d331747e697, reversing
    changes made to 49d6e684b0e6922e102ba2bbd25a711f44155498.

--- src/test/java/spoon/test/variable/AccessFullyQualifiedFieldTest.java
+++ src/test/java/spoon/test/variable/AccessFullyQualifiedFieldTest.java
@@ -1,16 +1,11 @@
 package spoon.test.variable;
 
 import org.junit.Test;
-import spoon.Launcher;
-import spoon.reflect.declaration.CtType;
-import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.factory.Factory;
 import spoon.test.main.MainTest;
-import spoon.test.variable.testclasses.Burritos;
 import spoon.test.variable.testclasses.Tacos;
 
 import static spoon.testing.utils.ModelUtils.build;
-import static spoon.testing.utils.ModelUtils.canBeBuilt;
 
 public class AccessFullyQualifiedFieldTest {
 	@Test
@@ -19,16 +14,4 @@ public class AccessFullyQualifiedFieldTest {
 
 		MainTest.checkAssignmentContracts(factory.Package().getRootPackage());
 	}
-
-	@Test
-	public void testNoFQN() throws Exception {
-		// contract: no fully qualified name if top package is shadowed by a local variable
-		Launcher spoon = new Launcher();
-		spoon.addInputResource("src/test/java/spoon/test/variable/testclasses/Burritos.java");
-		String output = "target/spooned-" + this.getClass().getSimpleName()+"/";
-		spoon.setSourceOutputDirectory(output);
-		spoon.run();
-		canBeBuilt(output, 7);
-	}
-
 }
