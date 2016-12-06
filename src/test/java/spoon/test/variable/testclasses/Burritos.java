a0a1329b7b8a0bc50535247bc6e034d94722f856
Simon Urli <simon.urli@inria.fr>
2016-12-06 10:06:12 +0100

    WiP #980 New tests and scanner for variables

--- src/test/java/spoon/test/variable/testclasses/Burritos.java
+++ src/test/java/spoon/test/variable/testclasses/Burritos.java
@@ -4,8 +4,16 @@ package spoon.test.variable.testclasses;
 import static spoon.Launcher.SPOONED_CLASSES;
 
 public class Burritos {
+	static void toto() {}
+
 	void foo() {
 		Object spoon = null;
-		Object xx = SPOONED_CLASSES; // cannot be written spoon.o, has to be with implicit visibility or static import
+		Object x = SPOONED_CLASSES; // cannot be written spoon.o, has to be with implicit visibility or static import
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Burritos.toto();
+			}
+		});
 	}
 }
