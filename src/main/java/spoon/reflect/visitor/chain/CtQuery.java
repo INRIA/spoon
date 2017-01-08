ff8a250945cb645c496161e2f97e074897a751f6
Pavel Vojtechovsky <pvojtechovsky@users.noreply.github.com>
2017-01-08 18:27:41 +0100

    add useless generic to pass the test

--- src/main/java/spoon/reflect/visitor/chain/CtQuery.java
+++ src/main/java/spoon/reflect/visitor/chain/CtQuery.java
@@ -52,7 +52,7 @@ public interface CtQuery extends CtQueryable {
 	 * @param input
 	 * @return this to support fluent API
 	 */
-	CtQuery setInput(Object... input);
+	<T extends CtQuery> T setInput(Object... input);
 
 	/**
 	 * actually evaluates the query and for each produced outputElement calls `consumer.accept(outputElement)`
