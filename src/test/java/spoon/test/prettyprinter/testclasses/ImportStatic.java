24707554aad815f68d78c8b8d9d626523729961c
Simon Urli <simon.urli@inria.fr>
2017-01-30 15:56:20 +0100

    Expose bug related in #1152

---
+++ src/test/java/spoon/test/prettyprinter/testclasses/ImportStatic.java
@@ -0,0 +1,12 @@
+package spoon.test.prettyprinter.testclasses;
+
+import spoon.test.prettyprinter.testclasses.sub.Constants;
+
+/**
+ * Created by urli on 30/01/2017.
+ */
+public class ImportStatic {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Constants.READY);
+    }
+}
---
+++ src/test/java/spoon/test/prettyprinter/testclasses/sub/Constants.java
@@ -0,0 +1,10 @@
+package spoon.test.prettyprinter.testclasses.sub;
+
+/**
+ * Created by urli on 30/01/2017.
+ */
+public class Constants {
+    public static int READY = 0x1;
+    public static int RUNNING = 0x2;
+}
+
