4644292da3c9fed77b89a3fefcd7287402c310ab
Pavel Vojtechovsky <p.vojtechovsky@post.cz>
2017-02-18 15:02:15 +0100

    fix CtTypeReferenceImpl#canAccess of protected and interface members

--- src/test/java/spoon/test/imports/testclasses/internal/SuperClass.java
+++ src/test/java/spoon/test/imports/testclasses/internal/SuperClass.java
@@ -3,4 +3,22 @@ package spoon.test.imports.testclasses.internal;
 class SuperClass {
 	protected class InnerClassProtected {
 	}
+	interface PackageProtectedInterface {
+		interface NestedOfPackageProtectedInterface {
+		}
+		public interface NestedPublicInterface {
+		}
+	}
+	protected interface ProtectedInterface {
+		interface NestedOfProtectedInterface {
+		}
+		public interface NestedPublicInterface {
+		}
+	}
+	public interface PublicInterface {
+		interface NestedOfPublicInterface {
+		}
+		public interface NestedPublicInterface {
+		}
+	}
 }
