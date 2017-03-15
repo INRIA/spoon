561853c421c1e8fc6b3cdd3b3837bf8c2940020a
Simon Urli <simon.urli@gmail.com>
2017-03-15 14:13:01 +0100

    fix: fix wrong behavior for special cases in removeStatement. Closes #1221. (#1224)

--- src/main/java/spoon/support/reflect/code/CtBlockImpl.java
+++ src/main/java/spoon/support/reflect/code/CtBlockImpl.java
@@ -190,7 +190,23 @@ public class CtBlockImpl<R> extends CtStatementImpl implements CtBlock<R> {
 	@Override
 	public void removeStatement(CtStatement statement) {
 		if (this.statements != CtElementImpl.<CtStatement>emptyList()) {
-			this.statements.remove(statement);
+
+			boolean hasBeenRemoved = false;
+			// we cannot use a remove(statement) as it uses the equals
+			// and a block can have twice exactly the same statement.
+			for (int i = 0; i < this.statements.size(); i++) {
+				if (this.statements.get(i) == statement) {
+					this.statements.remove(i);
+					hasBeenRemoved = true;
+					break;
+				}
+			}
+
+			// in case we use it with a statement manually built
+			if (!hasBeenRemoved) {
+				this.statements.remove(statement);
+			}
+
 			if (isImplicit() && statements.size() == 0) {
 				setImplicit(false);
 			}
---
+++ src/test/java/spoon/test/ctBlock/testclasses/Toto.java
@@ -0,0 +1,16 @@
+package spoon.test.ctBlock.testclasses;
+
+/**
+ * Created by urli on 15/03/2017.
+ */
+public class Toto {
+
+    public void foo() {
+        int i = 1;
+        i++;
+        if (i > 0) {
+            java.lang.System.out.println("test");
+        }
+        i++;
+    }
+}
