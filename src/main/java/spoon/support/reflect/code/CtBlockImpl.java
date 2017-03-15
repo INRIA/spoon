cb1500bde6bb8ce8e9f2e17be3541101881904e2
Simon Urli <simon.urli@inria.fr>
2017-03-15 13:14:44 +0100

    Add a new case in case of a statement is manually built or clone.

--- src/main/java/spoon/support/reflect/code/CtBlockImpl.java
+++ src/main/java/spoon/support/reflect/code/CtBlockImpl.java
@@ -191,14 +191,22 @@ public class CtBlockImpl<R> extends CtStatementImpl implements CtBlock<R> {
 	public void removeStatement(CtStatement statement) {
 		if (this.statements != CtElementImpl.<CtStatement>emptyList()) {
 
+			boolean hasBeenRemoved = false;
 			// we cannot use a remove(statement) as it uses the equals
 			// and a block can have twice exactly the same statement.
 			for (int i = 0; i < this.statements.size(); i++) {
 				if (this.statements.get(i) == statement) {
 					this.statements.remove(i);
+					hasBeenRemoved = true;
 					break;
 				}
 			}
+
+			// in case we use it with a statement manually built
+			if (!hasBeenRemoved) {
+				this.statements.remove(statement);
+			}
+
 			if (isImplicit() && statements.size() == 0) {
 				setImplicit(false);
 			}
