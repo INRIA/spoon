f190d8ebcdcfdba60a5490a5a934599fd93b159d
Pavel Vojtechovsky <pvojtechovsky@users.noreply.github.com>
2017-03-22 21:35:37 +0100

    refactor: CtFormalTypeDeclarer extends CtTypeMember (#1235)

--- src/main/java/spoon/reflect/declaration/CtFormalTypeDeclarer.java
+++ src/main/java/spoon/reflect/declaration/CtFormalTypeDeclarer.java
@@ -22,7 +22,7 @@ import java.util.List;
  * This abstract element defines a declaration that accepts formal type
  * parameters (aka generics), such as a CtType (<code>class A&lt;E&gt;</code>), CtMethod or CtConstructor.
  */
-public interface CtFormalTypeDeclarer extends CtElement {
+public interface CtFormalTypeDeclarer extends CtTypeMember {
 
 	/**
 	 * Returns the formal type parameters of this generic element.
