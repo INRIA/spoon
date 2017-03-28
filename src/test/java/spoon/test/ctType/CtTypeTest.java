1c5d383667c7e2bd5fe5381a43a7abb55de5e892
Pavel Vojtechovsky <p.vojtechovsky@post.cz>
2017-03-28 21:02:51 +0200

    refactor CtTypeReference#isSubtypeOf uses CtSuperTypeHierarchy

--- src/test/java/spoon/test/ctType/CtTypeTest.java
+++ src/test/java/spoon/test/ctType/CtTypeTest.java
@@ -2,17 +2,29 @@ package spoon.test.ctType;
 
 import org.junit.Test;
 import spoon.Launcher;
+import spoon.reflect.code.CtAssignment;
+import spoon.reflect.code.CtComment;
+import spoon.reflect.code.CtLocalVariable;
+import spoon.reflect.code.CtVariableAccess;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtInterface;
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtNamedElement;
 import spoon.reflect.declaration.CtType;
-import spoon.reflect.declaration.CtTypeMember;
 import spoon.reflect.declaration.CtTypeParameter;
 import spoon.reflect.factory.Factory;
+import spoon.reflect.reference.CtReference;
 import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.visitor.filter.NameFilter;
+import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.test.ctType.testclasses.X;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -84,27 +96,98 @@ public class CtTypeTest {
 		//using CtType implementation
 		assertTrue(xCtType.isSubtypeOf(xCtType.getReference()));
 	}
-
+	
 	@Test
 	public void testIsSubTypeOfonTypeParameters() throws Exception {
 		CtType<X> xCtType = buildClass(X.class);
+		Factory factory = xCtType.getFactory();
 
-		CtType<?> oCtType = xCtType.getFactory().Type().get("spoon.test.ctType.testclasses.O");
+		CtType<?> oCtType = factory.Type().get("spoon.test.ctType.testclasses.O");
+		CtType<?> pCtType = factory.Type().get("spoon.test.ctType.testclasses.P");
+		CtTypeReference<?> objectCtTypeRef = factory.Type().OBJECT;
 
-		List<CtTypeParameter> typeParameters = oCtType.getFormalCtTypeParameters();
-		assertTrue(typeParameters.size() == 1);
+		List<CtTypeParameter> oTypeParameters = oCtType.getFormalCtTypeParameters();
+		assertTrue(oTypeParameters.size() == 1);
+		List<CtTypeParameter> pTypeParameters = pCtType.getFormalCtTypeParameters();
+		assertTrue(pTypeParameters.size() == 2);
 
-		CtType<?> aCtType = typeParameters.get(0);
+		CtType<?> O_A_CtType = oTypeParameters.get(0);
+		CtType<?> P_D_CtType = pTypeParameters.get(0);
+		CtType<?> P_F_CtType = pTypeParameters.get(1);
 
-		List<CtMethod<?>> methods = oCtType.getMethodsByName("foo");
+		CtMethod<?> O_FooMethod = oCtType.filterChildren(new NameFilter<>("foo")).first();
+		CtMethod<?> P_FooMethod = pCtType.filterChildren(new NameFilter<>("foo")).first();
 
-		assertTrue(methods.size() == 1);
+		CtType<?> O_B_CtType = O_FooMethod.getType().getDeclaration();
+		CtType<?> P_E_CtType = P_FooMethod.getType().getDeclaration();
 
-		CtMethod<?> fooMethod = methods.get(0);
-		CtType<?> bCtType = fooMethod.getType().getDeclaration();
+		assertTrue(O_B_CtType.isSubtypeOf(xCtType.getReference()));
+		assertTrue(O_B_CtType.isSubtypeOf(O_A_CtType.getReference()));
+		
+		assertTrue(P_E_CtType.isSubtypeOf(xCtType.getReference()));
+		assertTrue(P_E_CtType.isSubtypeOf(P_D_CtType.getReference()));
+		assertTrue(P_E_CtType.isSubtypeOf(O_A_CtType.getReference()));
+		
+		assertTrue(P_D_CtType.isSubtypeOf(O_A_CtType.getReference()));
+		assertTrue(P_E_CtType.isSubtypeOf(O_B_CtType.getReference()));
 
-		//The TypeParameters are not supported yet
-//		assertTrue(bCtType.isSubtypeOf(xCtType.getReference()));
-//		assertTrue(bCtType.isSubtypeOf(aCtType.getReference()));
+		assertTrue(P_E_CtType.isSubtypeOf(objectCtTypeRef));
+		assertTrue(P_F_CtType.isSubtypeOf(objectCtTypeRef));
+	}
+	
+	@Test
+	public void testIsSubTypeOfonTypeReferences() throws Exception {
+		final Launcher launcher = new Launcher();
+		launcher.setArgs(new String[]{"-c"});
+		launcher.addInputResource("./src/test/java/spoon/test/ctType/testclasses/X.java");
+		launcher.buildModel();
+		Factory factory = launcher.getFactory();
+		
+		CtType<?> oCtType = factory.Class().get("spoon.test.ctType.testclasses.O");
+		CtMethod<?> O_FooMethod = oCtType.filterChildren(new NameFilter<>("foo")).first();
+
+		Map<String, CtTypeReference<?>> nameToTypeRef = new HashMap<>();
+		O_FooMethod.filterChildren(new TypeFilter<>(CtLocalVariable.class)).forEach((CtLocalVariable var)->{
+			nameToTypeRef.put(var.getSimpleName(), var.getType());
+		});
+		
+		int[] count = new int[1];
+
+		O_FooMethod.filterChildren(new TypeFilter<>(CtAssignment.class)).forEach((CtAssignment ass)->{
+			for (CtComment comment : ass.getComments()) {
+				checkIsNotSubtype(comment, nameToTypeRef);
+				count[0]++;
+			};
+			count[0]++;
+			checkIsSubtype(((CtVariableAccess) ass.getAssigned()).getVariable().getType(), ((CtVariableAccess) ass.getAssignment()).getVariable().getType(), nameToTypeRef);
+		});
+		
+		assertTrue(count[0]>(9*8));
+	}
+
+	private void checkIsSubtype(CtTypeReference superType, CtTypeReference subType, Map<String, CtTypeReference<?>> nameToTypeRef) {
+		String msg = getTypeName(subType)+" isSubTypeOf "+getTypeName(superType);
+		assertTrue(msg, subType.isSubtypeOf(superType));
+	}
+
+	private static final Pattern assignment = Pattern.compile("\\s*(\\w+)\\s*=\\s*(\\w+);");
+	private void checkIsNotSubtype(CtComment comment, Map<String, CtTypeReference<?>> nameToTypeRef) {
+		Matcher m = assignment.matcher(comment.getContent());
+		assertTrue(m.matches());
+		CtTypeReference<?> superType = nameToTypeRef.get(m.group(1));
+		CtTypeReference<?> subType = nameToTypeRef.get(m.group(2));
+		String msg = getTypeName(subType)+" is NOT SubTypeOf "+getTypeName(superType);
+		assertFalse(msg, subType.isSubtypeOf(superType));
+	}
+	
+	private String getTypeName(CtTypeReference<?> ref) {
+		String name;
+		CtReference r= ref.getParent(CtReference.class);
+		if(r!=null) {
+			name = r.getSimpleName();
+		} else {
+			name = ref.getParent(CtNamedElement.class).getSimpleName();
+		}
+		return ref.toString()+" "+name;
 	}
 }
