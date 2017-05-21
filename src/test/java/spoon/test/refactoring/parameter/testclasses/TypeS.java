82066543a2b4a17619483778a763f39399718c04
Pavel Vojtechovsky <p.vojtechovsky@post.cz>
2017-05-21 18:34:56 +0200

    test CtParameterRemoveRefactoring

--- src/test/java/spoon/test/refactoring/parameter/testclasses/TypeS.java
+++ src/test/java/spoon/test/refactoring/parameter/testclasses/TypeS.java
@@ -21,4 +21,16 @@ public class TypeS extends TypeR {
 		};
 		ifaceT.method1(1.0);
 	}
+
+	private void methodWithComplexExpression_R() {
+		IFaceT ifaceT = p->{
+			@TestHierarchy("R_method1")
+			int x;
+		};
+		/*
+		 * the refactoring should by default report calling of method as problem, because it cannot know,
+		 * whether that method call has side effects. Such method call remove might cause malfunction.
+		 */
+		ifaceT.method1(Math.abs(1.0));
+	}
 }
