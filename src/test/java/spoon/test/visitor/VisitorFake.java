9f0ff0254ab70e28db4c60f5a5578d5273f20914
Martin Monperrus <monperrus@users.noreply.github.com>
2017-05-29 17:19:27 +0200

    refactor: renamed confusing duplicate classes (#1333)

---
+++ src/test/java/spoon/test/visitor/VisitorFake.java
@@ -0,0 +1,14 @@
+package spoon.test.visitor;
+
+import spoon.reflect.visitor.CtAbstractVisitor;
+
+/**
+ * Created by nicolas on 12/01/2015.
+ */
+public class VisitorFake extends CtAbstractVisitor {
+	/**
+	 * This visitor implementation should always be empty :
+	 *
+	 * the compiler ensures that we have correctly defined all visit method in CtAbstractVisitor.
+	 */
+}
