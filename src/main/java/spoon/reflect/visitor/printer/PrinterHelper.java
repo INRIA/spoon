24c5242397bddd4db0dde0c572b82fc036ac8156
Benjamin DANGLOT <benjamin.danglot@inria.fr>
2017-06-12 14:44:33 +0200

    fix: Bug with escaped literal (#1394)
    * test case that reproduce the bug
    
    * fix #1393: spoon handle octal code value
    
    * add test case
    
    * fix checkstyle
    
    * fix license

--- src/main/java/spoon/reflect/visitor/printer/PrinterHelper.java
+++ src/main/java/spoon/reflect/visitor/printer/PrinterHelper.java
@@ -135,6 +135,7 @@ public class PrinterHelper {
 		nbTabs = tabCount;
 		return this;
 	}
+
 	public boolean removeLine() {
 		String ls = LINE_SEPARATOR;
 		int i = sbf.length() - ls.length();
@@ -229,26 +230,26 @@ public class PrinterHelper {
 	 */
 	public void preWriteUnaryOperator(UnaryOperatorKind o) {
 		switch (o) {
-		case POS:
-			write("+");
-			break;
-		case NEG:
-			write("-");
-			break;
-		case NOT:
-			write("!");
-			break;
-		case COMPL:
-			write("~");
-			break;
-		case PREINC:
-			write("++");
-			break;
-		case PREDEC:
-			write("--");
-			break;
-		default:
-			// do nothing (this does not feel right to ignore invalid ops)
+			case POS:
+				write("+");
+				break;
+			case NEG:
+				write("-");
+				break;
+			case NOT:
+				write("!");
+				break;
+			case COMPL:
+				write("~");
+				break;
+			case PREINC:
+				write("++");
+				break;
+			case PREDEC:
+				write("--");
+				break;
+			default:
+				// do nothing (this does not feel right to ignore invalid ops)
 		}
 	}
 
@@ -257,14 +258,14 @@ public class PrinterHelper {
 	 */
 	public void postWriteUnaryOperator(UnaryOperatorKind o) {
 		switch (o) {
-		case POSTINC:
-			write("++");
-			break;
-		case POSTDEC:
-			write("--");
-			break;
-		default:
-			// do nothing (this does not feel right to ignore invalid ops)
+			case POSTINC:
+				write("++");
+				break;
+			case POSTDEC:
+				write("--");
+				break;
+			default:
+				// do nothing (this does not feel right to ignore invalid ops)
 		}
 	}
 
@@ -273,118 +274,122 @@ public class PrinterHelper {
 	 */
 	public PrinterHelper writeOperator(BinaryOperatorKind o) {
 		switch (o) {
-		case OR:
-			write("||");
-			break;
-		case AND:
-			write("&&");
-			break;
-		case BITOR:
-			write("|");
-			break;
-		case BITXOR:
-			write("^");
-			break;
-		case BITAND:
-			write("&");
-			break;
-		case EQ:
-			write("==");
-			break;
-		case NE:
-			write("!=");
-			break;
-		case LT:
-			write("<");
-			break;
-		case GT:
-			write(">");
-			break;
-		case LE:
-			write("<=");
-			break;
-		case GE:
-			write(">=");
-			break;
-		case SL:
-			write("<<");
-			break;
-		case SR:
-			write(">>");
-			break;
-		case USR:
-			write(">>>");
-			break;
-		case PLUS:
-			write("+");
-			break;
-		case MINUS:
-			write("-");
-			break;
-		case MUL:
-			write("*");
-			break;
-		case DIV:
-			write("/");
-			break;
-		case MOD:
-			write("%");
-			break;
-		case INSTANCEOF:
-			write("instanceof");
-			break;
+			case OR:
+				write("||");
+				break;
+			case AND:
+				write("&&");
+				break;
+			case BITOR:
+				write("|");
+				break;
+			case BITXOR:
+				write("^");
+				break;
+			case BITAND:
+				write("&");
+				break;
+			case EQ:
+				write("==");
+				break;
+			case NE:
+				write("!=");
+				break;
+			case LT:
+				write("<");
+				break;
+			case GT:
+				write(">");
+				break;
+			case LE:
+				write("<=");
+				break;
+			case GE:
+				write(">=");
+				break;
+			case SL:
+				write("<<");
+				break;
+			case SR:
+				write(">>");
+				break;
+			case USR:
+				write(">>>");
+				break;
+			case PLUS:
+				write("+");
+				break;
+			case MINUS:
+				write("-");
+				break;
+			case MUL:
+				write("*");
+				break;
+			case DIV:
+				write("/");
+				break;
+			case MOD:
+				write("%");
+				break;
+			case INSTANCEOF:
+				write("instanceof");
+				break;
 		}
 		return this;
+	}
+
+	public void writeCharLiteral(Character c, boolean mayContainsSpecialCharacter) {
+		if (!mayContainsSpecialCharacter) {
+			write(c);
+		} else if (Character.UnicodeBlock.of(c) != Character.UnicodeBlock.BASIC_LATIN) {
+			if (c < 0x10) {
+				write("\\u000" + Integer.toHexString(c));
+			} else if (c < 0x100) {
+				write("\\u00" + Integer.toHexString(c));
+			} else if (c < 0x1000) {
+				write("\\u0" + Integer.toHexString(c));
+			} else {
+				write("\\u" + Integer.toHexString(c));
+			}
+		} else {
+			switch (c) {
+				case '\b':
+					write("\\b"); //$NON-NLS-1$
+					break;
+				case '\t':
+					write("\\t"); //$NON-NLS-1$
+					break;
+				case '\n':
+					write("\\n"); //$NON-NLS-1$
+					break;
+				case '\f':
+					write("\\f"); //$NON-NLS-1$
+					break;
+				case '\r':
+					write("\\r"); //$NON-NLS-1$
+					break;
+				case '\"':
+					write("\\\""); //$NON-NLS-1$
+					break;
+				case '\'':
+					write("\\'"); //$NON-NLS-1$
+					break;
+				case '\\': // take care not to display the escape as a potential
+					// real char
+					write("\\\\"); //$NON-NLS-1$
+					break;
+				default:
+					write(Character.isISOControl(c) ? String.format("\\u%04x", (int) c) : Character.toString(c));
+			}
+		}
 	}
 
 	public void writeStringLiteral(String value, boolean mayContainsSpecialCharacter) {
 		if (!mayContainsSpecialCharacter) {
 			write(value);
-			return;
-		}
-		// handle some special char.....
-		for (int i = 0; i < value.length(); i++) {
-			char c = value.charAt(i);
-			if (Character.UnicodeBlock.of(c) != Character.UnicodeBlock.BASIC_LATIN) {
-				if (c < 0x10) {
-					write("\\u000" + Integer.toHexString(c));
-				} else if (c < 0x100) {
-					write("\\u00" + Integer.toHexString(c));
-				} else if (c < 0x1000) {
-					write("\\u0" + Integer.toHexString(c));
-				} else {
-					write("\\u" + Integer.toHexString(c));
-				}
-				continue;
-			}
-			switch (c) {
-			case '\b':
-				write("\\b"); //$NON-NLS-1$
-				break;
-			case '\t':
-				write("\\t"); //$NON-NLS-1$
-				break;
-			case '\n':
-				write("\\n"); //$NON-NLS-1$
-				break;
-			case '\f':
-				write("\\f"); //$NON-NLS-1$
-				break;
-			case '\r':
-				write("\\r"); //$NON-NLS-1$
-				break;
-			case '\"':
-				write("\\\""); //$NON-NLS-1$
-				break;
-			case '\'':
-				write("\\'"); //$NON-NLS-1$
-				break;
-			case '\\': // take care not to display the escape as a potential
-				// real char
-				write("\\\\"); //$NON-NLS-1$
-				break;
-			default:
-				write(value.charAt(i));
+		} else {
+			for (int i = 0; i < value.length(); i++) {
+				writeCharLiteral(value.charAt(i), mayContainsSpecialCharacter);
 			}
 		}
 	}
@@ -399,10 +404,12 @@ public class PrinterHelper {
 	}
 
 	private ArrayDeque<Integer> lengths = new ArrayDeque<>();
+
 	/** stores the length of the printer */
 	public void snapshotLength() {
 		lengths.addLast(toString().length());
 	}
+
 	/** returns true if something has been written since the last call to snapshotLength() */
 	public boolean hasNewContent() {
 		return lengths.pollLast() < toString().length();
---
+++ src/test/java/spoon/test/literal/testclasses/EscapedLiteral.java
@@ -0,0 +1,22 @@
+package spoon.test.literal.testclasses;
+
+public class EscapedLiteral {
+
+  char c1 = '\0';
+
+  char c2 = '\7';
+
+  char c3 = '\77';
+
+  char c4 = '\177';
+
+  char c5 = '\277';
+
+  char c6 = '\377';
+
+  char c7 = '\u0000';
+
+  char c8 = '\u0001';
+
+  char c9 = '\u0002';
+}
