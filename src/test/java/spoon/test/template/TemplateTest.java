7f2431ef05bceb8399ac485d01a3b171bd3ec48d
Pavel Vojtechovsky <p.vojtechovsky@post.cz>
2017-07-24 17:25:01 +0200

    add GeneratedByMember required by new feature

--- src/test/java/spoon/test/template/TemplateTest.java
+++ src/test/java/spoon/test/template/TemplateTest.java
@@ -201,6 +201,8 @@ public class TemplateTest {
 		// contract; field access is handled same like local variable access
 		CtMethod<?> methodWithFieldAccess = subc.getElements(
 				new NameFilter<CtMethod<?>>("methodWithFieldAccess")).get(0);
+		elementToGeneratedByMember.put(methodWithFieldAccess, "#methodWithFieldAccess");
+		elementToGeneratedByMember.put(subc.getField("newVarName"), "#var");
 
 		// contract: field write are replaced by field write with modified field name
 		assertEquals("newVarName = o", methodWithFieldAccess.getBody().getStatement(2).toString());
