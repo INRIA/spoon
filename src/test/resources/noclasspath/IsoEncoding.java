217b7a1e5092c9d1dd3469ac6f57a635c3bb66e8
Simon Urli <simon.urli@inria.fr>
2017-08-18 12:11:52 +0200

    Add a new test to check encoding

---
+++ src/test/resources/noclasspath/IsoEncoding.java
@@ -0,0 +1,13 @@
+package my.pack;
+
+public class IsoEncoding {
+
+    private String entrée;
+    public IsoEncoding(String maChaîne) {
+        this.entrée = maChaîne;
+    }
+
+    private String récupérerUneEntrée() {
+        return this.entrée;
+    }
+}
