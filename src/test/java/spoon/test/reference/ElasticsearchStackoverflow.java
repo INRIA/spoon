899562de9af5c8cf81278944c094e5d3e9e61073
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2017-09-04 14:48:33 +0200

    Added an example that causes a stack overflow when calling getDeclaration.

---
+++ src/test/java/spoon/test/reference/ElasticsearchStackoverflow.java
@@ -0,0 +1,32 @@
+package spoon.test.reference;
+
+import org.junit.Test;
+import spoon.Launcher;
+import spoon.reflect.CtModel;
+import spoon.reflect.reference.CtExecutableReference;
+import spoon.reflect.visitor.CtScanner;
+
+public class ElasticsearchStackoverflow {
+
+	private class Scanner extends CtScanner {
+		@Override
+		public <T> void visitCtExecutableReference(
+				CtExecutableReference<T> reference) {
+			super.visitCtExecutableReference(reference);
+			reference.getDeclaration();
+		}
+	}
+
+	@Test
+	public void testStackOverflow() {
+		Launcher launcher = new Launcher();
+		launcher.addInputResource("./src/test/resources/noclasspath/elasticsearch-stackoverflow");
+		launcher.getEnvironment().setNoClasspath(true);
+		launcher.buildModel();
+
+		CtModel model = launcher.getModel();
+		Scanner scanner = new Scanner();
+		scanner.scan(model.getRootPackage());
+	}
+
+}
