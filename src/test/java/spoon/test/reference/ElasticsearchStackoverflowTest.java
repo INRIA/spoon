099eb254cb8e2e758c8d77cd441fede65f02b46b
Simon Urli <simon.urli@inria.fr>
2017-09-05 10:40:57 +0200

    Improve test

--- src/test/java/spoon/test/reference/ElasticsearchStackoverflowTest.java
+++ src/test/java/spoon/test/reference/ElasticsearchStackoverflowTest.java
@@ -3,8 +3,22 @@ package spoon.test.reference;
 import org.junit.Test;
 import spoon.Launcher;
 import spoon.reflect.CtModel;
+import spoon.reflect.declaration.CtExecutable;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
 import spoon.reflect.reference.CtExecutableReference;
+import spoon.reflect.reference.CtTypeParameterReference;
+import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.CtScanner;
+import spoon.reflect.visitor.filter.NamedElementFilter;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class ElasticsearchStackoverflowTest {
 
@@ -27,6 +41,27 @@ public class ElasticsearchStackoverflowTest {
 		CtModel model = launcher.getModel();
 		Scanner scanner = new Scanner();
 		scanner.scan(model.getRootPackage());
+
+		List<CtExecutableReference> executables = launcher.getModel().getElements(new TypeFilter<CtExecutableReference>(CtExecutableReference.class));
+		assertFalse(executables.isEmpty());
+
+		boolean result = false;
+		for (CtExecutableReference execRef : executables) {
+			if (execRef.getSimpleName().equals("setParentTask")) {
+				CtTypeReference typeRef = execRef.getDeclaringType();
+				assertTrue(typeRef instanceof CtTypeParameterReference);
+				assertEquals("ShardRequest", typeRef.getSimpleName());
+
+				CtType typeRefDecl = typeRef.getDeclaration();
+				assertEquals("BroadcastShardRequest", typeRefDecl.getSuperclass().getSimpleName());
+
+				assertNull(execRef.getDeclaration());
+				result = true;
+			}
+		}
+
+		assertTrue(result);
+
 	}
 
 }
