79f0588ce452636d7c755708317a90dc8e1547f7
tiagodrcarvalho <tiagodrcarvalho@gmail.com>
2017-09-06 20:50:13 +0200

    Fixes tabs

--- src/main/java/spoon/support/compiler/jdt/JDTBasedSpoonCompiler.java
+++ src/main/java/spoon/support/compiler/jdt/JDTBasedSpoonCompiler.java
@@ -515,10 +515,10 @@ public class JDTBasedSpoonCompiler implements spoon.SpoonModelBuilder {
 
 				// the path must be given relatively to to the working directory
 				try (InputStream is = getCompilationUnitInputStream(cu.getFile().getPath());
-                        		FileOutputStream outFile = new FileOutputStream(file);) {
-
-                    			IOUtils.copy(is, outFile);
-                		}
+					FileOutputStream outFile = new FileOutputStream(file);) {
+					
+					IOUtils.copy(is, outFile);
+				}
 
 				if (!printedFiles.contains(file)) {
 					printedFiles.add(file);
