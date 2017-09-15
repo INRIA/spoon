4e51132b81fca4628214d7000780e809e35b604d
tdurieux <durieuxthomas@hotmail.com>
2017-09-15 21:10:37 +0200

    fix test

--- src/test/java/spoon/MavenLauncherTest.java
+++ src/test/java/spoon/MavenLauncherTest.java
@@ -8,13 +8,13 @@ public class MavenLauncherTest {
 	@Test
 	public void spoonMavenLauncherTest() {
 		// without the tests
-		MavenLauncher launcher = new MavenLauncher("./", false);
+		MavenLauncher launcher = new MavenLauncher("./", MavenLauncher.SOURCE_TYPE.SOURCE);
 		assertEquals(10, launcher.getEnvironment().getSourceClasspath().length);
 		// 54 because of the sub folder
 		assertEquals(54, launcher.getModelBuilder().getInputSources().size());
 
 		// with the tests
-		launcher = new MavenLauncher("./", true);
+		launcher = new MavenLauncher("./", MavenLauncher.SOURCE_TYPE.ALL);
 		assertEquals(10, launcher.getEnvironment().getSourceClasspath().length);
 		// 54 because of the sub folder
 		assertEquals(235, launcher.getModelBuilder().getInputSources().size());
