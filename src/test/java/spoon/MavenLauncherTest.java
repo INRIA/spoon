1ccbd5af5d827ef5d6e13512fc134cde5f26a3a8
tdurieux <durieuxthomas@hotmail.com>
2017-09-19 07:55:57 +0200

    fix comment in test

--- src/test/java/spoon/MavenLauncherTest.java
+++ src/test/java/spoon/MavenLauncherTest.java
@@ -13,13 +13,13 @@ public class MavenLauncherTest {
 		assertEquals(8, launcher.getEnvironment().getComplianceLevel());
 
 		assertEquals(10, launcher.getEnvironment().getSourceClasspath().length);
-		// 54 because of the sub folder
+		// 54 because of the sub folders of src/main/java
 		assertEquals(54, launcher.getModelBuilder().getInputSources().size());
 
 		// with the tests
 		launcher = new MavenLauncher("./", MavenLauncher.SOURCE_TYPE.ALL);
 		assertEquals(10, launcher.getEnvironment().getSourceClasspath().length);
-		// 54 because of the sub folder
+		// 235 because of the sub folders of src/main/java and src/test/java
 		assertEquals(235, launcher.getModelBuilder().getInputSources().size());
 	}
 
