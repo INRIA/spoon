b7d0d6882c24431bf8dddc81c7e854f69e5b4a72
Simon Urli <simon.urli@inria.fr>
2017-10-26 13:50:33 +0200

    Remove the stage, run jobs in parallel

--- chore/travis-run-coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script intends to be run on TravisCI
-# It only computes and publish (through coveralls) the coverage of Spoon
-
-mvn -Djava.src.version=$JDK_VERSION test jacoco:report coveralls:report -Pcoveralls --fail-never
