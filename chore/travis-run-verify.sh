a3f5e0fec3b352ecd338d2cabd18f84c67924aaa
Simon Urli <simon.urli@inria.fr>
2017-10-26 14:19:53 +0200

    Fix path

--- chore/travis-run-verify.sh
+++ chore/travis-run-verify.sh
@@ -4,4 +4,4 @@
 # it runs verify and site maven goals
 # and to check documentation links
 
-mvn -Djava.src.version=$JDK_VERSION verify site install -DskipTests && python check-links-in-doc.py
+mvn -Djava.src.version=$JDK_VERSION verify site install -DskipTests && python ./chore/check-links-in-doc.py
