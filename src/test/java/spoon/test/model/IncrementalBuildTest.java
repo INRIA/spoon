d33e9b6ed7d04fa4a2821a11336a645ab2c058cb
Egor18 <egorbr1801@mail.ru>
2017-11-03 20:45:16 +0300

    Add incremental build test

---
+++ src/test/java/spoon/test/model/IncrementalBuildTest.java
@@ -0,0 +1,20 @@
+package spoon.test.model;
+import org.junit.Test;
+
+import spoon.Launcher;
+import spoon.OutputType;
+
+public class IncrementalBuildTest {
+	@Test
+	public void testIncrementalBuild() {
+		Launcher launcher = new Launcher();
+		launcher.addInputResource("./src/test/resources/incremental/in");
+		launcher.getEnvironment().setNoClasspath(true);
+		launcher.getEnvironment().setCommentEnabled(true);
+		launcher.createCompiler();
+		launcher.setSourceOutputDirectory("./src/test/resources/incremental/spooned");
+		launcher.getModelBuilder().setBuildOnlyOutdatedFiles(true);
+		launcher.buildModel();
+		launcher.getModelBuilder().generateProcessedSourceFiles(OutputType.COMPILATION_UNITS);
+	}
+}
---
+++ src/test/resources/incremental/in/HelloWorld.java
@@ -0,0 +1,6 @@
+
+public class HelloWorld {
+	public static void hello() {
+		System.out.println("Hello, World");
+	}
+}
