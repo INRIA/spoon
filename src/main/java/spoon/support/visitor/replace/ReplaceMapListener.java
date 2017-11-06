b38c8a0449626d6e410337b27a70f49aeb22e228
Pavel Vojtechovsky <pvojtechovsky@users.noreply.github.com>
2017-11-06 13:12:24 +0100

    refactor: move package `spoon.generating.*` from src/main to src/test (#1647)
    fix #1638

--- src/main/java/spoon/support/visitor/replace/ReplaceMapListener.java
+++ src/main/java/spoon/support/visitor/replace/ReplaceMapListener.java
@@ -14,10 +14,10 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C license and that you accept its terms.
  */
-package spoon.generating.replace;
+package spoon.support.visitor.replace;
 
-import java.util.Set;
+import java.util.Map;
 
-public interface ReplaceSetListener<T extends Set> {
+public interface ReplaceMapListener<T extends Map> {
 	void set(T replace);
 }
