228dfaeb7e43abdf47090e9206ba712f8c37f838
Simon Urli <simon.urli@inria.fr>
2017-11-09 15:16:44 +0100

    SetVisibility should only be called with public/private/protected

--- src/test/java/spoon/test/modifiers/TestModifiers.java
+++ src/test/java/spoon/test/modifiers/TestModifiers.java
@@ -2,15 +2,18 @@ package spoon.test.modifiers;
 
 import org.junit.Test;
 import spoon.Launcher;
+import spoon.SpoonException;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.declaration.ModifierKind;
 import spoon.test.modifiers.testclasses.MethodVarArgs;
+import spoon.test.modifiers.testclasses.StaticMethod;
 
 import java.util.Collections;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class TestModifiers {
 
@@ -32,5 +35,28 @@ public class TestModifiers {
         spoon.addInputResource("./src/test/java/spoon/test/modifiers/testclasses/MethodVarArgs.java");
         spoon.getEnvironment().setShouldCompile(true);
         spoon.run();
+    }
+
+    @Test
+    public void testSetVisibility() {
+        // contract: setVisibility should only work with public/private/protected modifiers
+
+        Launcher spoon = new Launcher();
+        spoon.addInputResource("./src/test/java/spoon/test/modifiers/testclasses/StaticMethod.java");
+        spoon.buildModel();
+
+        CtType<?> myClass = spoon.getFactory().Type().get(StaticMethod.class);
+        CtMethod methodPublicStatic = myClass.getMethodsByName("maMethod").get(0);
+
+        assertEquals(ModifierKind.PUBLIC, methodPublicStatic.getVisibility());
+        methodPublicStatic.setVisibility(ModifierKind.PROTECTED);
+        assertEquals(ModifierKind.PROTECTED, methodPublicStatic.getVisibility());
+        try {
+            methodPublicStatic.setVisibility(ModifierKind.FINAL);
+            fail();
+        } catch (SpoonException e) {
+        }
+
+        assertEquals(ModifierKind.PROTECTED, methodPublicStatic.getVisibility());
     }
 }
