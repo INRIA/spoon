2c7c0b5396da1e32ab98bd8231ed0adb5b6b2c75
Simon Urli <simon.urli@inria.fr>
2017-11-09 18:50:19 +0100

    Fix checkstyle

--- src/main/java/spoon/reflect/reference/CtImport.java
+++ src/main/java/spoon/reflect/reference/CtImport.java
@@ -1,15 +1,31 @@
+/**
+ * Copyright (C) 2006-2017 INRIA and contributors
+ * Spoon - http://spoon.gforge.inria.fr/
+ *
+ * This software is governed by the CeCILL-C License under French law and
+ * abiding by the rules of distribution of free software. You can use, modify
+ * and/or redistribute the software under the terms of the CeCILL-C license as
+ * circulated by CEA, CNRS and INRIA at http://www.cecill.info.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the CeCILL-C License for more details.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
 package spoon.reflect.reference;
 
 import spoon.reflect.declaration.CtElement;
 
 public interface CtImport extends CtElement {
-    <T extends CtImport> T setKindImport(ImportKind importKind);
+	<T extends CtImport> T setKindImport(ImportKind importKind);
 
-    ImportKind getKindImport();
+	ImportKind getKindImport();
 
-    CtReference getReference();
+	CtReference getReference();
 
-    <T extends CtImport> T setReference(CtReference reference);
+	<T extends CtImport> T setReference(CtReference reference);
 
-    String getSimpleName();
+	String getSimpleName();
 }
