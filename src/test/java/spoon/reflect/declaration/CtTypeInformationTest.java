3ebe26793971c7914950c8eba82d86f8900b81df
Simon Urli <simon.urli@inria.fr>
2017-11-30 16:18:50 +0100

    Fix imports

--- src/test/java/spoon/reflect/declaration/CtTypeInformationTest.java
+++ src/test/java/spoon/reflect/declaration/CtTypeInformationTest.java
@@ -1,9 +1,17 @@
 package spoon.reflect.declaration;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static spoon.testing.utils.ModelUtils.build;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import spoon.Launcher;
+import spoon.reflect.declaration.testclasses.ExtendsArrayList;
+import spoon.reflect.declaration.testclasses.Subclass;
+import spoon.reflect.declaration.testclasses.Subinterface;
+import spoon.reflect.declaration.testclasses.TestInterface;
+import spoon.reflect.factory.Factory;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.support.visitor.ClassTypingContext;
 
 import java.lang.reflect.Field;
 import java.util.AbstractCollection;
@@ -11,24 +19,11 @@ import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.RandomAccess;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import spoon.Launcher;
-import spoon.reflect.declaration.testclasses.ExtendsArrayList;
-import spoon.reflect.declaration.testclasses.ExtendsObject;
-import spoon.reflect.declaration.testclasses.Subclass;
-import spoon.reflect.declaration.testclasses.Subinterface;
-import spoon.reflect.declaration.testclasses.TestInterface;
-import spoon.reflect.factory.Factory;
-import spoon.reflect.reference.CtTypeReference;
-import spoon.support.comparator.SignatureComparator;
-import spoon.support.util.SortedList;
-import spoon.support.visitor.ClassTypingContext;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static spoon.testing.utils.ModelUtils.build;
 
 public class CtTypeInformationTest {
 	private Factory factory;
