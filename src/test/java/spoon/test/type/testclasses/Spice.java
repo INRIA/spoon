855c50532e17bae7be0f13a14803827a7873ac45
Simon Urli <simon.urli@inria.fr>
2017-12-08 15:54:57 +0100

    Fix tests

--- src/test/java/spoon/test/type/testclasses/Spice.java
+++ src/test/java/spoon/test/type/testclasses/Spice.java
@@ -17,6 +17,10 @@
 
 package spoon.test.type.testclasses;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
 public @interface Spice {
 	Class<?> klass();
 }
