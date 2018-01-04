8e6ea43aa30767fbd29e37cca6294fda47ad6747
Pavel Vojtechovsky <pvojtechovsky@users.noreply.github.com>
2018-01-04 22:39:47 +0100

    feature: add methods getRoleHandlers(Class), forEachRoleHandler, getRoleHandlerWrtParent in RoleHandlerHelper (#1794)

--- src/main/java/spoon/reflect/meta/impl/RoleHandlerHelper.java
+++ src/main/java/spoon/reflect/meta/impl/RoleHandlerHelper.java
@@ -17,7 +17,11 @@
 package spoon.reflect.meta.impl;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 import spoon.SpoonException;
 import spoon.reflect.declaration.CtElement;
@@ -31,6 +35,8 @@ import spoon.reflect.path.CtRole;
 public class RoleHandlerHelper {
 	private RoleHandlerHelper() {
 	}
+
+	private static Map<Class<?>, List<RoleHandler>> roleHandlersByClass = new HashMap<>();
 
 	@SuppressWarnings("unchecked")
 	private static final List<RoleHandler>[] roleHandlers = new List[CtRole.values().length];
@@ -47,7 +53,7 @@ public class RoleHandlerHelper {
 	 * @param targetClass the class of the to be manipulated node
 	 * @param role defines the to be manipulated attribute
 	 * @return {@link RoleHandler} implementation which knows how to manipulate the attribute of {@link CtRole} on `targetClass`
-	 * or throws exception if such role does not exists on the `targetClass`
+	 * or throws exception if such role doesn't exist on the `targetClass`
 	 */
 	public static RoleHandler getRoleHandler(Class<? extends CtElement> targetClass, CtRole role) {
 		RoleHandler rh = getOptionalRoleHandler(targetClass, role);
@@ -56,11 +62,12 @@ public class RoleHandlerHelper {
 		}
 		return rh;
 	}
+
 	/**
-	 * @param targetClass the class of the to be manipulated node
+	 * @param targetClass the Class of the to be manipulated node
 	 * @param role defines the to be manipulated attribute
-	 * @return {@link RoleHandler} implementation which knows how to manipulate the attribute of {@link CtRole} on `targetClass`
-	 * or returns null if such role does not exists on the `targetClass`
+	 * @return {@link RoleHandler} implementation, which knows how to manipulate the attribute of {@link CtRole} on `targetClass`
+	 * or returns null if such role doesn't exist on the `targetClass`
 	 */
 	public static RoleHandler getOptionalRoleHandler(Class<? extends CtElement> targetClass, CtRole role) {
 		List<RoleHandler> handlers = roleHandlers[role.ordinal()];
@@ -70,5 +77,52 @@ public class RoleHandlerHelper {
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * @param targetClass a Class whose handlers we are looking for
+	 * @return all {@link RoleHandler}s available for the `targetClass`
+	 */
+	public static List<RoleHandler> getRoleHandlers(Class<? extends CtElement> targetClass) {
+		List<RoleHandler> handlers = roleHandlersByClass.get(targetClass);
+		if (handlers == null) {
+			List<RoleHandler> modifiableHandlers = new ArrayList<>();
+			for (CtRole role : CtRole.values()) {
+				RoleHandler roleHandler = getOptionalRoleHandler(targetClass, role);
+				if (roleHandler != null) {
+					modifiableHandlers.add(roleHandler);
+				}
+			}
+			handlers = Collections.unmodifiableList(modifiableHandlers);
+			roleHandlersByClass.put(targetClass, handlers);
+		}
+		return handlers;
+	}
+
+	/**
+	 * @param consumer is called for each {@link RoleHandler} of SpoonModel
+	 */
+	public static void forEachRoleHandler(Consumer<RoleHandler> consumer) {
+		for (List<RoleHandler> list : roleHandlers) {
+			for (RoleHandler roleHandler : list) {
+				consumer.accept(roleHandler);
+			}
+		}
+	}
+
+	/**
+	 * @param element the {@link CtElement} whose relation from `element.getParent()` to `element` is needed.
+	 * @return {@link RoleHandler} handling relation from `element.getParent()` to `element`
+	 */
+	public static RoleHandler getRoleHandlerWrtParent(CtElement element) {
+		if (element.isParentInitialized() == false) {
+			return null;
+		}
+		CtElement parent = element.getParent();
+		CtRole roleInParent = element.getRoleInParent();
+		if (roleInParent == null) {
+			return null;
+		}
+		return RoleHandlerHelper.getRoleHandler(parent.getClass(), roleInParent);
 	}
 }
