b547c307fa409d533dd79fde68d265262a453e58
Shantanu <35997687+manic-coder@users.noreply.github.com>
2018-02-09 18:22:01 +0530

    fix: getDocComment() returns asterisk (*) and spaces before comment
    * fix: getDocComment() returns asterisk (*) and spaces before comment
    
    * fix: checkstyle compatibility
    Fix #1841

--- src/main/java/spoon/support/reflect/code/CtJavaDocTagImpl.java
+++ src/main/java/spoon/support/reflect/code/CtJavaDocTagImpl.java
@@ -85,4 +85,12 @@ public class CtJavaDocTagImpl extends CtElementImpl implements CtJavaDocTag {
 	public CtJavaDocTag clone() {
 		return (CtJavaDocTag) super.clone();
 	}
+
+	@Override
+	public String toString()	{
+		return this.getType().toString()	//Tag type, with @ sign included
+				+ " "	//Space required between tag type and parameter
+				+ this.param + System.lineSeparator()	//Tag parameter
+				+ "\t\t" + this.content + System.lineSeparator();
+	}
 }
