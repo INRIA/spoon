89b04f3c598b3d48a90bb72b879878efac5e4d3b
Egor18 <egorbr1801@mail.ru>
2018-02-10 03:02:07 +0300

    fix EqualsChecker for assignments

---
+++ src/test/resources/spoon/test/visitor/Assignments.java
@@ -0,0 +1,19 @@
+public class Assignments {
+
+	public void func(int x) {
+		x = 42;
+		x += 42;
+
+		x >>= 42;
+		x = 42;
+
+		x -= 42;
+		x += 42;
+
+		x = 42;
+		x = 100;
+
+		x = 42;
+		x = 42;
+	}
+}
