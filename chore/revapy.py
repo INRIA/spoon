a946d2b161ca9c7843e069787f13af5a70603b28
Martin Monperrus <martin.monperrus@gnieh.org>
2018-02-17 21:41:50 +0100

    Commit by Martin Monperrus on 17 February 2018

--- chore/revapy.py
+++ chore/revapy.py
@@ -65,7 +65,7 @@ if (action in accepted_actions):
         repo = gh.get_repo(repo_name,True)
 
         pr = repo.get_pull(pr_id)
-        for comment in pr.get_comments():
+        for comment in pr.get_issue_comments():
             # login is "spoon-bot" by default
             if comment.user.login == login:
                 comment.delete()
