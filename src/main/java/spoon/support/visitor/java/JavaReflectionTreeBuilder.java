25504734ac4a56f6ced983356d4bcbb280683300
Martin Monperrus <monperrus@users.noreply.github.com>
2018-02-17 06:42:26 +0100

    doc: another set of Javadoc comments for public methods (#1856)

--- src/main/java/spoon/support/visitor/java/JavaReflectionTreeBuilder.java
+++ src/main/java/spoon/support/visitor/java/JavaReflectionTreeBuilder.java
@@ -85,6 +85,7 @@ public class JavaReflectionTreeBuilder extends JavaReflectionVisitorImpl {
 		return contexts.pop();
 	}
 
+	/** transforms a java.lang.Class into a CtType (ie a shadow type in Spoon's parlance) */
 	public <T, R extends CtType<T>> R scan(Class<T> clazz) {
 		CtPackage ctPackage;
 		CtType<?> ctEnclosingClass;
