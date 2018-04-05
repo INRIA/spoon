db939a1d3d82b2667c516456145bab8c801f8225
Simon Urli <simon.urli@inria.fr>
2018-04-05 10:13:18 +0200

    Only check last job on master

--- chore/travis/travis-jdk9-other-seed.sh
+++ chore/travis/travis-jdk9-other-seed.sh
@@ -2,7 +2,7 @@
 
 # This script intends to be run on TravisCI
 # It executes compile and test goals
-
-source /opt/jdk_switcher/jdk_switcher.sh
-
-jdk_switcher use oraclejdk9 && SPOON_SEED_CU_COMPARATOR=$(( ( RANDOM % 10 )  + 1 )) mvn -Djava.src.version=1.9 test
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    source /opt/jdk_switcher/jdk_switcher.sh
+    jdk_switcher use oraclejdk9 && SPOON_SEED_CU_COMPARATOR=$(( ( RANDOM % 10 )  + 1 )) mvn -Djava.src.version=1.9 test
+fi
