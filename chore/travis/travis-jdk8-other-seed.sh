c4d329a07046024eb881963881d6a998ae025813
Simon Urli <simon.urli@inria.fr>
2018-04-11 14:43:09 +0200

    Remove comments to only execute last script on master

--- chore/travis/travis-jdk8-other-seed.sh
+++ chore/travis/travis-jdk8-other-seed.sh
@@ -5,7 +5,7 @@
 # The purpose of this script is to check that all tests are passing even when the compilation units are not sorted in the same order
 
 # Uncomment after Travis passes
-#if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     source /opt/jdk_switcher/jdk_switcher.sh
     jdk_switcher use oraclejdk8 && SPOON_SEED_CU_COMPARATOR=1 mvn -Djava.src.version=1.8 test
-#fi
+fi
