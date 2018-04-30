af0bd049ca45c8988d0b20054a9b8d51fbdbbebd
msramalho <up201403027@fe.up.pt>
2018-04-30 17:55:18 +0100

    creats test file stubs

---
+++ src/test/java/spoon/reflect/visitor/CtIteratorTest.java
@@ -0,0 +1,10 @@
+package spoon.reflect.visitor;
+
+import org.junit.Test;
+
+public class CtIteratorTest {
+    @Test
+    public void testMethodsInIterator() throws Exception {
+
+    }
+}
---
+++ src/test/java/spoon/reflect/visitor/processors/CheckIteratorTestProcessor.java
@@ -0,0 +1,11 @@
+package spoon.reflect.visitor.processors;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtClass;
+
+public class CheckIteratorTestProcessor extends AbstractProcessor<CtClass<?>> {
+    @Override
+    public void process(CtClass<?> element) {
+
+    }
+}
