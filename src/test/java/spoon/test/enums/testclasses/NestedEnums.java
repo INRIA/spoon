1ec88d5d553483ff14bf6d84821dd54393c60fd0
Pavel Vojtechovsky <pvojtechovsky@users.noreply.github.com>
2018-05-21 20:45:22 +0200

    fix: fix visibility of enum values (#1999)

---
+++ src/test/java/spoon/test/enums/testclasses/NestedEnums.java
@@ -0,0 +1,17 @@
+package spoon.test.enums.testclasses;
+
+public class NestedEnums {
+
+	private enum PrivateENUM {
+		VALUE
+	}
+	public enum PublicENUM {
+		VALUE
+	}
+	protected enum ProtectedENUM {
+		VALUE
+	}
+	enum PackageProtectedENUM {
+		VALUE
+	}
+}
