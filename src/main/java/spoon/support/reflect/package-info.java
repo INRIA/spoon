3ca138aa95f3d8166c73d52e547e4818b892aca4
Martin Monperrus <martin.monperrus@gnieh.org>
2018-05-23 19:17:39 +0200

    doc: move package.html to package-info.java

---
+++ src/main/java/spoon/support/reflect/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * <p>This package provides an implementation of the spoon.reflect package.</p>
+ * <p>The classes of this package should not be instantiated directly, but by using {@link spoon.reflect.factory.Factory}.</p>
+ */
+package spoon.support.reflect;
