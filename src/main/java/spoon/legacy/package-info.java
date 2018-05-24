c133ea96f5d4a8816d21d1048d644db87314f2ca
Martin Monperrus <monperrus@users.noreply.github.com>
2018-05-24 14:58:29 +0200

    doc: move package.html to package-info.java (#2005)

---
+++ src/main/java/spoon/legacy/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>This package contains legacy code of Spoon. This code is kept for sake of maximum compatibility but no support is provided anymore.</p>
+ * <h2>Related Documentation</h2>
+ * <ul>
+ * <li><a href="http://spoon.gforge.inria.fr/">Spoon Official Web Site</a>
+ * </ul>
+ */
+package spoon.legacy;
---
+++ src/main/java/spoon/support/template/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * <p>This package defines the substitution and matching engines for Java templates.</p>
+ */
+package spoon.support.template;
