5fcf05a75f3931883d79ec093053e24d93a9f3e4
Pavel Vojtěchovský <p.vojtechovsky@post.cz>
2018-05-26 12:51:47 +0200

    fix wrong code and tests which makes invalid AST

--- src/main/java/spoon/support/reflect/code/CtCaseImpl.java
+++ src/main/java/spoon/support/reflect/code/CtCaseImpl.java
@@ -140,7 +140,11 @@ public class CtCaseImpl<E> extends CtStatementImpl implements CtCase<E> {
 
 	@Override
 	public <T extends CtStatementList> T insertEnd(CtStatementList statements) {
-		for (CtStatement s : statements.getStatements()) {
+		List<CtStatement> tobeInserted = new ArrayList<>(statements.getStatements());
+		//remove statements from the `statementsToBeInserted` before they are added to spoon model
+		//note: one element MUST NOT be part of two models.
+		statements.setStatements(null);
+		for (CtStatement s : tobeInserted) {
 			insertEnd(s);
 		}
 		return (T) this;
