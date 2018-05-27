3d27ca151187ee13224e2ab8528463142a02074a
Martin Monperrus <monperrus@users.noreply.github.com>
2018-05-27 21:26:19 +0200

    update message

--- src/main/java/spoon/support/util/ModelList.java
+++ src/main/java/spoon/support/util/ModelList.java
@@ -93,7 +93,12 @@ public abstract class ModelList<T extends CtElement> extends AbstractList<T> imp
 		if (owner.getFactory().getEnvironment().checksAreSkipped() == false && element.isParentInitialized() && element.getParent() != owner) {
 			//the `e` already has an different parent. Check if it is still linked to that parent
 			if (element.getRoleInParent() != null) {
-				throw new SpoonException("The element " + element + " is already used by another part of SpoonModel. Remove it from previous model or clone it before.");
+				throw new SpoonException("The default behavior has changed, a new check has been added! Don't worry, you can disable this check\n"
+							 + "with one of the following options:\n"
+							 + " - by configuring Spoon with getEnvironment().setSelfChecks(true)\n"
+							 + " - by removing the node from its previous parent (element.delete())\n"
+							 + " - by cloning the node before adding it here (element.clone())\n"
+							);
 			}
 		}
 		element.setParent(owner);
