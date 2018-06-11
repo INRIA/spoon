d9aeb34536e10d443980697d5f7fbb4961a3274b
Simon Urli <simon.urli@inria.fr>
2018-06-11 16:11:44 +0200

    Add a new concept for an inferred variable

---
+++ src/main/java/spoon/reflect/declaration/CtInferredVariable.java
@@ -0,0 +1,7 @@
+package spoon.reflect.declaration;
+
+/**
+ * This abstract element defined an inferred variable as defined in Java 10 with the keyword `var`.
+ */
+public interface CtInferredVariable extends CtVariable {
+}
