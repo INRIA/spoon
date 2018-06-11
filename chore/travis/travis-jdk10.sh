95c5d0af0e7756f81f495e3b667187ffc59d2587
Simon Urli <simon.urli@inria.fr>
2018-06-11 16:14:05 +0200

    Still working on the right parameters for the script

--- chore/travis/travis-jdk10.sh
+++ chore/travis/travis-jdk10.sh
@@ -6,6 +6,8 @@
 source /opt/jdk_switcher/jdk_switcher.sh
 wget https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
 chmod +x install-jdk.sh
-./install-jdk.sh -F 10 -L BCL
+
+export JAVA_HOME=$HOME/openjdk8
+./install-jdk.sh --install openjdk10 --target $JAVA_HOME
 
 jdk_switcher use openjdk10 && mvn -Djava.src.version=1.10 test
