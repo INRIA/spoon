589aa554679a0f960f26ea4664b6602a266763d9
Simon Urli <simon.urli@inria.fr>
2018-06-11 15:57:07 +0200

    Still trying to make it work

--- chore/travis/travis-jdk10.sh
+++ chore/travis/travis-jdk10.sh
@@ -4,7 +4,8 @@
 # It executes compile and test goals
 
 source /opt/jdk_switcher/jdk_switcher.sh
-
+wget https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
+chmod +x install-jdk.sh
 ./install-jdk.sh -F 10 -L BCL
 
 jdk_switcher use openjdk10 && mvn -Djava.src.version=1.10 test
