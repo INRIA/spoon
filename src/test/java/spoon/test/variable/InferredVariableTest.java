632daabdd6d9fd47b8c733b42ea36f1fb700234c
Simon Urli <simon.urli@inria.fr>
2018-06-11 16:40:46 +0200

    Fix compliance issue with Java 10 and more.

--- src/test/java/spoon/test/variable/InferredVariableTest.java
+++ src/test/java/spoon/test/variable/InferredVariableTest.java
@@ -9,6 +9,8 @@ import spoon.reflect.visitor.filter.TypeFilter;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class InferredVariableTest {
 
@@ -16,10 +18,15 @@ public class InferredVariableTest {
     public void testInferredVariableAreMarked() {
         Launcher launcher = new Launcher();
         launcher.getEnvironment().setComplianceLevel(10);
-        launcher.addInputResource("./src/test/resources/spoon/test/var/example.java");
+        launcher.addInputResource("./src/test/resources/spoon/test/var/Main.java");
 
         CtModel model = launcher.buildModel();
         List<CtLocalVariable> localVariables = model.getElements(new TypeFilter<>(CtLocalVariable.class));
         assertEquals(4, localVariables.size());
+
+        assertFalse(localVariables.get(0).isInferred());
+        assertTrue(localVariables.get(1).isInferred());
+        assertTrue(localVariables.get(2).isInferred());
+        assertTrue(localVariables.get(3).isInferred());
     }
 }
