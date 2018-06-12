7d46ca1459a203a312802132ea0e792d723a69d0
Simon Urli <simon.urli@inria.fr>
2018-06-12 09:54:38 +0200

    Fix assertions

--- src/test/java/spoon/MavenLauncherTest.java
+++ src/test/java/spoon/MavenLauncherTest.java
@@ -15,13 +15,13 @@ public class MavenLauncherTest {
 		// without the tests
 		MavenLauncher launcher = new MavenLauncher("./", MavenLauncher.SOURCE_TYPE.APP_SOURCE);
 
-		assertEquals(13, launcher.getEnvironment().getSourceClasspath().length);
+		assertEquals(15, launcher.getEnvironment().getSourceClasspath().length);
 		// 56 because of the sub folders of src/main/java
 		assertEquals(58, launcher.getModelBuilder().getInputSources().size());
 
 		// with the tests
 		launcher = new MavenLauncher("./", MavenLauncher.SOURCE_TYPE.ALL_SOURCE);
-		assertEquals(20, launcher.getEnvironment().getSourceClasspath().length);
+		assertEquals(22, launcher.getEnvironment().getSourceClasspath().length);
 		// 236 because of the sub folders of src/main/java and src/test/java
 		assertTrue("size: "+launcher.getModelBuilder().getInputSources().size(), launcher.getModelBuilder().getInputSources().size() >= 220);
 
