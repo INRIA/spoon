266e2e7ef8101ffd40ad462d6bb825f93826d4da
Simon Urli <simon.urli@inria.fr>
2018-06-21 17:41:12 +0200

    Fix DJPP test

--- src/test/java/spoon/test/prettyprinter/DefaultPrettyPrinterTest.java
+++ src/test/java/spoon/test/prettyprinter/DefaultPrettyPrinterTest.java
@@ -210,7 +210,7 @@ public class DefaultPrettyPrinterTest {
 
 		expected =
 			"public enum ENUM {" +nl+
-			"    E1(spoon.test.prettyprinter.testclasses.sub.TypeIdentifierCollision.globalField,spoon.test.prettyprinter.testclasses.sub.TypeIdentifierCollision.ENUM.E1);" +nl+
+			"    E1(spoon.test.prettyprinter.testclasses.sub.TypeIdentifierCollision.globalField, spoon.test.prettyprinter.testclasses.sub.TypeIdentifierCollision.ENUM.E1);" +nl+
 			"    final int NUM;" +nl+nl+
 			"    final Enum<?> e;" +nl+nl+
 			"    private ENUM(int num, Enum<?> e) {" +nl+
