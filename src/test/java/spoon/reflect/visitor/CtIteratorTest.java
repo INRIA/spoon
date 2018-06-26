7c4f17dbfa6f89e771d74d3bc9795e9fd49377cc
Tomek <tomek.j2ee@gmail.com>
2018-06-26 22:47:08 +0200

    Fix checkstyle violations

--- src/test/java/spoon/reflect/visitor/CtIteratorTest.java
+++ src/test/java/spoon/reflect/visitor/CtIteratorTest.java
@@ -11,66 +11,66 @@ import static org.junit.Assert.assertEquals;
 
 public class CtIteratorTest {
 
-    @Test
-    public void testCtElementIteration() throws Exception {
-        // contract: CtIterator must go over all nodes in dfs order
-        final Launcher launcher = new Launcher();
-        launcher.setArgs(new String[] {"--output-type", "nooutput"});
-        launcher.getEnvironment().setNoClasspath(true);
-        // resources to iterate
-        launcher.addInputResource("./src/main/java/spoon/reflect/visitor/CtScanner.java");
-        launcher.buildModel();
+	@Test
+	public void testCtElementIteration() throws Exception {
+		// contract: CtIterator must go over all nodes in dfs order
+		final Launcher launcher = new Launcher();
+		launcher.setArgs(new String[]{"--output-type", "nooutput"});
+		launcher.getEnvironment().setNoClasspath(true);
+		// resources to iterate
+		launcher.addInputResource("./src/main/java/spoon/reflect/visitor/CtScanner.java");
+		launcher.buildModel();
 
-        // get the first Type
-        CtElement root = launcher.getFactory().getModel().getAllTypes().iterator().next();
+		// get the first Type
+		CtElement root = launcher.getFactory().getModel().getAllTypes().iterator().next();
 
-        testCtIterator(root);
-        testAsIterable(root);
-        testDescendantIterator(root);
-    }
+		testCtIterator(root);
+		testAsIterable(root);
+		testDescendantIterator(root);
+	}
 
-    public void testCtIterator(CtElement root) {
-        Deque<CtElement> ctElements = getDescendantsInDFS(root);
+	public void testCtIterator(CtElement root) {
+		Deque<CtElement> ctElements = getDescendantsInDFS(root);
 
-        CtIterator iterator = new CtIterator(root);
-        while (iterator.hasNext()) {
-            assertEquals(ctElements.pollFirst(), iterator.next());
-        }
-    }
+		CtIterator iterator = new CtIterator(root);
+		while (iterator.hasNext()) {
+			assertEquals(ctElements.pollFirst(), iterator.next());
+		}
+	}
 
-    public void testAsIterable(CtElement root) {
-        Deque<CtElement> ctElements = getDescendantsInDFS(root);
+	public void testAsIterable(CtElement root) {
+		Deque<CtElement> ctElements = getDescendantsInDFS(root);
 
-        for (CtElement elem : root.asIterable()) {
-            assertEquals(elem, ctElements.pollFirst());
-        }
-    }
+		for (CtElement elem: root.asIterable()) {
+			assertEquals(elem, ctElements.pollFirst());
+		}
+	}
 
-    public void testDescendantIterator(CtElement root) {
-        Deque<CtElement> ctElements = getDescendantsInDFS(root);
+	public void testDescendantIterator(CtElement root) {
+		Deque<CtElement> ctElements = getDescendantsInDFS(root);
 
-        root.descendantIterator().forEachRemaining((CtElement elem) ->
-                assertEquals(ctElements.pollFirst(), elem)
-        );
-    }
+		root.descendantIterator().forEachRemaining((CtElement elem) ->
+				assertEquals(ctElements.pollFirst(), elem)
+		);
+	}
 
-    Deque<CtElement> getDescendantsInDFS(CtElement root) {
-        CtScannerList counter = new CtScannerList();
-        root.accept(counter);
+	Deque<CtElement> getDescendantsInDFS(CtElement root) {
+		CtScannerList counter = new CtScannerList();
+		root.accept(counter);
 
-        return counter.nodes;
-    }
+		return counter.nodes;
+	}
 
-    /**
-     * Class that saves a deque with all the nodes the {@link CtScanner} visits,
-     * in DFS order, for the {@link CtIterator} test
-     */
-    class CtScannerList extends CtScanner {
-        public Deque<CtElement> nodes = new ArrayDeque<>();
+	/**
+	 * Class that saves a deque with all the nodes the {@link CtScanner} visits,
+	 * in DFS order, for the {@link CtIterator} test
+	 */
+	class CtScannerList extends CtScanner {
+		public Deque<CtElement> nodes = new ArrayDeque<>();
 
-        @Override
-        protected void enter(CtElement e) {
-            nodes.addLast(e);
-        }
-    }
+		@Override
+		protected void enter(CtElement e) {
+			nodes.addLast(e);
+		}
+	}
 }
