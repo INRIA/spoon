9f69da222506897e25598a2b31b492b89b4277b7
Tomek <tomek.j2ee@gmail.com>
2018-06-28 21:38:10 +0200

    Fix checkstyle violations

--- src/test/java/spoon/reflect/declaration/testclasses/Subclass.java
+++ src/test/java/spoon/reflect/declaration/testclasses/Subclass.java
@@ -1,10 +1,10 @@
 package spoon.reflect.declaration.testclasses;
 
 public class Subclass extends ExtendsArrayList implements Subinterface {
-    @Override
-    public int compareTo(Object o) {
-        return 0;
-    }
+	@Override
+	public int compareTo(Object o) {
+		return 0;
+	}
 
 	@Override
 	public void foo() {
