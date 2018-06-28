d0a962bac20faef02f01074d5dcbb1d83f27d5b9
Tomek <tomek.j2ee@gmail.com>
2018-06-28 23:24:22 +0200

    Fix checkstyle violations

--- src/test/java/spoon/test/annotation/testclasses/AnnotParam.java
+++ src/test/java/spoon/test/annotation/testclasses/AnnotParam.java
@@ -1,10 +1,9 @@
 package spoon.test.annotation.testclasses;
 
-
 public class AnnotParam {
 
-	@SuppressWarnings({"unused","rawtypes"})
+	@SuppressWarnings({"unused", "rawtypes"})
 	public void m(int a) {
-	} 
+	}
 
 }
