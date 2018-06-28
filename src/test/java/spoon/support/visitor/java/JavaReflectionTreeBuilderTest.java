89e053fea0791943d2fd1b6062a3d5eaa7c6d0bb
Tomek <tomek.j2ee@gmail.com>
2018-06-28 22:39:45 +0200

    Simplify 'if' expression

--- src/test/java/spoon/support/visitor/java/JavaReflectionTreeBuilderTest.java
+++ src/test/java/spoon/support/visitor/java/JavaReflectionTreeBuilderTest.java
@@ -379,10 +379,7 @@ public class JavaReflectionTreeBuilderTest {
 				List<CtAnnotation<?>> fileteredElements = ((List<CtAnnotation<?>>) elements).stream().filter(a -> {
 					CtTypeReference<?> at = a.getAnnotationType();
 					Class ac = at.getActualClass();
-					if (ac == Override.class || ac == SuppressWarnings.class || ac == Root.class) {
-						return false;
-					}
-					return true;
+					return ac != Override.class && ac != SuppressWarnings.class && ac != Root.class;
 				}).collect(Collectors.toList());
 				super.biScan(role, fileteredElements, others);
 				return;
