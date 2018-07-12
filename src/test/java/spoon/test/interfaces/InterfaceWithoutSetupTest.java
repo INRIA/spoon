94adffd254fb9a14c559e2958c68333cc878e725
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-07-13 00:01:08 +0200

    refactor: unify name of TestInterfaceWithoutSetup.java

--- src/test/java/spoon/test/interfaces/InterfaceWithoutSetupTest.java
+++ src/test/java/spoon/test/interfaces/InterfaceWithoutSetupTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class TestInterfaceWithoutSetup {
+public class InterfaceWithoutSetupTest {
 
 	@Test
 	public void testModifierFromInterfaceFieldAndMethod() {
