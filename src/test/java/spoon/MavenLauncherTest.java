8fef644bb76a3c9050aa71af6b8d54d60a4dc880
nharrand <nicolas.harrand@gmail.com>
2018-07-12 16:21:24 +0200

    clean(MavenLauncherTest): remove unused import

--- src/test/java/spoon/MavenLauncherTest.java
+++ src/test/java/spoon/MavenLauncherTest.java
@@ -1,19 +1,14 @@
 package spoon;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.maven.shared.invoker.*;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class MavenLauncherTest {
 	@Test
