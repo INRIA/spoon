e4c27ba509bed179526e2930859bbaed6f8746be
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-07-13 00:02:30 +0200

    refactor: fix formatting in JavaDocTest.java

--- src/test/java/spoon/test/javadoc/JavaDocTest.java
+++ src/test/java/spoon/test/javadoc/JavaDocTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class JavaDocTest {
+
 	@Test
 	public void testJavaDocReprint() throws Exception {
 		SpoonAPI launcher = new Launcher();
