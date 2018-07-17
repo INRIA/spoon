68911e0cbffdaa0343ff74462b4867be0163f839
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-07-18 00:00:59 +0200

    Fix import

--- src/main/java/spoon/support/util/RtHelper.java
+++ src/main/java/spoon/support/util/RtHelper.java
@@ -33,7 +33,6 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
