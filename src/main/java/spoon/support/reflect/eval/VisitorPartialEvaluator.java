81560fdb21e5a88e1cdcd3fdf8a89dc9590db021
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-07-22 01:03:13 +0200

    refactor: remove unused assignment from VisitorPartialEvaluator.java

--- src/main/java/spoon/support/reflect/eval/VisitorPartialEvaluator.java
+++ src/main/java/spoon/support/reflect/eval/VisitorPartialEvaluator.java
@@ -425,7 +425,7 @@ public class VisitorPartialEvaluator extends CtScanner implements PartialEvaluat
 				}
 			} else {
 				// try to completely evaluate
-				T r = null;
+				T r;
 				try {
 					// System.err.println("invocking "+i);
 					r = RtHelper.invoke(i);
