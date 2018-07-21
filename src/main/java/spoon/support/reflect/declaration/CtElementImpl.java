4ab9fe0f61c3cca1297b6fe611838c4494a68018
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-07-22 01:35:21 +0200

    refactor: fix SonarQube: A NullPointerException could be thrown; o is nullable here.

--- src/main/java/spoon/support/reflect/declaration/CtElementImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtElementImpl.java
@@ -132,6 +132,9 @@ public abstract class CtElementImpl implements CtElement, Serializable {
 
 	@Override
 	public boolean equals(Object o) {
+		if (o == null) {
+			return false;
+		}
 		if (this == o) {
 			return true;
 		}
