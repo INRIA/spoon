37f130787370532caf8ff6b291f220666ed5a8f7
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-07-26 23:39:46 +0200

    fix import

--- src/main/java/spoon/reflect/visitor/JavaIdentifiers.java
+++ src/main/java/spoon/reflect/visitor/JavaIdentifiers.java
@@ -16,12 +16,11 @@
  */
 package spoon.reflect.visitor;
 
-import java.util.Arrays;
+import spoon.reflect.reference.CtExecutableReference;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
-
-import spoon.reflect.reference.CtExecutableReference;
 
 /**
  * This enum defines the Java keywords and some helper method to determine if
