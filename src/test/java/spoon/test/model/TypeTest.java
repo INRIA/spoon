93902be3c38ae9407a21dd0a4318dc3f6eed6bf9
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-07-29 22:38:38 +0200

    refactor: move resources to testclasses in model

--- src/test/java/spoon/test/model/TypeTest.java
+++ src/test/java/spoon/test/model/TypeTest.java
@@ -95,7 +95,7 @@ public class TypeTest {
 
 	@Test
 	public void testGetDeclaredOrIheritedFieldByReflection() throws Exception {
-		CtTypeReference<?> type = build("spoon.test.model", "ClassWithSuperOutOfModel").getReference();
+		CtTypeReference<?> type = build("spoon.test.model.testclasses", "ClassWithSuperOutOfModel").getReference();
 
 		assertEquals("buf", type.getDeclaredOrInheritedField("buf").getSimpleName());
 		assertEquals("count", type.getDeclaredOrInheritedField("count").getSimpleName());
@@ -105,9 +105,9 @@ public class TypeTest {
 	@Test
 	public void testTypeInfoIsInterface() throws Exception {
 		//contract: isInterface returns true only for interfaces
-		CtType<?> clazz = build("spoon.test.model", "ClassWithSuperOutOfModel");
+		CtType<?> clazz = build("spoon.test.model.testclasses", "ClassWithSuperOutOfModel");
 		checkIsSomething("class", clazz);
-		CtType<?> type = build("spoon.test.model", "InterfaceWrithFields");
+		CtType<?> type = build("spoon.test.model.testclasses", "InterfaceWrithFields");
 		checkIsSomething("interface", type);
 		checkIsSomething("enum", type.getFactory().Enum().create(type.getPackage(), "someEnum"));
 		CtType<?> ctAnnotation = type.getFactory().Annotation().create(type.getPackage(), "someAnnotation");
