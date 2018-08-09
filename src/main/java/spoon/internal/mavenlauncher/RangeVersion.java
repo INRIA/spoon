762a8b301cb9e5df4fd804b9ee4308af1b22bf23
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-08-09 23:17:37 +0200

    refactor: use isEmpty()

--- src/main/java/spoon/internal/mavenlauncher/RangeVersion.java
+++ src/main/java/spoon/internal/mavenlauncher/RangeVersion.java
@@ -29,18 +29,17 @@ public class RangeVersion {
 		includeStart = range.startsWith("[");
 		includeEnd = range.endsWith("]");
 		String[] splitRange = range.substring(1, range.length() - 1).split(",");
-		if (splitRange[0].length() == 0) {
+		if (splitRange[0].isEmpty()) {
 			start = new Version("0.0.0.0");
 		} else {
 			start = new Version(splitRange[0]);
 		}
-		if (splitRange.length == 1 || splitRange[1].length() == 0) {
+		if (splitRange.length == 1 || splitRange[1].isEmpty()) {
 			end = new Version("99999999.9999999.999999.99999");
 		} else {
 			end = new Version(splitRange[1]);
 		}
 	}
-
 
 	boolean include(Version v) {
 		int compareToStart = v.compareTo(start);
