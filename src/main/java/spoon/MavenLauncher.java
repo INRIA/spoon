27ae53798b6e773e479048dbec00f972e3ef6184
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-08-18 21:25:49 +0200

    refactor: close resource in readClassPath() in MavenLauncher.java

--- src/main/java/spoon/MavenLauncher.java
+++ src/main/java/spoon/MavenLauncher.java
@@ -172,22 +172,22 @@ public class MavenLauncher extends Launcher {
 
 		//Read the content of spoon.classpath.tmp
 		for (File classPathFile: classPathFiles) {
-			BufferedReader br = new BufferedReader(new FileReader(classPathFile));
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
-			while (line != null) {
-				sb.append(line);
-				line = br.readLine();
-			}
-			if (!"".equals(sb.toString())) {
-				String[] classpath = sb.toString().split(File.pathSeparator);
-				for (String cpe : classpath) {
-					if (!classpathElements.contains(cpe)) {
-						classpathElements.add(cpe);
+			try (BufferedReader br = new BufferedReader(new FileReader(classPathFile))) {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+				while (line != null) {
+					sb.append(line);
+					line = br.readLine();
+				}
+				if (!"".equals(sb.toString())) {
+					String[] classpath = sb.toString().split(File.pathSeparator);
+					for (String cpe : classpath) {
+						if (!classpathElements.contains(cpe)) {
+							classpathElements.add(cpe);
+						}
 					}
 				}
 			}
-			br.close();
 		}
 		String[] classpath = new String[classpathElements.size()];
 		classpath = classpathElements.toArray(classpath);
