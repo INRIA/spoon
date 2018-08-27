9c4483300c85b622124b732fb841db49f657a764
Pavel Vojtechovsky <p.vojtechovsky@post.cz>
2018-08-27 21:22:03 +0200

    fix: CtType#getMethod, CtType#getConstructor

--- src/main/java/spoon/support/reflect/declaration/CtClassImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtClassImpl.java
@@ -85,13 +85,7 @@ public class CtClassImpl<T extends Object> extends CtTypeImpl<T> implements CtCl
 				continue;
 			}
 			CtConstructor<T> c = (CtConstructor<T>) typeMember;
-			boolean cont = c.getParameters().size() == parameterTypes.length;
-			for (int i = 0; cont && (i < c.getParameters().size()) && (i < parameterTypes.length); i++) {
-				if (!parameterTypes[i].getQualifiedName().equals(c.getParameters().get(i).getType().getQualifiedName())) {
-					cont = false;
-				}
-			}
-			if (cont) {
+			if (hasSameParameters(c, parameterTypes)) {
 				return c;
 			}
 		}
