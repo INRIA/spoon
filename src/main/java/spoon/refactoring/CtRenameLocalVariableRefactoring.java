0ec619340ba437315e0adfefc5db796f608b1358
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-09-01 23:51:07 +0200

    refactor: inline local variable

--- src/main/java/spoon/refactoring/CtRenameLocalVariableRefactoring.java
+++ src/main/java/spoon/refactoring/CtRenameLocalVariableRefactoring.java
@@ -84,9 +84,8 @@ public class CtRenameLocalVariableRefactoring extends AbstractRenameRefactoring<
 
 		@Override
 		public ScanningMode enter(CtElement element) {
-			if (ignoredParent != null && element instanceof CtElement) {
-				CtElement ele = (CtElement) element;
-				if (ele.hasParent(ignoredParent)) {
+			if (ignoredParent != null && element != null) {
+				if (element.hasParent(ignoredParent)) {
 					return ScanningMode.SKIP_ALL;
 				}
 			}
