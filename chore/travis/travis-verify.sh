d61e3d6b3b259777eac0149bc0725bb354958744
nharrand <nicolas.harrand@gmail.com>
2018-09-17 13:07:12 +0200

    adding redondant command line arguments in travis-verify.sh in the hope that it helps with javadoc error...

--- chore/travis/travis-verify.sh
+++ chore/travis/travis-verify.sh
@@ -7,4 +7,4 @@
 source /opt/jdk_switcher/jdk_switcher.sh
 pip install --user CommonMark==0.7.5 requests pygithub
 
-jdk_switcher use oraclejdk9 && mvn -Djava.src.version=1.9 verify license:check site javadoc:jar install -DskipTests && python ./chore/check-links-in-doc.py
+jdk_switcher use oraclejdk9 && mvn -Djava.src.version=1.9 verify license:check site javadoc:jar install -DskipTests -DadditionalJOption=-Xdoclint:none && python ./chore/check-links-in-doc.py
