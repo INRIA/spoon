1d337f098a44a15a141d5920335dfe9e2b86260c
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-09-22 17:48:18 +0200

    refactor: restore annotation {}

--- src/main/java/spoon/reflect/annotations/MetamodelPropertyField.java
+++ src/main/java/spoon/reflect/annotations/MetamodelPropertyField.java
@@ -31,7 +31,7 @@ import java.lang.annotation.Target;
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target( ElementType.FIELD )
+@Target({ ElementType.FIELD })
 public @interface MetamodelPropertyField {
 	CtRole[] role();
 }
