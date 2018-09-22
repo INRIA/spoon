102d10e754b0564977ee13515fc666236a42c052
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-09-22 16:35:59 +0200

    refactor: remove unused imports from tests

--- src/test/java/spoon/test/snippets/SnippetTest.java
+++ src/test/java/spoon/test/snippets/SnippetTest.java
@@ -17,7 +17,6 @@ import spoon.support.compiler.VirtualFile;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static spoon.testing.utils.ModelUtils.createFactory;
