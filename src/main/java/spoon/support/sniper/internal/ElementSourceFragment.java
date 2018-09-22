94547257186fb1be7e7e6888725f7dc2983f6e2f
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-09-22 22:57:12 +0200

    refactor: fix array

--- src/main/java/spoon/support/sniper/internal/ElementSourceFragment.java
+++ src/main/java/spoon/support/sniper/internal/ElementSourceFragment.java
@@ -616,7 +616,7 @@ public class ElementSourceFragment implements SourceFragment {
 			*/
 	));
 
-	private static final String[] javaKeywordsJoined = new String[] {
+	private static final String[] javaKeywordsJoined = {
 			"abstract continue for new switch",
 			"assert default goto package synchronized",
 			"boolean do if private this",
@@ -626,7 +626,8 @@ public class ElementSourceFragment implements SourceFragment {
 			"catch extends int short try",
 			"char final interface static void",
 			"class finally long strictfp volatile",
-			"const float native super while"};
+			"const float native super while"
+	};
 
 	private static final Set<String> javaKeywords = new HashSet<>();
 	static {
