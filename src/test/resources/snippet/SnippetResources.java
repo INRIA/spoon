dba76200a869c397a58e1749f4d14039a52b3b7b
Martin Monperrus <monperrus@users.noreply.github.com>
2018-09-23 08:49:23 +0200

    fix: fix compileAndReplaceSnippetsIn that was broken (#2505)

---
+++ src/test/resources/snippet/SnippetResources.java
@@ -0,0 +1,13 @@
+package snippet.test.resources;
+
+public class SnippetResources {
+
+    public static void staticMethod() {
+        SnippetResources s = new SnippetResources();
+    }
+
+    public void method(int a) {
+        System.out.println(a);
+    }
+
+}
