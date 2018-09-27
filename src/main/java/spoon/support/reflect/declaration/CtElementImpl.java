27222d15d1de6b4d6ccd7ff1b0fe644a0a1b47e3
Tomasz Zielinski <tomek.j2ee@gmail.com>
2018-09-28 00:05:15 +0200

    fix formatting

--- src/main/java/spoon/support/reflect/declaration/CtElementImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtElementImpl.java
@@ -14,7 +14,6 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C license and that you accept its terms.
  */
-
 package spoon.support.reflect.declaration;
 
 import org.apache.log4j.Logger;
