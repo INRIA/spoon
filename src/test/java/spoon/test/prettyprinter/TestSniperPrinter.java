abbeb29045b00703ca82ec354429c167bc038a74
Tomasz Zieliński <tomek.j2ee@gmail.com>
2018-10-04 18:51:55 +0200

    refactor: remove commented code in TestSniperPrinter.java (#2596)

--- src/test/java/spoon/test/prettyprinter/TestSniperPrinter.java
+++ src/test/java/spoon/test/prettyprinter/TestSniperPrinter.java
@@ -237,7 +237,6 @@ public class TestSniperPrinter {
 		while (m.find()) {
 			lastImportEnd = m.end();
 		}
-		//System.out.println(lastImportEnd);
 		return source.substring(lastImportEnd).trim();
 	}
 }
