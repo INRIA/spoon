1fe790142d490725ebcb3299db27488c75cf3d22
nharrand <nicolas.harrand@gmail.com>
2018-10-09 14:04:59 +0200

    PoC SpoonClassFileTransformer

---
+++ doc/agent.md
@@ -0,0 +1,44 @@
+# Spoon Agent
+
+Spoon can also be used at load time to transform classes. `SpoonClassFileTransformer` provide an abstraction of `ClassFileTransformer`
+where the user can define its transformation with spoon instead of working directly on bytecode.
+Bytecode of classes will be decompiled gradually when loaded, and the model will be updated in consequence.
+
+The following example show the definition of a basic agent inserting a trace a the end of every method called `main`.
+
+Here is the agent:
+```java
+public class Agent {
+	public static void premain(String agentArgs, Instrumentation inst) {
+		System.out.println( "Hello Agent" );
+
+		//We exclude any classes not in our package from decompilation
+		inst.addTransformer(new InsertTraceInMainTransformer(cl -> cl.startsWith("org/my/package")));
+
+		System.out.println( "Agent Done." );
+	}
+}
+```
+
+Here is the class extending SpoonClassFileTransformer
+```java
+public class InsertTraceInMainTransformer extends SpoonClassFileTransformer {
+	public TSpoonClassFileTransformer(Predicate<String> classNameFilter) {
+		super(classNameFilter);
+	}
+
+	@Override
+	//Accept any class for which we have decompiled bytecode but exclude interfaces
+	public boolean accept(CtType type) {
+		return (type instanceof CtClass);
+	}
+
+	@Override
+	public void transform(CtType type) {
+	    //Get main method
+		CtMethod main = (CtMethod) type.getMethodsByName("main").get(0);
+		//Insert a trace containing the name of the class
+		main.getBody().addStatement(type.getFactory().createCodeSnippetStatement("System.out.println(\"Hello " + type.getQualifiedName() + "\");"));
+	}
+}
+```
---
+++ src/main/java/spoon/decompiler/SpoonClassFileTransformer.java
@@ -0,0 +1,136 @@
+package spoon.decompiler;
+
+import org.benf.cfr.reader.Main;
+import spoon.IncrementalLauncher;
+import spoon.SpoonModelBuilder;
+import spoon.reflect.CtModel;
+import spoon.reflect.declaration.CtType;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.nio.file.Files;
+import java.security.ProtectionDomain;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public abstract class SpoonClassFileTransformer implements ClassFileTransformer {
+
+	Predicate<String> classNameFilter;
+	String pathToDecompiled;
+	String pathToRecompile;
+	String pathToCache;
+	public Decompiler decompiler;
+	SpoonModelBuilder compiler;
+	IncrementalLauncher launcher;
+	Set<File> inputSources;
+
+	//Exclude jvm classes
+	public static final Predicate<String> defaultFilter = s -> !(s.startsWith("java") || s.startsWith("sun"));
+
+	/**
+	 * Default Constructor for SpooClassFileTransformer
+	 *
+	 * @param classNameFilter Filter for classname. If classeNameFilter.test(className) returns false,
+	 *                        the class will be loaded without decompilation nor transformation.
+	 *                        If null, a default filter will filter out typical jvm classes (starting with java* or sun*)
+	 *                        Note @{SpoonClassFileTransformer.defaultFilter} may be used in conjunction of custom filter
+	 *                        with `defaultFilter.and(classNameFilter)`.
+	 */
+	public SpoonClassFileTransformer(Predicate<String> classNameFilter) {
+		if (classNameFilter == null) {
+			this.classNameFilter = defaultFilter;
+		} else {
+			this.classNameFilter = classNameFilter;
+		}
+		String classPathAr[] = System.getProperty("java.class.path").split(":");
+		Set<String> classPath = new HashSet<>(Arrays.asList(classPathAr));
+		pathToDecompiled = "spoon-decompiled";
+		pathToRecompile = "spoon-recompiled";
+		pathToCache = "spoon-cache";
+
+
+		//inputSources = Collections.singleton(new File(pathToDecompiled));
+		inputSources = new HashSet<>();
+		inputSources.add(new File(pathToDecompiled));
+		launcher = new IncrementalLauncher(inputSources, classPath, new File(pathToCache));
+		launcher.addInputResource(pathToDecompiled);
+
+		decompiler = s -> Main.main(new String[]{s, "--outputdir", pathToDecompiled});
+
+
+		//Should we wait for directory creation?
+
+		compiler = launcher.createCompiler();
+		launcher.buildModel();
+	}
+
+	@Override
+	public byte[] transform(
+			ClassLoader loader,
+			String className,
+			Class<?> classBeingRedefined,
+			ProtectionDomain protectionDomain,
+			byte[] classfileBuffer
+	) throws IllegalClassFormatException {
+		try {
+			launcher.getEnvironment().debugMessage("[Agent] loading " + className);
+
+			//If the class is not matched by user's filter, resume unmodified loading
+			if (!classNameFilter.test(className)) {
+				return classfileBuffer;
+			}
+
+			//Decompile classfile
+			launcher.getEnvironment().debugMessage("[Agent] decompiling " + className);
+			String pathToClassFile = loader.getResource(className + ".class").getPath();
+			decompiler.decompile(pathToClassFile);
+
+			//Get updated model
+			CtModel model = launcher.getModel();
+
+			//Get class model
+			CtType toBeTransformed =  model.getAllTypes().stream().filter(t -> t.getQualifiedName().equals(className.replace("/", "."))).findAny().get();
+
+			//If the class model is not modified by user, resume unmodified loading
+			if (!accept(toBeTransformed)) {
+				return classfileBuffer;
+			}
+			launcher.getEnvironment().debugMessage("[Agent] transforming " + className);
+			transform(toBeTransformed);
+
+			//Compile new class model
+			compiler.compile(SpoonModelBuilder.InputType.CTTYPES);
+			File transformedClass = new File(compiler.getBinaryOutputDirectory(), className + ".class");
+			try {
+				//Load Modified classFIle
+				byte[] fileContent = Files.readAllBytes(transformedClass.toPath());
+				launcher.getEnvironment().debugMessage("[Agent] loading transformed " + className);
+				return fileContent;
+			} catch (IOException e) {
+				launcher.getEnvironment().debugMessage("[ERROR][Agent] while loading transformed " + className);
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			launcher.getEnvironment().debugMessage("[ERROR][Agent] while processing " + className);
+			e.printStackTrace();
+		}
+		return classfileBuffer;
+	}
+
+	/**
+	 * User defined filter to discard type that will not be transformed by the SpoonClassFileTransformer.
+	 * @param type type considered for transformation
+	 */
+	public abstract boolean accept(CtType type);
+
+
+	/**
+	 * User's implementation of transformation to apply on type.
+	 * @param type type to be transformed
+	 */
+	public abstract void transform(CtType type);
+}
