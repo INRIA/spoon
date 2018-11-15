aa9a73c9151bf5434b6a007a54c0d3e969d7ebdf
nharrand <nicolas.harrand@gmail.com>
2018-11-15 13:50:10 +0100

    add travis build

---
+++ chore/travis/travis-spoon-bytecode-verify.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script intends to be run on TravisCI
+# it runs verify and site maven goals
+# and to check documentation links
+
+# fails if anything fails
+set -e
+
+source /opt/jdk_switcher/jdk_switcher.sh
+
+pip install --user CommonMark==0.7.5 requests pygithub
+
+jdk_switcher use oraclejdk9
+
+cd spoon-bytecode
+
+mvn -Djava.src.version=1.9 verify license:check site javadoc:jar install -DskipTests -DadditionalJOption=-Xdoclint:none
+
+# checkstyle in src/tests
+mvn  checkstyle:checkstyle -Pcheckstyle-test
