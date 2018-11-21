f89d53ac6e91a8af1f903f6a013de2a8da5ffbd5
nharrand <nicolas.harrand@gmail.com>
2018-11-21 11:33:34 +0100

    more migration between poms

--- chore/travis/travis-spoon-bytecode-verify.sh
+++ chore/travis/travis-spoon-bytecode-verify.sh
@@ -9,7 +9,7 @@ set -e
 
 source /opt/jdk_switcher/jdk_switcher.sh
 
-pip install --user CommonMark==0.7.5 requests pygithub
+# pip install --user CommonMark==0.7.5 requests pygithub
 
 jdk_switcher use oraclejdk9
 
