543a3dc94e5013fa5bf2906cef223bd5417cf86c
nharrand <nicolas.harrand@gmail.com>
2018-11-26 17:52:38 +0100

    up

--- chore/travis/travis-verify.sh
+++ chore/travis/travis-verify.sh
@@ -29,13 +29,3 @@ mvn verify license:check site javadoc:jar install -DskipTests -DadditionalJOptio
 
 # checkstyle in src/tests
 mvn  checkstyle:checkstyle -Pcheckstyle-test
-
-cd ..
-
-# Maven 3.3.9
-wget archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
-unzip -qq apache-maven-3.3.9-bin.zip
-export M2_HOME=$PWD/apache-maven-3.3.9
-
-$M2_HOME/bin/mvn --version
-$M2_HOME/bin/mvn clean install -DskipTests=true
