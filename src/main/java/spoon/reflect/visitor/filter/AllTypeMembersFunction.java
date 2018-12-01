7ade8418cb3041607818690604a4b2ecd8ab8317
Pavel Vojtechovsky <pvojtechovsky@users.noreply.github.com>
2018-12-01 19:38:55 +0100

    feature: AllTypeMembersFunction has new mode SKIP_PRIVATE (#2803)

--- src/main/java/spoon/reflect/visitor/filter/AllTypeMembersFunction.java
+++ src/main/java/spoon/reflect/visitor/filter/AllTypeMembersFunction.java
@@ -19,9 +19,11 @@ package spoon.reflect.visitor.filter;
 import java.util.HashSet;
 import java.util.Set;
 
+import spoon.SpoonException;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.declaration.CtTypeInformation;
 import spoon.reflect.declaration.CtTypeMember;
+import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.visitor.chain.CtConsumableFunction;
 import spoon.reflect.visitor.chain.CtConsumer;
 import spoon.reflect.visitor.chain.CtQuery;
@@ -31,13 +33,27 @@ import spoon.reflect.visitor.chain.CtQueryable;
 /**
  * Expects {@link CtType} as input
  * and produces all {@link CtTypeMember}s declared in input class
- * or any super class or super interface
+ * or any super class or super interface.
+ * It first returns own type members, then type members of superclass, etc.
  */
 public class AllTypeMembersFunction implements CtConsumableFunction<CtTypeInformation>, CtQueryAware {
+
+	public enum Mode {
+		/**
+		 * Returns all type members - including private
+		 */
+		ALL,
+		/**
+		 * Returns only type members, which are accessible from the input `type`.
+		 * It means that inherited private type members are skipped
+		 */
+		SKIP_PRIVATE
+	}
 
 	private CtQuery query;
 	private final Class<?> memberClass;
 	private Set<String> distinctSet;
+	private Mode mode = Mode.ALL;
 
 	/**
 	 * returns all type members
@@ -69,12 +85,25 @@ public class AllTypeMembersFunction implements CtConsumableFunction<CtTypeInform
 
 	@Override
 	public void apply(CtTypeInformation input, final CtConsumer<Object> outputConsumer) {
+		String inputQName = input.getQualifiedName();
 		final CtQuery q = ((CtQueryable) input).map(new SuperInheritanceHierarchyFunction(distinctSet == null ? new HashSet<>() : distinctSet).includingSelf(true));
 		q.forEach(new CtConsumer<CtType<?>>() {
 			@Override
 			public void accept(CtType<?> type) {
-				for (CtTypeMember typeMember : type.getTypeMembers()) {
+				boolean isInputType = inputQName.equals(type.getQualifiedName());
+				loop: for (CtTypeMember typeMember : type.getTypeMembers()) {
 					if (memberClass == null || memberClass.isInstance(typeMember)) {
+						switch (mode) {
+						case ALL:
+							break;
+						case SKIP_PRIVATE:
+							if (typeMember.hasModifier(ModifierKind.PRIVATE) && !isInputType) {
+								continue loop;
+							}
+							break;
+						default:
+							throw new SpoonException("Unexpected mode " + mode);
+						}
 						outputConsumer.accept(typeMember);
 					}
 					if (query.isTerminated()) {
@@ -88,5 +117,13 @@ public class AllTypeMembersFunction implements CtConsumableFunction<CtTypeInform
 	@Override
 	public void setQuery(CtQuery query) {
 		this.query = query;
+	}
+
+	/**
+	 * @param mode defines how whether type members with limited visibility are returned
+	 */
+	public AllTypeMembersFunction setMode(Mode mode) {
+		this.mode = mode;
+		return this;
 	}
 }
