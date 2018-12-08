ada4b7bf6162bb97328d7bd321ecd8cfcaa67e29
Pavel Vojtechovsky <p.vojtechovsky@post.cz>
2018-12-08 10:31:58 +0100

    test: position of modifier after comment

---
+++ src/test/java/spoon/test/position/testclasses/Kokos.java
@@ -0,0 +1,11 @@
+package spoon.test.position.testclasses;
+
+import spoon.support.Experimental;
+
+/**
+ * This comment contains word public and that might confuse detection of public modifier position.
+ */
+@Experimental
+public interface Kokos {
+	String method();
+}
