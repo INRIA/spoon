c2ffc33abdbba219d829f156a57af08eaf4e80bd
Martin Monperrus <monperrus@users.noreply.github.com>
2018-12-10 16:11:23 +0100

    chore: clean old Travis scripts (#2829)
    cleaning under the carpet of #2782

--- chore/travis/travis-jdk9.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script intends to be run on TravisCI
-# It executes compile and test goals
-
-source /opt/jdk_switcher/jdk_switcher.sh
-
-jdk_switcher use oraclejdk9 && mvn -Djava.src.version=1.9 test
