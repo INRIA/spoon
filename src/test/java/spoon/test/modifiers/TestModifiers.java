3e7e007ed9a2e23de2c37325ad847b3ba6055763
nharrand <nicolas.harrand@gmail.com>
2019-01-09 11:26:08 +0100

    fixes modifiers preceeded by opening parenthesis

--- src/test/java/spoon/test/modifiers/TestModifiers.java
+++ src/test/java/spoon/test/modifiers/TestModifiers.java
@@ -39,7 +39,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-public class ModifiersTest {
+public class TestModifiers {
 
     @Test
     public void testNoSpaceBeforeFinal() {
--- src/test/java/spoon/test/modifiers/testclasses/NoSpaceBeforeFinal.java
+++ src/test/java/spoon/test/modifiers/testclasses/NoSpaceBeforeFinal.java
@@ -1,4 +1,15 @@
 package spoon.test.modifiers.testclasses;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class NoSpaceBeforeFinal {
+	public void m() {
+		try (final InputStream input = new FileInputStream("file.txt")) {
+			// some code
+		} catch (IOException ex) {
+			// some code
+		}
+	}
 }
