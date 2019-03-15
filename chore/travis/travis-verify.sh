7327d54ed7fc5b79914a7705db1ad3d8f7f72265
Martin Monperrus <monperrus@users.noreply.github.com>
2019-03-15 17:17:10 +0100

    chore: fix javadoc and reporting config and check it in CI (#2911)

--- chore/travis/travis-verify.sh
+++ chore/travis/travis-verify.sh
@@ -15,29 +15,31 @@ pip install --user CommonMark==0.7.5 requests pygithub
 
 jdk_switcher use oraclejdk9
 
-mvn -Djava.src.version=1.9 verify license:check site javadoc:jar install -DskipTests -DadditionalJOption=-Xdoclint:none
+# javadoc check is included in goal "site"
+# it's better to have the doclint here because the pom.xml config of javadoc is a nightmare
+mvn -Djava.src.version=1.9 verify license:check site install -DskipTests  -DadditionalJOption=-Xdoclint:syntax,-missing
 
 # checkstyle in src/tests
 mvn  checkstyle:checkstyle -Pcheckstyle-test
 
 python ./chore/check-links-in-doc.py
 
-#Spoon-decompiler
+# Spoon-decompiler
 cd spoon-decompiler
 
 mvn test
 
-mvn verify license:check site javadoc:jar install -DskipTests -DadditionalJOption=-Xdoclint:none
+mvn verify license:check javadoc:jar install -DskipTests
 
 # checkstyle in src/tests
 mvn  checkstyle:checkstyle -Pcheckstyle-test
 
-#Spoon-control-flow
+# Spoon-control-flow
 cd ../spoon-control-flow
 
 mvn test
 
-mvn verify license:check site javadoc:jar install -DskipTests -DadditionalJOption=-Xdoclint:none
+mvn verify license:check javadoc:jar install -DskipTests
 
 # checkstyle in src/tests
 mvn  checkstyle:checkstyle -Pcheckstyle-test
