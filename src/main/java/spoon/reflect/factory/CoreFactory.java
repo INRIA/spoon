b4d7c82ebbfc0ba9f75eee148a8c1f9b6f633875
Nicolas Harrand <nicolas.harrand@gmail.com>
2019-05-07 17:43:58 +0200

    Update CoreFactory.java

--- src/main/java/spoon/reflect/factory/CoreFactory.java
+++ src/main/java/spoon/reflect/factory/CoreFactory.java
@@ -530,8 +530,8 @@ public interface CoreFactory {
 	CtImport createImport();
 
 	/**
-	 * Creates an unresolved import. 
-	 * CtUnresolvedImport stores the original content of the imort as a String in order to be able 
+	 * Creates an unresolved import.
+	 * CtUnresolvedImport stores the original content of the imort as a String in order to be able
 	 * to restituate it when pretty printing.
 	 */
 	CtImport createUnresolvedImport();
