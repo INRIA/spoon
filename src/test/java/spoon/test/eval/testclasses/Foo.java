0c268054192be4fc20093324b9f63c463fd99c45
Michael Täge <mictaege@googlemail.com>
2019-05-18 12:47:54 +0200

    fix: handling of String const in getCorrespondingRuntimeObject/convertElementToRuntimeObject (#2978)
    fix #2977

--- src/test/java/spoon/test/eval/testclasses/Foo.java
+++ src/test/java/spoon/test/eval/testclasses/Foo.java
@@ -1,5 +1,7 @@
 package spoon.test.eval.testclasses;
 
+import java.io.File;
+
 public class Foo {
 	final boolean b0 = true && false;
 	boolean b2 = true; // not final so not considered
@@ -8,7 +10,7 @@ public class Foo {
 	boolean b5 = true ^ false; // exec BITXOR
 	final boolean b6 = b0;
 	final int i1 = Integer.MAX_VALUE; // access to static field
-
+	final String str1 = File.pathSeparator;
 	void foo() {
 		boolean b1 = true ? false || b0 : b2; // will be simplified to "false"
 	}
