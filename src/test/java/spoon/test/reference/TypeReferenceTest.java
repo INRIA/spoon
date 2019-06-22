64fbda2b963a7ff447ca89420b2055d9cad95b95
Egor18 <egorbr1801@mail.ru>
2019-06-22 19:07:51 -0400

    fix: NPE in canAccess()

--- src/test/java/spoon/test/reference/TypeReferenceTest.java
+++ src/test/java/spoon/test/reference/TypeReferenceTest.java
@@ -21,6 +21,8 @@ import spoon.Launcher;
 import spoon.SpoonModelBuilder;
 import spoon.compiler.SpoonResource;
 import spoon.compiler.SpoonResourceHelper;
+import spoon.reflect.CtModel;
+import spoon.reflect.code.CtAssignment;
 import spoon.reflect.code.CtConstructorCall;
 import spoon.reflect.code.CtExpression;
 import spoon.reflect.code.CtFieldRead;
@@ -676,5 +678,27 @@ public class TypeReferenceTest {
 		typeRef.getPackage().setImplicit(false);
 		assertFalse(typeRef.isImplicitParent());
 		assertFalse(typeRef.getPackage().isImplicit());
+	}
+
+	@Test
+	public void testIsInTheSamePackageNoClasspath() {
+		// contract: we should not get NPE within canAccess() in noclasspath mode
+		final Launcher launcher = new Launcher();
+		launcher.addInputResource("./src/test/resources/noclasspath/A5.java");
+		launcher.getEnvironment().setNoClasspath(true);
+		CtModel model = launcher.buildModel();
+		CtType<?> type = model.getAllTypes().stream().findFirst().get();
+
+		List<CtLocalVariable> vars = type.getElements(new TypeFilter<>(CtLocalVariable.class));
+		List<CtField> fields = type.getElements(new TypeFilter<>(CtField.class));
+		List<CtInvocation> invocations = type.getElements(new TypeFilter<>(CtInvocation.class));
+
+		// test null type
+		CtTypeReference tr1 = invocations.get(1).getType();
+		assertTrue(fields.get(0).getReference().getType().canAccess(tr1));
+
+		// test type with null package
+		CtTypeReference tr2 = fields.get(0).getType();
+		assertTrue(vars.get(0).getReference().getType().canAccess(tr2));
 	}
 }
