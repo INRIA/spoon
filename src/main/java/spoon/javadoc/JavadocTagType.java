32556519874e2fa6370b919a401f1bee3f6f9e80
Egor18 <egorbr1801@mail.ru>
2019-06-24 22:53:47 -0400

    Fix checkstyle

--- src/main/java/spoon/javadoc/JavadocTagType.java
+++ src/main/java/spoon/javadoc/JavadocTagType.java
@@ -11,55 +11,55 @@ import static spoon.reflect.code.CtJavaDocTag.JAVADOC_TAG_PREFIX;
 
 public enum JavadocTagType {
 
-    AUTHOR("author"),
-    DEPRECATED("deprecated"),
-    EXCEPTION("exception"),
-    PARAM("param"),
-    RETURN("return"),
-    SEE("see"),
-    SERIAL("serial"),
-    SERIAL_DATA("serialData"),
-    SERIAL_FIELD("serialField"),
-    SINCE("since"),
-    THROWS("throws"),
-    VERSION("version"),
-    UNKNOWN("unknown");
+	AUTHOR("author"),
+	DEPRECATED("deprecated"),
+	EXCEPTION("exception"),
+	PARAM("param"),
+	RETURN("return"),
+	SEE("see"),
+	SERIAL("serial"),
+	SERIAL_DATA("serialData"),
+	SERIAL_FIELD("serialField"),
+	SINCE("since"),
+	THROWS("throws"),
+	VERSION("version"),
+	UNKNOWN("unknown");
 
-    JavadocTagType(String name) {
-        this.name = name;
-    }
+	JavadocTagType(String name) {
+		this.name = name;
+	}
 
-    private String name;
+	private String name;
 
-    /**
-     * Get tag name
-     * @return name
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * Get tag name
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * Return true if the tag can have a parameter
-     * @return true if the tag can have a parameter
-     */
-    public boolean hasParam() {
-        return this == PARAM || this == THROWS || this == EXCEPTION;
-    }
+	/**
+	 * Return true if the tag can have a parameter
+	 * @return true if the tag can have a parameter
+	 */
+	public boolean hasParam() {
+		return this == PARAM || this == THROWS || this == EXCEPTION;
+	}
 
-    /**
-     * Get the tag type associated to a name
-     * @param tagName the tag name
-     * @return the tag type
-     */
-    public static JavadocTagType fromName(String tagName) {
-        return Arrays.stream(JavadocTagType.values())
-            .filter(v -> v.name.equals(tagName))
-            .findFirst().orElse(UNKNOWN);
-    }
+	/**
+	 * Get the tag type associated to a name
+	 * @param tagName the tag name
+	 * @return the tag type
+	 */
+	public static JavadocTagType fromName(String tagName) {
+		return Arrays.stream(JavadocTagType.values())
+				.filter(v -> v.name.equals(tagName))
+				.findFirst().orElse(UNKNOWN);
+	}
 
-    @Override
-    public String toString() {
-        return JAVADOC_TAG_PREFIX + name().toLowerCase();
-    }
+	@Override
+	public String toString() {
+		return JAVADOC_TAG_PREFIX + name().toLowerCase();
+	}
 }
