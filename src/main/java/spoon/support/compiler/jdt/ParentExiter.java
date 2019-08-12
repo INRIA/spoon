40c3fd5b0222e8aaf99fa1b8a6da9480e4da7222
Egor18 <egorbr1801@mail.ru>
2019-08-12 10:16:42 -0400

    Fix setType

--- src/main/java/spoon/support/compiler/jdt/ParentExiter.java
+++ src/main/java/spoon/support/compiler/jdt/ParentExiter.java
@@ -423,7 +423,7 @@ public class ParentExiter extends CtInheritanceScanner {
 				op.setKind(BinaryOperatorKind.PLUS);
 				op.setLeftHandOperand(operator.getRightHandOperand());
 				op.setRightHandOperand((CtExpression<?>) child);
-				//op.setType((CtTypeReference) operator.getFactory().Type().STRING);
+				op.setType((CtTypeReference) operator.getFactory().Type().STRING.clone());
 				operator.setRightHandOperand(op);
 				int[] lineSeparatorPositions = jdtTreeBuilder.getContextBuilder().getCompilationUnitLineSeparatorPositions();
 				SourcePosition leftPosition = op.getLeftHandOperand().getPosition();
