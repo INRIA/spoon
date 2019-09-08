3ce30729dc006724d4834d85ed0755e592c9091b
Martin Monperrus <martin.monperrus@gnieh.org>
2019-09-08 10:44:29 +0200

    up

--- chore/travis/travis-coverage.sh
+++ chore/travis/travis-coverage.sh
@@ -6,8 +6,4 @@
 # Note thet Coveralls/Jacoco does not work on JDK9, see https://github.com/trautonen/coveralls-maven-plugin/issues/112
 set -e
 
-wget https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
-chmod +x install-jdk.sh
-
-source ./install-jdk.sh -f 8 -c
 mvn -Pcoveralls test jacoco:report coveralls:report --fail-never
--- chore/travis/travis-maven339-jdk8.sh
+++ chore/travis/travis-maven339-jdk8.sh
@@ -4,9 +4,6 @@ set -e
 # This script intends to check if Spoon can be compiled
 # using Maven 3.3.9
 
-source /opt/jdk_switcher/jdk_switcher.sh
-
-jdk_switcher use oraclejdk8
 wget archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
 unzip -qq apache-maven-3.3.9-bin.zip
 export M2_HOME=$PWD/apache-maven-3.3.9
