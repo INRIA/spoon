34e39c1e696d12e982362a4aba3a74da9878b328
Martin Monperrus <monperrus@users.noreply.github.com>
2019-09-09 18:12:38 +0200

    Update travis-extra.sh

--- chore/travis/travis-extra.sh
+++ chore/travis/travis-extra.sh
@@ -25,35 +25,40 @@ mvn  checkstyle:checkstyle -Pcheckstyle-test
 
 python ./chore/check-links-in-doc.py
 
-# for the module, we use Java11
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
-
 ##################################################################
 # Spoon-decompiler
 ##################################################################
 cd spoon-decompiler
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
 
 # always depends on the latest snapshot, just installed with "mvn install" above
 mvn versions:use-latest-versions -DallowSnapshots=true -Dincludes=fr.inria.gforge.spoon
 git diff
 
 mvn test
+mvn checkstyle:checkstyle license:check
 
 ##################################################################
 # Spoon-control-flow
 ##################################################################
 cd ../spoon-control-flow
 
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
+
 # always depends on the latest snapshot, just installed with "mvn install" above
 mvn versions:use-latest-versions -DallowSnapshots=true -Dincludes=fr.inria.gforge.spoon
 git diff
 
 mvn test
+mvn checkstyle:checkstyle license:check
 
 ##################################################################
 # Spoon-dataflow
 ##################################################################
 cd ../spoon-dataflow
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 
 # download and install z3 lib
 wget https://github.com/Z3Prover/z3/releases/download/z3-4.8.4/z3-4.8.4.d6df51951f4c-x64-ubuntu-14.04.zip
@@ -68,6 +73,8 @@ export LD_LIBRARY_PATH=./z3-4.8.4.d6df51951f4c-x64-ubuntu-14.04/bin
 # Spoon-visualisation
 ##################################################################
 cd ../spoon-visualisation
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 
 wget https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
 chmod +x install-jdk.sh
