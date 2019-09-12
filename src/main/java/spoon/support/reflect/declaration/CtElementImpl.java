bd773fdc4d9e727d6a03144901948bb7c70a58bf
Martin Monperrus <martin.monperrus@gnieh.org>
2019-09-12 10:39:56 +0200

    Commit by Martin Monperrus on 12 September 2019 😺

--- src/main/java/spoon/support/reflect/declaration/CtElementImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtElementImpl.java
@@ -286,7 +286,7 @@ public abstract class CtElementImpl implements CtElement, Serializable {
 	@Override
 	public String toString() {
 		DefaultJavaPrettyPrinter printer = (DefaultJavaPrettyPrinter) getFactory().getEnvironment().createPrettyPrinter();
-		if (getFactory().getEnvironment().getPrettyPrintingMode().equals(Environment.PRETTY_PRINTING_MODE.VANILLA)) {
+		if (!getFactory().getEnvironment().getPrettyPrintingMode().equals(Environment.PRETTY_PRINTING_MODE.AUTOIMPORT)) {
 			printer.setIgnoreImplicit(true);
 		}
 		String errorMessage = "";
