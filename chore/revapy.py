28004dcbb3ae52835436e4fabfda82b7722c3bd7
Martin Monperrus <martin.monperrus@gnieh.org>
2019-09-19 20:55:04 +0200

    @monperrus at work 😕

--- chore/revapy.py
+++ chore/revapy.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python
-# Retrieve revapi report from target and publish it as PR comment
-# This script intends to be launched by a webhooks and take as parameter the hook payload
-# see: https://developer.github.com/v3/activity/events/types/#pullrequestevent
+# Computes the revapi report and publish it as PR comment
 
-import os, sys, json
+import os, sys, json, re
 from github import *
 
 revapi_file = "./target/revapi_report.md"
-accepted_actions = ["opened", "synchronize"]
+accepted_actions = ["pull_request"]
 
 args = sys.argv
 
-def usage():
-    print "Usage: "+str(args[0])+ " <github_login> <github_token> <PR_payload_path>"
-    exit(1)
-
-if (args.__len__() < 4):
-    print "Error in the args number"
-    usage()
+os.system("mvn -U revapi:report")
 
 if (not(os.path.isfile(revapi_file))):
     print "Revapi report file cannot be found at the following location: "+revapi_file
     exit(1)
 
-if (not(os.path.isfile(args[3]))):
-    print "JSON payload file cannot be found at the following location: "+args[3]
-    exit(1)
 
 file_content = ""
 with open(revapi_file) as f:
@@ -36,8 +25,10 @@ if (file_content == ""):
     print "Revapi report is empty"
     exit(1)
 
-login = args[1]
-token = args[2]
+if (len(re.findall("changes.*: [123456789]"))==0): exit(0)
+
+login = os.environ["GITHUB_AUTH_USER"]
+token = os.environ["GITHUB_AUTH_TOKEN"]
 
 try:
     gh = Github(login,token)
@@ -46,19 +37,10 @@ except GithubException as e:
     print e
     exit(1)
 
-payload_string = ""
-with open(args[3]) as f:
-    for line in f:
-        payload_string += line
 
-if (payload_string == ""):
-    print "Payload is empty"
-    exit(1)
-
-payloadJSON = json.loads(payload_string)
-repo_name = payloadJSON['repository']['full_name']
-pr_id = payloadJSON['pull_request']['number']
-action = payloadJSON['action']
+repo_name = os.environ["TRAVIS_REPO_SLUG"]
+pr_id = os.environ["TRAVIS_PULL_REQUEST"]
+action = os.environ["TRAVIS_EVENT_TYPE"]
 
 if (action in accepted_actions):
     try:
--- chore/travis/travis-extra.sh
+++ chore/travis/travis-extra.sh
@@ -16,6 +16,9 @@ export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
 
 mvn -version
 
+# compute the revapi report
+python chore/revapy.py
+
 # javadoc check is included in goal "site"
 # it's better to have the doclint here because the pom.xml config of javadoc is a nightmare
 mvn -q -Djava.src.version=1.8 verify license:check site install -DskipTests  -DadditionalJOption=-Xdoclint:syntax,-missing
