ddbb2eb65bca1141fa5827b5d9c417b48d3a2d4f
Martin Monperrus <martin.monperrus@gnieh.org>
2019-09-28 09:32:18 +0200

    @monperrus at work 🐢

--- src/main/java/spoon/support/StandardEnvironment.java
+++ src/main/java/spoon/support/StandardEnvironment.java
@@ -654,6 +654,7 @@ private transient  ClassLoader inputClassloader;
 				//compute final imports
 				new ImportCleaner().setImportComparator(new DefaultImportComparator())
 		));
+		printer.setForceFullyQualified(false);
 		printer.setPreprocessors(preprocessors);
 		return printer;
 	}
