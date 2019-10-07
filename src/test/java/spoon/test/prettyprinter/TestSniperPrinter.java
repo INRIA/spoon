d62e42a5cd270dc5ecaf050f8ee9b2a54afbca5f
Haris Adzemovic <adzemovic.haris@gmail.com>
2019-10-07 18:44:52 +0200

    Fix path

--- src/test/java/spoon/test/prettyprinter/TestSniperPrinter.java
+++ src/test/java/spoon/test/prettyprinter/TestSniperPrinter.java
@@ -171,7 +171,7 @@ public class TestSniperPrinter {
 	@Test
 	public void testBinaryOperatorElement() throws Exception {
 		final String[] args = {
-				"-i", "/Users/haris/Documents/Skola/Exjobb/programming/spoon_fork/src/test/java/spoon/test/processing/testclasses/ElementScan.java",
+				"-i", "./src/test/java/spoon/test/processing/testclasses/ElementScan.java",
 				"-o", "target/spooned/",
 				"-p", "spoon.test.processing.processors.ElementScanProcessor",
 				"--compile"
