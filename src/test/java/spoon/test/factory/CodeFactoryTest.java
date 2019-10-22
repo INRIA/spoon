c373fb3e09fdfbc28fba43e5d6b683cfa7fc649a
Muhammet Ali AKBAY <maakbay@gmail.com>
2019-10-22 16:32:02 +0300

    fix(CodeFactory): createVariableAssignment now creates a CtVariableWrite (#3157)
    fix#3155

--- src/test/java/spoon/test/factory/CodeFactoryTest.java
+++ src/test/java/spoon/test/factory/CodeFactoryTest.java
@@ -17,8 +17,13 @@
 package spoon.test.factory;
 
 import org.junit.Test;
+import spoon.reflect.code.CtAssignment;
 import spoon.reflect.code.CtThisAccess;
 import spoon.reflect.code.CtTypeAccess;
+import spoon.reflect.code.CtVariableWrite;
+import spoon.reflect.declaration.CtField;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.factory.Factory;
 import spoon.reflect.reference.CtTypeReference;
 
@@ -28,6 +33,8 @@ import static org.junit.Assert.assertTrue;
 import static spoon.testing.utils.ModelUtils.createFactory;
 
 public class CodeFactoryTest {
+	int i;
+
 	@Test
 	public void testThisAccess() {
 		final Factory factory = createFactory();
@@ -37,5 +44,18 @@ public class CodeFactoryTest {
 		assertNotNull(thisAccess.getTarget());
 		assertTrue(thisAccess.getTarget() instanceof CtTypeAccess);
 		assertEquals(type, ((CtTypeAccess) thisAccess.getTarget()).getAccessedType());
+	}
+
+	@Test
+	public void testCreateVariableAssignement() {
+		Factory factory = createFactory();
+		CtType t = factory.Class().create("my.MyClass");
+		CtField f = factory.createCtField("f", factory.Type().booleanPrimitiveType(), "false", ModifierKind.PRIVATE);
+		t.addField(f);
+		CtAssignment va = factory.Code().createVariableAssignment(f.getReference(),false,factory.createLiteral(true));
+
+		//Variable assignment's assignee is a CtVariableWrite that point toward the right variable.
+		assertTrue(va.getAssigned() instanceof CtVariableWrite);
+		assertEquals(f.getReference(), ((CtVariableWrite) va.getAssigned()).getVariable());
 	}
 }
