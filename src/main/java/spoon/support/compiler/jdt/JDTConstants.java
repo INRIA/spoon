92140411f3a5b57af07c2ca8d61c1d51ad6d45a1
Marcel Manseer <marcel@divine.engineering>
2019-12-02 17:46:29 +0100

    Added whitespace to a private constructor.

--- src/main/java/spoon/support/compiler/jdt/JDTConstants.java
+++ src/main/java/spoon/support/compiler/jdt/JDTConstants.java
@@ -11,7 +11,9 @@ package spoon.support.compiler.jdt;
  */
 public class JDTConstants {
 
-	private JDTConstants() {}
+	private JDTConstants() {
+
+	}
 
 	public static final char[] MODULE_INFO_FILE_NAME = "module-info.java".toCharArray();
 	public static final char[] MODULE_INFO_CLASS_NAME = "module-info.class".toCharArray();
