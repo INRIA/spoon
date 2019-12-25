81f6f185a6fc340f41dc076249d2399907a16ffc
moraispgsi <moraispgis@gmail.com>
2019-12-25 15:02:17 +0000

    Travis test.

--- chore/travis/travis-leafactor.sh
+++ chore/travis/travis-leafactor.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-set -e
-
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
-
-mvn -version
-
-wget archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
-unzip -qq apache-maven-3.3.9-bin.zip
-export M2_HOME=$PWD/apache-maven-3.3.9
-
-$M2_HOME/bin/mvn --version
-#$M2_HOME/bin/mvn clean install
 
 export LEAFACTOR_VERSION=1
 export ANDROID_SDK_DIR=/usr/local/android-sdk
@@ -24,8 +12,8 @@ echo "targetCompatibility=7" >> gradle.properties
 
 cd ..
 cd sample
-echo "sdk.dir=${ANDROID_SDK_DIR}" > gradle.properties
-echo "leafactor.dir=../../leafactor-ci/build/libs/leafactor-${LEAFACTOR_VERSION}.jar" >> gradle.properties
+echo "sdk.dir=${ANDROID_SDK_DIR}" > local.properties
+echo "leafactor.dir=../../leafactor-ci/build/libs/leafactor-${LEAFACTOR_VERSION}.jar" >> local.properties
 ./gradlew app:build
 ./gradlew app:refactor
 
