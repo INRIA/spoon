fe157b226b75d38eca9bae851bb51da0abf534a2
moraispgsi <moraispgis@gmail.com>
2019-12-25 14:23:43 +0000

    Travis CI test.

--- chore/travis/travis-leafactor.sh
+++ chore/travis/travis-leafactor.sh
@@ -20,13 +20,14 @@ echo "version=1.0.${LEAFACTOR_VERSION}" > gradle.properties
 echo "group=com.leafactorci" >> gradle.properties
 echo "sourceCompatibility=7" >> gradle.properties
 echo "targetCompatibility=7" >> gradle.properties
-gradlew :jar
+./gradlew :jar
 
 cd ..
 cd sample
 echo "sdk.dir=${ANDROID_SDK_DIR}" > gradle.properties
 echo "leafactor.dir=../../leafactor-ci/build/libs/leafactor-${LEAFACTOR_VERSION}.jar" >> gradle.properties
-gradlew app:refactor
+./gradlew app:build
+./gradlew app:refactor
 
 
 
