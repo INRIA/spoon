314a266e275f43922a79d67552a8fa1e361aaa83
moraispgsi <moraispgis@gmail.com>
2019-12-29 14:49:23 +0000

    Test with spoon-core jar.

--- chore/travis/travis-leafactor.sh
+++ chore/travis/travis-leafactor.sh
@@ -3,6 +3,22 @@
 export LEAFACTOR_VERSION=1.0.1
 export ANDROID_SDK_DIR=/usr/local/android-sdk
 
+set -e
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
+
+mvn -version
+
+wget archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
+unzip -qq apache-maven-3.3.9-bin.zip
+export M2_HOME=$PWD/apache-maven-3.3.9
+
+$M2_HOME/bin/mvn --version
+$M2_HOME/bin/mvn clean install
+
+# Build spoon-core
+mvn jar:jar
+
 echo 'PREPARING LEAFACTOR JAR'
 cd leafactor-ci
 echo "version=${LEAFACTOR_VERSION}" > gradle.properties
