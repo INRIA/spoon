1885f251698d6bed4a92d5d361fe7b5dd912cac0
Martin Wittlinger <wittlinger.martin@gmail.com>
2019-12-30 13:58:08 +0100

    move test case to correct place

--- src/test/java/spoon/test/refactoring/RefactoringTest.java
+++ src/test/java/spoon/test/refactoring/RefactoringTest.java
@@ -16,13 +16,28 @@
  */
 package spoon.test.refactoring;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+
 import org.junit.Test;
+
 import spoon.Launcher;
 import spoon.refactoring.Refactoring;
 import spoon.reflect.code.BinaryOperatorKind;
 import spoon.reflect.code.CtBinaryOperator;
 import spoon.reflect.code.CtInvocation;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.Query;
 import spoon.reflect.visitor.filter.AbstractFilter;
@@ -30,13 +45,6 @@ import spoon.reflect.visitor.filter.AbstractReferenceFilter;
 import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.test.refactoring.processors.ThisTransformationProcessor;
 import spoon.test.refactoring.testclasses.AClass;
-
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 public class RefactoringTest {
 	@Test
@@ -135,9 +143,31 @@ public class RefactoringTest {
 
 	@Test
 	public void testremoveDeprecatedMethods() {
-		// this shows that we can remove deprecated methods
-		// with Spoon
-		// ... and that the sniper mode works well-enough to be applied on Spoon itself
-		// Refactoring.removeDeprecatedMethods("src/main/java");
+		// clean dir if exists
+		try {
+			Files.walk(Paths.get("src\\test\\resources\\deprecated\\result")).sorted(Comparator.reverseOrder())
+					.map(Path::toFile).forEach(File::delete);
+		} catch (Exception e) {
+			// error is kinda okay
+		}
+		// create Spoon
+		String input = "src\\test\\resources\\deprecated\\input";
+		String resultPath = "src\\test\\resources\\deprecated\\result";
+		String correctResultPath = "src\\test\\resources\\deprecated\\correctResult";
+		Launcher spoon = new Launcher();
+
+		spoon.addInputResource(correctResultPath);
+		List<CtMethod<?>> correctResult = spoon.buildModel().getElements(new TypeFilter<>(CtMethod.class));
+		// save Methods before cleaning
+
+		// now refactor code
+		Refactoring.removeDeprecatedMethods(input, resultPath);
+
+		// verify result
+		spoon = new Launcher();
+		spoon.addInputResource(resultPath);
+		List<CtMethod<?>> calculation = spoon.buildModel().getElements(new TypeFilter<>(CtMethod.class));
+		assertTrue(correctResult.containsAll(calculation));
+		assertTrue(calculation.containsAll(calculation));
 	}
 }
