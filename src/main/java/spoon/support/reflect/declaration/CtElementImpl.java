ced8c3935fcaf98266e43300e5b7079c959c1544
nharrand <nicolas.harrand@gmail.com>
2020-01-09 18:04:46 +0100

    checkstyle

--- src/main/java/spoon/support/reflect/declaration/CtElementImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtElementImpl.java
@@ -7,7 +7,6 @@ package spoon.support.reflect.declaration;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
 import spoon.Launcher;
 import spoon.reflect.ModelElementContainerDefaultCapacities;
 import spoon.reflect.annotations.MetamodelPropertyField;
