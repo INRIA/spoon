99775fcf4e51ef73c80e2ef04edd6131615885e2
Nicolas Harrand <nicolas.harrand@gmail.com>
2020-01-10 11:19:40 +0100

    refactor: migrate to org.apache.logging.log4j:log4j-core:2.13.0 (#3202)

--- src/test/java/spoon/test/processing/processors/TestProcessor.java
+++ src/test/java/spoon/test/processing/processors/TestProcessor.java
@@ -18,7 +18,7 @@ package spoon.test.processing.processors;
 
 import java.util.Date;
 
-import org.apache.log4j.Level;
+import org.apache.logging.log4j.Level;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.cu.CompilationUnit;
 import spoon.reflect.declaration.CtClass;
