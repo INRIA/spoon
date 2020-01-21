7a58221e9e89ec1ee7d7cab803681415f44ee5a5
Martin Monperrus <monperrus@users.noreply.github.com>
2020-01-21 09:42:14 +0100

    feat: bridge logger calls to slf4j-api (#3208)

--- src/test/java/spoon/test/logging/LogTest.java
+++ src/test/java/spoon/test/logging/LogTest.java
@@ -16,16 +16,24 @@
  */
 package spoon.test.logging;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.ArgumentCaptor;
 import spoon.Launcher;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
 @RunWith(Parameterized.class)
 public class LogTest {
@@ -33,12 +41,12 @@ public class LogTest {
 	@Parameterized.Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {
-				{Level.ALL, true, true, true, true },
-				{Level.DEBUG, true, true, true, true },
-				{Level.INFO, true, true, true, false },
-				{Level.WARN, false, true, true, false },
-				{Level.ERROR, false, false, true, false },
-				{Level.OFF, false, false, false, false }
+				{Level.ALL, 5 },
+				{Level.DEBUG, 5 },
+				{Level.INFO, 1 },
+				{Level.WARN, 0 },
+				{Level.ERROR, 0 },
+				{Level.OFF, 0 }
 		});
 	}
 
@@ -46,33 +54,37 @@ public class LogTest {
 	public Level level;
 
 	@Parameterized.Parameter(1)
-	public boolean isInfo;
-
-	@Parameterized.Parameter(2)
-	public boolean isWarn;
-
-	@Parameterized.Parameter(3)
-	public boolean isError;
-
-	@Parameterized.Parameter(4)
-	public boolean isDebug;
+	public int nbLogMessagesMinimum;
 
 	@Test
-	public void testAllLevelsForLogs() {
+	public void testAllLevelsForLogs() throws Exception {
 		final Launcher launcher = new Launcher();
+
+		Logger logger = mock(Logger.class);
+		FieldUtils.writeField(launcher.getEnvironment(), "logger", logger, true);
+
+		ArgumentCaptor<String> valueCaptureMessage = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<Level> valueCaptureLevel = ArgumentCaptor.forClass(Level.class);
+
+		doNothing().when(logger).log(valueCaptureLevel.capture(), valueCaptureMessage.capture());
+
 		launcher.setArgs(new String[] {
 				"-i", "./src/test/java/spoon/test/logging",
 				"--level", level.toString()
 		});
-		assertEquals(level, launcher.getFactory().getEnvironment().getLevel());
-		Launcher.LOGGER.info("Log info");
-		Launcher.LOGGER.warn("Log warn");
-		Launcher.LOGGER.error("Log error");
-		Launcher.LOGGER.debug("Log debug");
 
-		assertEquals(isInfo, Launcher.LOGGER.getLevel().isLessSpecificThan(Level.INFO));
-		assertEquals(isWarn, Launcher.LOGGER.getLevel().isLessSpecificThan(Level.WARN));
-		assertEquals(isError, Launcher.LOGGER.getLevel().isLessSpecificThan(Level.ERROR));
-		assertEquals(isDebug, Launcher.LOGGER.getLevel().isLessSpecificThan(Level.DEBUG));
+		// launcher provides two logging methods
+		launcher.getEnvironment().debugMessage("debugMessage");
+		launcher.getEnvironment().reportProgressMessage("reportProgressMessage");
+
+		// contract: the --level arguments sets the level
+		assertEquals(level, launcher.getFactory().getEnvironment().getLevel());
+
+		// contract: the number of messages increases with the log level
+		// System.out.println(level+ " " + valueCaptureMessage.getAllValues().size());
+		assertTrue(valueCaptureMessage.getAllValues().size() >= nbLogMessagesMinimum);
+
+
+
 	}
 }
