4e8d155d8f638c726e6dfe11360224849f347ff6
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-02-10 22:44:14 +0100

    fix doclint

--- src/main/java/spoon/refactoring/Refactoring.java
+++ src/main/java/spoon/refactoring/Refactoring.java
@@ -209,7 +209,7 @@ public final class Refactoring {
 	/**
 	 * Removes all deprecated methods for all java files in the given path.
 	 *
-	 * <b>Only use it if you have >= jdk9. Older jdk versions may result in wrong results.</b>
+	 * <b>Only use it if you have &gt; jdk8. Older jdk versions may result in wrong results.</b>
 	 * Only removes deprecated methods, that are no longer invoked by any method or
 	 * field. If a deprecated method is invoked by a deprecated method only, which
 	 * can be removed, the deprecated method is removed too.
@@ -226,7 +226,7 @@ public final class Refactoring {
 	/**
 	 * Removes all deprecated methods for all java files in the given path.
 	 *
-	 * <b>Only use it if you have >= jdk9. Older jdk versions may result in wrong results.</b>
+	 * <b>Only use it if you have &gt; jdk8. Older jdk versions may result in wrong results.</b>
 	 * Only removes deprecated methods, that are no longer invoked by any method or
 	 * field. If a deprecated method is invoked by a deprecated method only, which
 	 * can be removed, the deprecated method is removed too.
