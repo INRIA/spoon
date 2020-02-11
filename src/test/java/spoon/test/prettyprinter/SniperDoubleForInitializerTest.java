0a0a88b1c72d7c9e11a0f01a9665d4128478dd7a
Martin Monperrus <monperrus@users.noreply.github.com>
2020-02-11 09:59:03 +0000

    Update SniperDoubleForInitializerTest.java

--- src/test/java/spoon/test/prettyprinter/SniperDoubleForInitializerTest.java
+++ src/test/java/spoon/test/prettyprinter/SniperDoubleForInitializerTest.java
@@ -48,7 +48,7 @@ public class SniperDoubleForInitializerTest {
 
     launcher.process();
     launcher.prettyprint();
-    // Verify result file exist and is not empty
+    // Verify result file exists and is not empty
     assertThat("Output file for " + path + " should exist", OUTPUT_PATH.resolve(path).toFile().exists(),
         CoreMatchers.equalTo(true));
 
