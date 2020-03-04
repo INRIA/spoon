f7130f4ce330735707be95c21b32a989c8c5c35c
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-03-05 00:05:21 +0100

    restore testcase

--- src/test/java/spoon/Issue3275Test.java
+++ src/test/java/spoon/Issue3275Test.java
@@ -1,12 +1,10 @@
 package spoon;
 
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.CtModel;
 import spoon.reflect.reference.CtTypeReference;
-import spoon.test.UnresolvedBug;
 
 /**
  * Issue3275
@@ -14,7 +12,6 @@ import spoon.test.UnresolvedBug;
 public class Issue3275Test {
 	// the following code reproduces the behavior from the issue
 	@Test
-	@Category(UnresolvedBug.class)
 	public void test() {
 		CtModel model = new FluentLauncher().inputResource("src/test/resources/issue3275/BOMCostPrice.java")
 				.noClasspath(true)
