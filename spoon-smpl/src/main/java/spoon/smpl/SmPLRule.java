8f82362c67791ee01689211637b97f86c8859ddf
mkforsb <mkforsb@gmail.com>
2020-03-27 13:28:07 +0100

    introduce SmPLRule

---
+++ spoon-smpl/src/main/java/spoon/smpl/SmPLRule.java
@@ -0,0 +1,52 @@
+package spoon.smpl;
+
+import spoon.smpl.formula.Formula;
+import spoon.smpl.formula.MetavariableConstraint;
+
+import java.util.Map;
+
+/**
+ * An SmPLRule represents a single named or anonymous SmPL rule consisting of a formula, a
+ * set of metavariables with respective constraints, and a set of current metavariable bindings
+ * from the most recently matched code.
+ */
+public interface SmPLRule {
+    // public boolean matchesMethodHeader
+    // public getMetavariableBindings
+    // public reset
+
+    /**
+     * Set the name of the rule.
+     * @param name name of the rule, or null for anonymous rule
+     */
+    public void setName(String name);
+
+    /**
+     * Get the name of the rule.
+     * @return name of the rule, or null if the rule is anonymous
+     */
+    public String getName();
+
+    /**
+     * Get the formula of the rule.
+     * @return formula of the rule
+     */
+    public Formula getFormula();
+
+    /**
+     * Get the metavariable names and their respective constraints.
+     * @return Metavariable names and their respective constraints
+     */
+    public Map<String, MetavariableConstraint> getMetavariableConstraints();
+
+    /**
+     * Get the current metavariable bindings.
+     * @return the current metavariable bindings, or null if there are none
+     */
+    public Map<String, Object> getMetavariableBindings();
+
+    /**
+     * Reset the metavariable bindings.
+     */
+    public void reset();
+}
---
+++ spoon-smpl/src/main/java/spoon/smpl/SmPLRuleImpl.java
@@ -0,0 +1,105 @@
+package spoon.smpl;
+
+import org.apache.commons.lang3.NotImplementedException;
+import spoon.smpl.formula.Formula;
+import spoon.smpl.formula.MetavariableConstraint;
+
+import java.util.Map;
+
+/**
+ * Implementation of SmPLRule.
+ */
+public class SmPLRuleImpl implements SmPLRule {
+    /**
+     * Create a new SmPLRule.
+     * @param formula Formula of the rule
+     * @param metavars Metavariable names and their respective constraints
+     */
+    public SmPLRuleImpl(Formula formula, Map<String, MetavariableConstraint> metavars) {
+        this.formula = formula;
+        this.metavars = metavars;
+        this.name = null;
+    }
+
+    /**
+     * Set the name of the rule.
+     * @param name name of the rule, or null for anonymous rule
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the name of the rule.
+     * @return name of the rule, or null if the rule is anonymous
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Get the formula of the rule.
+     * @return formula of the rule
+     */
+    @Override
+    public Formula getFormula() {
+        return formula;
+    }
+
+    /**
+     * Get the set of metavariable names and their respective constraints.
+     * @return Metavariable names and their respective constraints
+     */
+    @Override
+    public Map<String, MetavariableConstraint> getMetavariableConstraints() {
+        return metavars;
+    }
+
+    /**
+     * Get the current metavariable bindings.
+     * @return Current metavariable bindings, or null if there are none
+     */
+    @Override
+    public Map<String, Object> getMetavariableBindings() {
+        throw new NotImplementedException("Not implemented");
+    }
+
+    /**
+     * Reset the metavariable bindings.
+     */
+    @Override
+    public void reset() {
+        throw new NotImplementedException("Not implemented");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("SmPLRule(name=")
+                .append(name)
+                .append(", metavars=")
+                .append(metavars.toString())
+                .append(", formula=")
+                .append(formula.toString())
+                .append(")");
+
+        return sb.toString();
+    }
+
+    /**
+     * Name of the rule.
+     */
+    private String name;
+
+    /**
+     * Formula of the rule.
+     */
+    private final Formula formula;
+
+    /**
+     * Metavariable names and their respective constraints.
+     */
+    private final Map<String, MetavariableConstraint> metavars;
+}
