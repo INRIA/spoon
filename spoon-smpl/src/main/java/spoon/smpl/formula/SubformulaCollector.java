a65f65bed62cc01cb5bca064c22c18ca8944b148
mkforsb <mkforsb@gmail.com>
2020-04-08 17:37:28 +0200

    introduce SubformulaCollector

---
+++ spoon-smpl/src/main/java/spoon/smpl/formula/SubformulaCollector.java
@@ -0,0 +1,83 @@
+package spoon.smpl.formula;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubformulaCollector implements FormulaVisitor {
+    public SubformulaCollector() {
+        subformulas = new ArrayList<>();
+    }
+
+    public List<Formula> getResult() {
+        return subformulas;
+    }
+
+    private List<Formula> subformulas;
+
+    @Override
+    public void visit(True element) {
+        subformulas.add(element);
+    }
+
+    @Override
+    public void visit(And element) {
+        subformulas.add(element);
+        element.getLhs().accept(this);
+        element.getRhs().accept(this);
+    }
+
+    @Override
+    public void visit(Or element) {
+        subformulas.add(element);
+        element.getLhs().accept(this);
+        element.getRhs().accept(this);
+    }
+
+    @Override
+    public void visit(Neg element) {
+        subformulas.add(element);
+        element.getInnerElement().accept(this);
+    }
+
+    @Override
+    public void visit(Predicate element) {
+        subformulas.add(element);
+    }
+
+    @Override
+    public void visit(ExistsNext element) {
+        subformulas.add(element);
+        element.getInnerElement().accept(this);
+    }
+
+    @Override
+    public void visit(AllNext element) {
+        subformulas.add(element);
+        element.getInnerElement().accept(this);
+    }
+
+    @Override
+    public void visit(ExistsUntil element) {
+        subformulas.add(element);
+        element.getLhs().accept(this);
+        element.getRhs().accept(this);
+    }
+
+    @Override
+    public void visit(AllUntil element) {
+        subformulas.add(element);
+        element.getLhs().accept(this);
+        element.getRhs().accept(this);
+    }
+
+    @Override
+    public void visit(ExistsVar element) {
+        subformulas.add(element);
+        element.getInnerElement().accept(this);
+    }
+
+    @Override
+    public void visit(SetEnv element) {
+        subformulas.add(element);
+    }
+}
