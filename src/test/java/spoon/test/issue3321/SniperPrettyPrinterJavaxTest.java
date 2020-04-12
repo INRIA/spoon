33b57316a197b85a734c4f1bcda2155da56bb0fd
Gibah Joseph <gibahjoe@gmail.com>
2020-04-12 23:23:41 +0100

    added test for issue #3321

---
+++ src/test/java/spoon/test/issue3321/SniperPrettyPrinterJavaxTest.java
@@ -0,0 +1,27 @@
+package spoon.test.issue3321;
+
+import org.junit.Test;
+import spoon.Launcher;
+import spoon.compiler.Environment;
+import spoon.support.sniper.SniperJavaPrettyPrinter;
+
+/**
+ * @author Gibah Joseph
+ * Email: gibahjoe@gmail.com
+ * Apr, 2020
+ **/
+public class SniperPrettyPrinterJavaxTest {
+    @Test
+    public void testThatCanGenerateSniperPrettyPrintedSourceForJavaxAnnotatedClasses() {
+        final Launcher l = new Launcher();
+        Environment e = l.getEnvironment();
+
+        e.setPrettyPrinterCreator(() -> new SniperJavaPrettyPrinter(e));
+        e.setNoClasspath(true);
+        e.setAutoImports(true);
+        l.addProcessor(new CtClassProcessor());
+        l.addInputResource("src/test/java/spoon/test/issue3321/source/JavaxImportTestSource.java");
+        l.setSourceOutputDirectory("src/test/resources");
+        l.run();
+    }
+}
