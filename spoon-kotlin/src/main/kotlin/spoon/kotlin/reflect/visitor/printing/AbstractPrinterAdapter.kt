91d5ed93cca70950d48b0f5ebd3a3d07ea31d087
jlundhol <jesper.a.lundholm@gmail.com>
2020-05-20 01:09:38 +0200

    Add PrinterAdapters

---
+++ spoon-kotlin/src/main/kotlin/spoon/kotlin/reflect/visitor/printing/AbstractPrinterAdapter.kt
@@ -0,0 +1,39 @@
+package spoon.kotlin.reflect.visitor.printing
+
+abstract class AbstractPrinterAdapter(
+        private val lineSeparator : String = System.getProperty("line.separator")
+) {
+
+    abstract infix fun write(c : Char) : AbstractPrinterAdapter
+
+    var line = 1
+        protected set
+
+    var column = 1
+        protected set
+
+    open infix fun write(s : String) : AbstractPrinterAdapter {
+        s.forEach { write(it) }
+        return this
+    }
+
+    open fun newline() : AbstractPrinterAdapter = write(lineSeparator)
+
+    open infix fun writeln(s : String) : AbstractPrinterAdapter {
+        write(s)
+        newline()
+        return this
+    }
+
+    open infix fun writeln(c : Char) : AbstractPrinterAdapter {
+        write(c)
+        newline()
+        return this
+    }
+
+    infix fun and(s : String) = write(s)
+    infix fun and(c : Char) = write(c)
+
+    abstract override fun toString() : String
+
+}
---
+++ spoon-kotlin/src/main/kotlin/spoon/kotlin/reflect/visitor/printing/DefaultPrinterAdapter.kt
@@ -0,0 +1,91 @@
+package spoon.kotlin.reflect.visitor.printing
+
+import spoon.kotlin.reflect.KtModifierKind
+
+open class DefaultPrinterAdapter(
+        private val ignoredModifiers : List<KtModifierKind> = listOf(
+            KtModifierKind.PUBLIC, KtModifierKind.FINAL)
+) : AbstractPrinterAdapter() {
+
+    /**
+    * Declaration type: fun foo(): Int, var bar: String
+    * Of supertype: class A : B, fun <T : Number> foo()
+    * Constructor delegation: constructor(x : Int) : this(x)
+    * Object declaration: val o = object : T
+    */
+    enum class ColonContext { DECLARATION_TYPE, OF_SUPERTYPE, CONSTRUCTOR_DELEGATION, OBJECT_DECLARATION }
+
+    val KT_FILE_EXTENSION = ".kt"
+    open val LINE_SEPARATOR = "\n" // System.getProperty("line.separator")
+    val KDOC_START = "/**"
+    val BLOCK_COMMENT_START = "/* "
+    val BLOCK_COMMENT_END = "*/"
+    val INLINE_COMMENT_START = "// "
+    open val FQ_NAME_SEPARATOR = "."
+
+    open val INDENT_UNIT = "    " // 4 spaces
+    var indentCount = 0
+
+    private var onNewLine = false
+
+    private val sb : StringBuilder = StringBuilder()
+
+    override infix fun write(c: Char) : DefaultPrinterAdapter {
+        if(onNewLine && c != '\n') {
+            onNewLine = false
+            writeIndent()
+        }
+        sb.append(c)
+        if(c == '\n') {
+            column = 1
+            line++
+            onNewLine = true
+        }
+        else column++
+
+        return this
+    }
+
+
+    private fun writeIndent() = repeat(indentCount) { write(INDENT_UNIT) }
+
+    override fun toString(): String = sb.toString()
+
+    fun pushIndent() = indentCount++
+    fun popIndent() = indentCount--
+
+    inline fun withIndentDiff(indentDiff : Int = 1, action : (DefaultPrinterAdapter) -> Unit) {
+        indentCount += indentDiff
+        action(this)
+        indentCount -= indentDiff
+    }
+
+    inline fun withIndent(indent : Int, action : (DefaultPrinterAdapter) -> Unit) {
+        val oldIndentCount = indentCount
+        indentCount = indent
+        action(this)
+        indentCount = oldIndentCount
+    }
+
+    /**
+     * Writes a colon with or without a leading whitespace depending on the context.
+     *
+     * Declaration type: fun foo(): Int, var bar: String
+     * Of supertype: class A : B, fun <T : Number> foo()
+     * Constructor delegation: constructor(x : Int) : this(x)
+     * Object declaration: val o = object : T
+     */
+    fun writeColon(context : ColonContext) = write(when(context) {
+        ColonContext.DECLARATION_TYPE -> ": "
+        else -> " : "
+    })
+
+    infix fun writeModifiers(modifierSet : Set<KtModifierKind>?) : DefaultPrinterAdapter = writeModifiers(modifierSet?.toList())
+
+    infix fun writeModifiers(modifierList : List<KtModifierKind>?) : DefaultPrinterAdapter = this.apply {
+        modifierList?.filterNot { it in ignoredModifiers }?.sorted()?.nullOrNotEmpty()?.
+            joinToString(separator = " ", postfix = " ") { it.token }?.let { this.write(it) }
+    }
+
+    private fun <T> List<T>.nullOrNotEmpty() = if(this.isEmpty()) null else this
+}
