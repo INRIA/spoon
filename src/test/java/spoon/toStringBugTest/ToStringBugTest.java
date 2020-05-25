de92d516ef2e6d1638c187d0ab768c9208a1a5de
Samuel Santos <samuel.c.santos@tecnico.ulisboa.pt>
2020-05-25 22:45:15 +0100

    bug test toString annoying error

---
+++ src/test/java/spoon/toStringBugTest/ToStringBugTest.java
@@ -0,0 +1,53 @@
+package spoon.toStringBugTest;
+
+import org.junit.Test;
+import spoon.Launcher;
+import spoon.SpoonException;
+import spoon.reflect.declaration.CtType;
+
+import java.util.List;
+
+import static org.junit.Assert.fail;
+
+public class ToStringBugTest {
+
+    @Test
+    public void test1() {
+        final Launcher launcher = new Launcher();
+        launcher.getEnvironment().setNoClasspath(true);
+        // folder with both classes
+        launcher.addInputResource("./src/test/resources/toStringBugTest");
+        launcher.buildModel();
+        List<CtType<?>> all = launcher.getFactory().Class().getAll();
+
+        // first class of resources
+        CtType<?> ctType = all.get(0);
+        try {
+            String s = ctType.toString();
+        } catch (SpoonException e) {
+            // spoon.SpoonException: Unexpected next line after last line
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void test2() {
+        final Launcher launcher = new Launcher();
+        launcher.getEnvironment().setNoClasspath(true);
+        // folder with both classes
+        launcher.addInputResource("./src/test/resources/toStringBugTest");
+        launcher.buildModel();
+        List<CtType<?>> all = launcher.getFactory().Class().getAll();
+
+        // second class of resources
+        CtType<?> ctType = all.get(1);
+        try {
+            String s = ctType.toString();
+        } catch (SpoonException e) {
+            // spoon.SpoonException: Unexpected next line after last line
+            e.printStackTrace();
+            fail();
+        }
+    }
+}
