b619b8097851f056737701d00fbb4a33c5f2d484
Martin Monperrus <martin.monperrus@gnieh.org>
2020-05-28 17:41:46 +0200

    up

--- src/test/java/spoon/toStringBugTest/ToStringBugTest.java
+++ src/test/java/spoon/toStringBugTest/ToStringBugTest.java
@@ -14,10 +14,9 @@ import static org.junit.Assert.fail;
 
 public class ToStringBugTest {
     
-    @Ignore("UnresolvedBug")
-    @GitHubIssue(issueNumber = 3381)
     @Test
-    public void test1() {
+    public void testIssue3382() {
+        // contract: the cleanComment should not crash
         final Launcher launcher = new Launcher();
         launcher.getEnvironment().setNoClasspath(true);
         // folder with both classes
@@ -26,35 +25,20 @@ public class ToStringBugTest {
         List<CtType<?>> all = launcher.getFactory().Class().getAll();
 
         // first class of resources
-        CtType<?> ctType = all.get(0);
+        CtType<?> ctType0 = all.get(0);
         try {
-            String s = ctType.toString();
+            String s = ctType0.toString();
         } catch (SpoonException e) {
             // spoon.SpoonException: Unexpected next line after last line
-            e.printStackTrace();
-            fail();
+            fail(e.toString());
         }
-    }
-    
-	@Ignore("UnresolvedBug")
-	@GitHubIssue(issueNumber = 3381)
-    @Test
-    public void test2() {
-        final Launcher launcher = new Launcher();
-        launcher.getEnvironment().setNoClasspath(true);
-        // folder with both classes
-        launcher.addInputResource("./src/test/resources/toStringBugTest");
-        launcher.buildModel();
-        List<CtType<?>> all = launcher.getFactory().Class().getAll();
 
         // second class of resources
-        CtType<?> ctType = all.get(1);
+        CtType<?> ctType1 = all.get(1);
         try {
-            String s = ctType.toString();
+            String s = ctType1.toString();
         } catch (SpoonException e) {
-            // spoon.SpoonException: Unexpected next line after last line
-            e.printStackTrace();
-            fail();
+            fail(e.toString());
         }
     }
 }
