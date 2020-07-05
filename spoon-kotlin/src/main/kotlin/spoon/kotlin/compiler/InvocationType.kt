f89cebac006a4c8d1d4bb15def7066fef2e338c7
jlundhol <jesper.a.lundholm@gmail.com>
2020-07-05 16:58:30 +0200

    Specialize receiver type in calls

--- spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/InvocationType.kt
+++ spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/InvocationType.kt
@@ -8,12 +8,12 @@ import spoon.reflect.code.UnaryOperatorKind
 
 internal sealed class InvocationType {
     data class UNKNOWN(val functionCall: FirFunctionCall): InvocationType()
-    data class NORMAL_CALL(val receiver: FirElement?, val function: FirFunctionCall) : InvocationType()
+    data class NORMAL_CALL(val receiver: FirExpression?, val function: FirFunctionCall) : InvocationType()
     data class INFIX_CALL(val lhs: FirElement, val function: FirFunctionCall, val rhs: FirElement) : InvocationType()
     data class BINARY_OPERATOR(val lhs: FirElement, val kind: KtBinaryOperatorKind, val rhs: FirElement, val originalFunction: FirFunctionCall) : InvocationType()
     data class ASSIGNMENT_OPERATOR(val lhs: FirElement, val kind: KtBinaryOperatorKind, val rhs: FirElement, val originalFunction: FirFunctionCall) : InvocationType()
     data class POSTFIX_OPERATOR(val operand: FirElement, val kind: UnaryOperatorKind, val originalFunction: FirFunctionCall) : InvocationType()
     data class PREFIX_OPERATOR(val kind: UnaryOperatorKind, val operand: FirElement, val originalFunction: FirFunctionCall) : InvocationType()
-    data class GET_OPERATOR(val receiver: FirElement, val args: List<FirExpression>, val originalFunction: FirFunctionCall) : InvocationType() // a[x]
-    data class SET_OPERATOR(val receiver: FirElement, val args: List<FirExpression>, val rhs: FirExpression, val originalFunction: FirFunctionCall) : InvocationType() // a[x] = b
+    data class GET_OPERATOR(val receiver: FirExpression, val args: List<FirExpression>, val originalFunction: FirFunctionCall) : InvocationType() // a[x]
+    data class SET_OPERATOR(val receiver: FirExpression, val args: List<FirExpression>, val rhs: FirExpression, val originalFunction: FirFunctionCall) : InvocationType() // a[x] = b
 }
