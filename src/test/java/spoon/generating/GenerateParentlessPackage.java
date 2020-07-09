c0aa0a975457a977ccf3cbfe417164c4941fc51f
Clemens Bartz <clemens@clemens-bartz.de>
2020-07-09 21:20:47 +0200

    Add test for generating parent-less package (issue #3488).

---
+++ src/test/java/spoon/generating/GenerateParentlessPackage.java
@@ -0,0 +1,43 @@
+package spoon.generating;
+
+import org.junit.jupiter.api.Test;
+import spoon.Launcher;
+import spoon.reflect.declaration.CtPackage;
+import spoon.reflect.factory.Factory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+/**
+ * This test generates a parent-less package.
+ */
+public class GenerateParentlessPackage {
+
+    @Test
+    public void generateParentlessPackage() {
+        final Path tempDir;
+
+        try {
+            tempDir = Files.createTempDirectory("spoon");
+        } catch (final IOException e) {
+            fail("Could not generate temporary directory: " + e.getMessage());
+            return;
+        }
+
+        final Launcher launcher = new Launcher();
+        launcher.addInputResource(tempDir.toFile().getAbsolutePath());
+        launcher.setSourceOutputDirectory(tempDir.toFile().getAbsolutePath());
+        launcher.getEnvironment().setAutoImports(true);
+        launcher.buildModel();
+
+        final Factory factory = launcher.getFactory();
+
+        final CtPackage ctPackage = factory.createPackage(null, "fooBar");
+        assertNotNull(ctPackage);
+    }
+
+}
