f8260c0c25342d4d1b4e0d8484030bc407a4e60e
Clemens Bartz <clemens@clemens-bartz.de>
2020-07-12 18:28:38 +0200

    Replace fail and assertNotNull with JUnit 5 versions.

--- src/test/java/spoon/generating/GenerateParentlessPackageTest.java
+++ src/test/java/spoon/generating/GenerateParentlessPackageTest.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * This test generates a parent-less package.
