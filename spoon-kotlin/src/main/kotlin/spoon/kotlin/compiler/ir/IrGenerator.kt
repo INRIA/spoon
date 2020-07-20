ab12a46f53ccc9d258e397f74641de59dbc0058f
jlundhol <jesper.a.lundholm@gmail.com>
2020-07-20 22:37:47 +0200

    Change IrGenerator to provide context

--- spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/ir/IrGenerator.kt
+++ spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/ir/IrGenerator.kt
@@ -7,14 +7,18 @@ import org.jetbrains.kotlin.cli.jvm.compiler.NoScopeRecordCliBindingTrace
 import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
 import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.util.generateTypicalIrProviderList
 import org.jetbrains.kotlin.psi2ir.Psi2IrConfiguration
 import org.jetbrains.kotlin.psi2ir.Psi2IrTranslator
-import org.jetbrains.kotlin.psi2ir.generators.GeneratorExtensions
+import org.jetbrains.kotlin.psi2ir.generators.GeneratorContext
 import spoon.kotlin.compiler.SpoonKtEnvironment
 
-class IrGenerator(ktEnvironment: SpoonKtEnvironment) {
+internal class IrGenerator(ktEnvironment: SpoonKtEnvironment) {
     val environment = ktEnvironment.ktEnvironment
     val config = ktEnvironment.config
+    private val translator= Psi2IrTranslator(config.languageVersionSettings, Psi2IrConfiguration(true))
+    private val analysisResult by lazy { analyzePsi(environment) }
+
     private fun analyzePsi(environment: KotlinCoreEnvironment) : org.jetbrains.kotlin.analyzer.AnalysisResult {
         val sourceFiles = environment.getSourceFiles()
         val collector = environment.configuration.getNotNull(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY)
@@ -32,10 +36,18 @@ class IrGenerator(ktEnvironment: SpoonKtEnvironment) {
 
     }
 
-    fun generateIr() : IrModuleFragment {
-        val a = analyzePsi(environment)
+    fun generateIrWithContext(): Pair<IrModuleFragment,GeneratorContext> {
+        val context = translator.createGeneratorContext(
+            analysisResult.moduleDescriptor,
+            analysisResult.bindingContext
+        )
+        val irProviders = generateTypicalIrProviderList(
+            analysisResult.moduleDescriptor,
+            context.irBuiltIns,
+            context.symbolTable
 
-        val translator = Psi2IrTranslator(config.languageVersionSettings, Psi2IrConfiguration(true))
-        return translator.generateModule(a.moduleDescriptor,environment.getSourceFiles(),a.bindingContext, GeneratorExtensions())
+        )
+        return translator.generateModuleFragment(context,
+        environment.getSourceFiles(), irProviders) to context
     }
 }
