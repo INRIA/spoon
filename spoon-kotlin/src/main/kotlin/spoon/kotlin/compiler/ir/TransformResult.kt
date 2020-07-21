4eced73c86168c90e825f77790be748b83565082
jlundhol <jesper.a.lundholm@gmail.com>
2020-07-22 00:24:47 +0200

    Add nothing to TransformResult

--- spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/ir/TransformResult.kt
+++ spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/ir/TransformResult.kt
@@ -3,9 +3,14 @@ package spoon.kotlin.compiler.ir
 import spoon.reflect.declaration.CtElement
 
 sealed class TransformResult<out T : Any>(protected val result: T?) {
-    val isEmpty get() = result == null
+    val isNothing get() = result == null
+    val isDefinite get() = this is DefiniteTransformResult<*>
+    val isMaybe get() = this is MaybeTransformResult<*>
     val resultUnsafe get() = result ?: throw RuntimeException("Empty TransformResult")
     val resultOrNull get() = result
+    companion object {
+        fun <T: Any> nothing() = MaybeTransformResult<T>(null)
+    }
 }
 
 class DefiniteTransformResult<out T : Any>(result: T) : TransformResult<T>(result) {
