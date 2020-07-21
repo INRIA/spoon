ea0d880a755428d07b0efaf8865caa43cb60d7e1
jlundhol <jesper.a.lundholm@gmail.com>
2020-07-21 17:21:57 +0200

    Move KtMetadata to IrUtils

--- spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/ir/IrTreeBuilder.kt
+++ spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/ir/IrTreeBuilder.kt
@@ -481,18 +481,6 @@ internal class IrTreeBuilder(
         return superAccess
     }
 
-    internal class KtMetadata<T> private constructor(val value: T) {
-        // Used for type safety when putting metadata, Kt metadata is either Boolean or a CtElement
-        companion object {
-            fun wrap(b: Boolean): KtMetadata<Boolean> = KtMetadata(b)
-            fun wrap(c: CtElement): KtMetadata<CtElement> = KtMetadata(c)
-        }
-    }
-
-    internal fun <T: CtElement> T.putKtMetadata(s: String, d: KtMetadata<*>) {
-        putMetadata<CtElement>(s, d.value)
-    }
-
     private fun <T> CtExpression<T>.wrapInImplicitReturn() : CtReturn<T> {
         val r = factory.Core().createReturn<T>()
         r.setReturnedExpression<CtReturn<T>>(this)
--- spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/ir/IrUtils.kt
+++ spoon-kotlin/src/main/kotlin/spoon/kotlin/compiler/ir/IrUtils.kt
@@ -7,3 +7,16 @@ import spoon.reflect.declaration.CtElement
 fun CtElement.addModifiersAsMetadata(modifierList: List<KtModifierKind>) {
     putMetadata<CtElement>(KtMetadataKeys.KT_MODIFIERS, modifierList.toMutableSet())
 }
+
+
+internal class KtMetadata<T> private constructor(val value: T) {
+    // Used for type safety when putting metadata, Kt metadata is either Boolean or a CtElement
+    companion object {
+        fun wrap(b: Boolean): KtMetadata<Boolean> = KtMetadata(b)
+        fun wrap(c: CtElement): KtMetadata<CtElement> = KtMetadata(c)
+    }
+}
+
+internal fun <T: CtElement> T.putKtMetadata(s: String, d: KtMetadata<*>) {
+    putMetadata<CtElement>(s, d.value)
+}
