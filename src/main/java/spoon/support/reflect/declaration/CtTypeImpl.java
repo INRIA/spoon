5016b692073bca1c84b7603e15192f169e955fed
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-08-01 11:45:42 +0200

    fix diff

--- src/main/java/spoon/support/reflect/declaration/CtTypeImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtTypeImpl.java
@@ -52,7 +52,15 @@ import spoon.support.util.SignatureBasedSortedSet;
 import spoon.support.visitor.ClassTypingContext;
 
 import java.lang.annotation.Annotation;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * The implementation for {@link spoon.reflect.declaration.CtType}.
