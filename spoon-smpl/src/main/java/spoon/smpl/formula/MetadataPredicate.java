fa6ca2c745c1a6e3f8f04ae88a35490b35f6998d
mkforsb <mkforsb@gmail.com>
2020-08-20 16:12:31 +0200

    fix mistakes in class doc comment of MetadataPredicate

--- spoon-smpl/src/main/java/spoon/smpl/formula/MetadataPredicate.java
+++ spoon-smpl/src/main/java/spoon/smpl/formula/MetadataPredicate.java
@@ -3,16 +3,22 @@ package spoon.smpl.formula;
 import java.util.Map;
 
 /**
- * A MetadataPredicate is an non-metavariable-parameterized predicate that represents a search for states that export
- * arbitrary metadata under a certain name (key), binding the metadata value to a variable in the environment.
+ * A MetadataPredicate is an non-metavariable-parameterized predicate that represents a search for
+ * states that export arbitrary metadata under a certain name (key), binding the metadata value to
+ * a variable in the environment.
  *
- * For example, picture a model M with states s1 and s2, both labeled as exporting the metadata key "parent", with
- *   the values s1.parent = 1:int and s2.parent = 2:int.
+ * For example, picture a model M with states s1 and s2, both labeled as exporting the metadata key
+ * "parent", with the values s1.parent = 1:int and s2.parent = 2:int.
  *
- *   We would then expect SAT(MetadataPredicate(parent, p1)) = {..., (s1, {parent=1}, []), (s2, {parent=2}, []), ...}
+ *   We would then expect SAT(MetadataPredicate("p1", "parent")) = {..., (s1, {p1=1}, []),
+ *                                                                       (s2, {p1=2}, []), ...}
  *
- * The predicate can be used for filtering results based on arbitrary metadata, as in the following example:
- *   And(MetadataPredicate(r1, reachable), SetEnv(r1, true))
+ * The predicate can be used for filtering results based on arbitrary metadata, as in the following
+ * example:
+ *   And(MetadataPredicate("r1", "reachable"), SetEnv("r1", true))
+ *
+ *   This formula would select all states exporting the metadata key "reachable" with the specific
+ *   value of true:boolean.
  */
 public class MetadataPredicate implements Predicate {
     /**
