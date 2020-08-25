e82ec957e722ba67565592d0c08f10da06d7f837
Martin Monperrus <martin.monperrus@gnieh.org>
2020-08-25 11:59:51 +0200

    test: add test for CtDequeScanner

--- src/test/java/spoon/reflect/visitor/CtScannerTest.java
+++ src/test/java/spoon/reflect/visitor/CtScannerTest.java
@@ -356,4 +356,21 @@ public class CtScannerTest {
 			fail();
 		}
 	}
+
+	@Test
+	public void testDequeSCanner() {
+		CtClass<?> c = Launcher.parseClass("class A { void m() { System.out.println(\\\"yeah\\\");} }");
+		ArrayList l = new ArrayList<Integer>();
+		new CtDequeScanner() {
+			@Override
+			public void scan(CtElement o) {
+				l.add(elementsDeque.size());
+				super.scan(o);
+			}
+		}.scan(c);
+		// contract: the CtDequeScanner has the context information of the parents
+		assertEquals(0, l.get(0));
+		assertEquals(1, l.get(1)); // visiting a child
+		assertEquals(2, l.get(3)); // visiting a grand child
+	}
 }
