ea499b0e91593303acca4231fa89ba7d438da672
Nicolas Harrand <nicolas.harrand@gmail.com>
2020-09-01 13:27:04 +0200

    Update ReplaceScanner.java

--- src/test/java/spoon/generating/replace/ReplaceScanner.java
+++ src/test/java/spoon/generating/replace/ReplaceScanner.java
@@ -194,8 +194,7 @@ public class ReplaceScanner extends CtScanner {
 					       + "private final CtElement element XXX(CtElement element) { this.element = element; }\n"
 					       + "@java.lang.Override public void set(CtElement replace) {}\n"
 					       + "}");
-		// for some reason, parseClass adds an empty static block
-		//((CtAnonymousExecutable) listener.getAnonymousExecutables().get(0)).delete();
+		
 		listener.setSimpleName(listenerName);
 		target.addNestedType(listener);
 		final List<CtTypeReference> references = listener.getElements(new TypeFilter<CtTypeReference>(CtTypeReference.class) {
