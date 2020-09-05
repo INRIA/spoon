681d1c13da0221ff5501b1169bbe55b0fb2268ca
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-09-05 19:21:14 +0200

    add failing testcase for issue

--- src/test/java/spoon/generating/CorrectIdentifierTest.java
+++ src/test/java/spoon/generating/CorrectIdentifierTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.Ignore;
 import org.junit.Test;
+import spoon.FluentLauncher;
 import spoon.Launcher;
 import spoon.SpoonException;
+import spoon.reflect.CtModel;
 import spoon.reflect.reference.CtLocalVariableReference;
+import spoon.test.GitHubIssue;
 /**
  * for correct identifier see JLS chapter 3.8 and for keywords 3.9.
  * Ignored tests because we have to cut some corners between spec and jdt.
@@ -68,5 +71,12 @@ public class CorrectIdentifierTest {
 	public void correctIdentiferUtfChinese() {
 		CtLocalVariableReference<Object> localVariableRef = new Launcher().getFactory().createLocalVariableReference();
 		assertDoesNotThrow(() -> localVariableRef.setSimpleName("処理"));
+	}
+
+	@GitHubIssue(issueNumber = 3564)
+	@Test
+	public void fooTest() {
+		CtModel model = new FluentLauncher().inputResource("./src/test/resources/identifier/InliningImplementationMatcher.java").buildModel();
+		// after fix byte-buddy/byte-buddy-dep must run
 	}
 }
