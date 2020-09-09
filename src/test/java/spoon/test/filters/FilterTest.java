f07b85a0be47e7b2871521a0845c101e362e7486
Martin Monperrus <martin.monperrus@gnieh.org>
2020-09-09 10:43:29 +0200

    up

--- src/test/java/spoon/test/filters/FilterTest.java
+++ src/test/java/spoon/test/filters/FilterTest.java
@@ -110,6 +110,15 @@ public class FilterTest {
 	}
 
 	@Test
+	public void testNameFilter() throws Exception {
+		// contract: legacy NameFilter is tested and works
+		CtClass<?> foo = factory.Package().get("spoon.test.filters").getType("Foo");
+		assertEquals("Foo", foo.getSimpleName());
+		List<CtNamedElement> elements = foo.getElements(new NameFilter<>("i"));
+		assertEquals(1, elements.size());
+	}
+
+	@Test
 	public void testFilters() {
 		CtClass<?> foo = factory.Package().get("spoon.test.filters.testclasses").getType("Foo");
 		assertEquals("Foo", foo.getSimpleName());
