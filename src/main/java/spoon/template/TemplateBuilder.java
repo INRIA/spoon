9533c9999a30bf4f1ac47cc8e4b79ec0e033036f
Martin Monperrus <martin.monperrus@gnieh.org>
2020-09-11 14:36:18 +0200

    up

--- src/main/java/spoon/template/TemplateBuilder.java
+++ src/main/java/spoon/template/TemplateBuilder.java
@@ -97,8 +97,7 @@ class TemplateBuilder {
 			pb = new PatternBuilder(Collections.singletonList(templateRoot));
 		}
 		Map<String, Object> templateParameters = template == null ? null : Parameters.getTemplateParametersAsMap(f, null, template);
-		//legacy templates always automatically simplifies generated code
-		pb.setAutoSimplifySubstitutions(true);
+		pb.setAutoSimplifySubstitutions(false);
 		pb.configurePatternParameters(pc -> {
 			pc.byTemplateParameter(templateParameters);
 			pc.byParameterValues(templateParameters);
