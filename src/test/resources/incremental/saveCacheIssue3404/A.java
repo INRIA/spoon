0bce8053f759c469e17c2413176756771c4e4a88
Restyled.io <commits@restyled.io>
2020-09-23 18:59:46 +0000

    Restyled by clang-format

--- src/test/resources/incremental/saveCacheIssue3404/A.java
+++ src/test/resources/incremental/saveCacheIssue3404/A.java
@@ -8,20 +8,19 @@ import soot.SootClass;
 import soot.SootMethod;
 import soot.Unit;
 import soot.UnitPrinter;
+import soot.jimple.internal.*;
 import soot.toolkits.graph.ExceptionalUnitGraph;
 import soot.toolkits.graph.UnitGraph;
-import soot.jimple.internal.*;
 
 public class A {
-public static void main(String[] args) {
-		String[] sootArgs = {
-			"-pp", 	// sets the class path for Soot
-			"-w", 						// Whole program analysis, necessary for using Transformer
-			"-src-prec", "java",		// Specify type of source file
-			"-main-class", "Main",	// Specify the main class 
-			"-f", "J", 					// Specify type of output file
-			"Main" 
-		};
-		soot.Main.main(sootArgs);
-	}
+  public static void main(String[] args) {
+    String[] sootArgs = {
+        "-pp", // sets the class path for Soot
+        "-w",  // Whole program analysis, necessary for using Transformer
+        "-src-prec",   "java", // Specify type of source file
+        "-main-class", "Main", // Specify the main class
+        "-f",          "J",    // Specify type of output file
+        "Main"};
+    soot.Main.main(sootArgs);
+  }
 }
