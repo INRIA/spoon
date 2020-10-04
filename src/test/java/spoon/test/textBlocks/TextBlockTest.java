d23a20de0d82062fcc7008ec1e9b2b5955c61cc9
Martin Monperrus <martin.monperrus@gnieh.org>
2020-10-04 04:41:33 +0000

    Update TextBlockTest.java

--- src/test/java/spoon/test/textBlocks/TextBlockTest.java
+++ src/test/java/spoon/test/textBlocks/TextBlockTest.java
@@ -13,7 +13,7 @@ import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.path.CtRole;
 
-// Test for the new Java 15 text block feature with """
+/** Test for the new Java 15 text block feature with */
 public class TextBlockTest{
 	private Launcher setUpTest() {
 		Launcher launcher = new Launcher();
