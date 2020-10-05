e4c7cb93eec4d31aca95f1d1aba02aac4cd68c37
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2020-10-05 21:42:03 +0530

    test: Add testcases for Text Blocks introduced in Java 15 (#3627)
    Co-authored-by: Martin Monperrus <martin.monperrus@gnieh.org>

---
+++ src/test/resources/textBlock/TextBlockTestClass.java
@@ -0,0 +1,43 @@
+package textBlock;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+public class TextBlockTestClass{
+	void m1() {
+		String html = """
+	                  <html>
+	                      <body>
+	                          <p>Hello, कसौटी 🥲</p>
+	                      </body>
+	                  </html>
+	                  """;
+	}
+
+	void m2(){
+		String query = """
+	                       SELECT "EMP_ID", "LAST_NAME" FROM "EMPLOYEE_TB"
+	                       WHERE "CITY" = 'INDIANAPOLIS'
+	                       ORDER BY "EMP_ID", "LAST_NAME";
+	                       """;
+	}
+
+	void m3(){
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("js");
+		try {
+			Object obj = engine.eval("""
+			                         function hello() {
+			                             print('"Hello, world"');
+			                         }
+			                     
+			                         hello();
+			                         """);
+		}catch(Exception e) {}
+		System.out.println(query);
+	}
+
+	void m4() {
+		String empty = """
+		""";
+	}
+}
