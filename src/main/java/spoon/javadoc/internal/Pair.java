e0d282a21a77ab8cb3427050ffd2e2d4c2f20320
Martin Monperrus <martin.monperrus@gnieh.org>
2020-10-19 21:43:45 +0000

    chore: remove uncovered, unnecessary code (#3652)

--- src/main/java/spoon/javadoc/internal/Pair.java
+++ src/main/java/spoon/javadoc/internal/Pair.java
@@ -28,31 +28,4 @@ package spoon.javadoc.internal;
 		this.a = a;
 		this.b = b;
 	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-
-		Pair<?, ?> pair = (Pair<?, ?>) o;
-
-		if (a != null ? !a.equals(pair.a) : pair.a != null) {
-			return false;
-		}
-		if (b != null ? !b.equals(pair.b) : pair.b != null) {
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = a != null ? a.hashCode() : 0;
-		return 31 * result + (b != null ? b.hashCode() : 0);
-	}
-	}
+}
