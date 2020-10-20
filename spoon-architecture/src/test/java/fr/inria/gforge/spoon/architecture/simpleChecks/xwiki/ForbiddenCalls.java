8b1068aa67320393ba3b956bcb93eef0eb03f0f9
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-10-21 01:19:07 +0200

    add testcase for forbidden invocation

---
+++ spoon-architecture/src/test/java/fr/inria/gforge/spoon/architecture/simpleChecks/xwiki/ForbiddenCalls.java
@@ -0,0 +1,21 @@
+package fr.inria.gforge.spoon.architecture.simpleChecks.xwiki;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import fr.inria.gforge.spoon.architecture.defaultChecks.ForbiddenInvocation;
+import fr.inria.gforge.spoon.architecture.runner.Architecture;
+import spoon.reflect.CtModel;
+
+public class ForbiddenCalls {
+
+  private Map<String, List<String>> methodsByType = new HashMap<>();
+
+
+  @Architecture
+  public void forbiddenCallsTest(CtModel srcModel, CtModel testModel) {
+    methodsByType.put("java.lang.System.out", Arrays.asList("println"));
+    ForbiddenInvocation.forbiddenInvocationCheck(methodsByType, System.out::println).runCheck(srcModel);
+  }
+}
