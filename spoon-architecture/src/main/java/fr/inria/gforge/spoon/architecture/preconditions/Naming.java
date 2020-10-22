59d00dc0b9a3b46af90373923268e808b72e93ba
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-10-22 07:35:51 +0200

    add regex support for naming

--- spoon-architecture/src/main/java/fr/inria/gforge/spoon/architecture/preconditions/Naming.java
+++ spoon-architecture/src/main/java/fr/inria/gforge/spoon/architecture/preconditions/Naming.java
@@ -1,6 +1,7 @@
 package fr.inria.gforge.spoon.architecture.preconditions;
 
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import spoon.reflect.declaration.CtNamedElement;
 
 public class Naming {
@@ -19,5 +20,18 @@ public class Naming {
 	}
 	public static  <T extends CtNamedElement> Predicate<T> endsWith(String name) {
 		return (input) -> input.getSimpleName().endsWith(name);
+	}
+	public static  <T extends CtNamedElement> Predicate<T> matches(String regex) {
+		return (input) -> input.getSimpleName().matches(regex);
+	}
+	public static  <T extends CtNamedElement> Predicate<T> matches(Pattern regex) {
+		return (input) -> regex.matcher(input.getSimpleName()).matches();
+	}
+
+	public static  <T extends CtNamedElement> Predicate<T> matchesNot(String regex) {
+		return (input) -> !input.getSimpleName().matches(regex);
+	}
+	public static  <T extends CtNamedElement> Predicate<T> matchesNot(Pattern regex) {
+		return (input) -> !regex.matcher(input.getSimpleName()).matches();
 	}
 }
