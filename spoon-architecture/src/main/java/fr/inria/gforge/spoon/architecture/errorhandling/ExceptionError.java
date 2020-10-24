f8e50a190d785b958e121b30d361091430e1b624
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-10-24 03:24:18 +0200

    add documentation for errorHandling package

--- spoon-architecture/src/main/java/fr/inria/gforge/spoon/architecture/errorhandling/ExceptionError.java
+++ spoon-architecture/src/main/java/fr/inria/gforge/spoon/architecture/errorhandling/ExceptionError.java
@@ -1,11 +1,24 @@
 package fr.inria.gforge.spoon.architecture.errorhandling;
 
 import spoon.SpoonException;
-
+/**
+ * This class defines a default implementation for {@code IError}. Any constraint violation will result in an thrown {@code SpoonException}.
+ * <p>
+ * You can use this e.g. for failing the CI if any architecture rule is violated. The exception message will consist of a String and the violated element printed.
+ * Use {@code #ExceptionError(String)} for a custom errorPrefix string.   
+ */
 public class ExceptionError<T> implements IError<T> {
 
+	private String errorPrefix;
+
+	public ExceptionError(String errorPrefix) {
+		this.errorPrefix = errorPrefix;
+	}
+	public ExceptionError() {
+		this.errorPrefix = "Element violates architectural rule";
+	}
 	@Override
 	public void printError(T element) {
-		throw new SpoonException("Element violates architectural rule " + element);
+		throw new SpoonException(errorPrefix + element);
 	}
 }
--- spoon-architecture/src/main/java/fr/inria/gforge/spoon/architecture/errorhandling/NopError.java
+++ spoon-architecture/src/main/java/fr/inria/gforge/spoon/architecture/errorhandling/NopError.java
@@ -1,5 +1,6 @@
 package fr.inria.gforge.spoon.architecture.errorhandling;
 
+/** This class is a default implementation omitting the error and doing nothing. */
 public class NopError<T> implements IError<T> {
 
 	@Override
