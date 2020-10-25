6c503544ba8250a58842cb033224a17cde51fc1f
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-10-25 20:11:21 +0100

    improve fieldReferenceMatcher constraint

--- spoon-architecture/src/main/java/spoon/architecture/constraints/FieldReferenceMatcher.java
+++ spoon-architecture/src/main/java/spoon/architecture/constraints/FieldReferenceMatcher.java
@@ -5,22 +5,27 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.Predicate;
 import spoon.reflect.CtModel;
+import spoon.reflect.code.CtFieldAccess;
 import spoon.reflect.declaration.CtField;
 import spoon.reflect.reference.CtFieldReference;
 import spoon.reflect.visitor.filter.TypeFilter;
 
 public class FieldReferenceMatcher implements Predicate<CtField<?>> {
 
-	private Set<CtField<?>> lookUp;
+	private Set<CtFieldReference<?>> lookUp;
 
 	public FieldReferenceMatcher(CtModel model) {
 		lookUp = new HashSet<>();
-		model.getElements(new TypeFilter<>(CtFieldReference.class)).stream().map(v -> v.getDeclaration()).filter(Objects::nonNull).map(lookUp::add);
+		model.getElements(new TypeFilter<>(CtFieldAccess.class))
+				.stream()
+				.map(v -> v.getVariable())
+				.filter(Objects::nonNull)
+				.forEach(lookUp::add);
 	}
 
 	@Override
 	public boolean test(CtField<?> t) {
-		return lookUp.contains(t);
+		return lookUp.contains(t.getReference());
 	}
 
 }
