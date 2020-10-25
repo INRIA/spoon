882d153a01cc7c30fdf8653dd1e37947f46f3ee1
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2020-10-25 14:47:20 +0530

    Support TextBlock creation from parsed source and add tests checking creation of TextBlock element in model

--- src/test/java/spoon/test/textBlocks/TextBlockTest.java
+++ src/test/java/spoon/test/textBlocks/TextBlockTest.java
@@ -1,6 +1,7 @@
 package spoon.test.textBlocks;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -10,6 +11,7 @@ import spoon.reflect.code.CtInvocation;
 import spoon.reflect.code.CtLiteral;
 import spoon.reflect.code.CtReturn;
 import spoon.reflect.code.CtStatement;
+import spoon.reflect.code.CtTextBlock;
 import spoon.reflect.code.CtTry;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
@@ -39,7 +41,8 @@ public class TextBlockTest{
 		CtMethod<?> m1 =  allstmt.getMethod("m1");
 		
 		CtStatement stmt1 = m1.getBody().getStatement(0);
-		CtLiteral l1 = (CtLiteral) stmt1.getValueByRole(CtRole.ASSIGNMENT);
+		assertTrue(stmt1.getValueByRole(CtRole.ASSIGNMENT) instanceof CtTextBlock);
+		CtTextBlock l1 = (CtTextBlock) stmt1.getValueByRole(CtRole.ASSIGNMENT);
 		assertEquals(l1.getValue(), "<html>\n    <body>\n        <p>Hello, कसौटी 🥲</p>\n    </body>\n</html>\n");
 	}
 
@@ -51,7 +54,8 @@ public class TextBlockTest{
 		CtMethod<?> m2 =  allstmt.getMethod("m2");
 		
 		CtStatement stmt2 = m2.getBody().getStatement(0);
-		CtLiteral l2 = (CtLiteral) stmt2.getValueByRole(CtRole.ASSIGNMENT);
+		assertTrue(stmt2.getValueByRole(CtRole.ASSIGNMENT) instanceof CtTextBlock);
+		CtTextBlock l2 = (CtTextBlock) stmt2.getValueByRole(CtRole.ASSIGNMENT);
 		assertEquals(l2.getValue(), "SELECT \"EMP_ID\", \"LAST_NAME\" FROM \"EMPLOYEE_TB\"\n"
 				+ "WHERE \"CITY\" = 'INDIANAPOLIS'\n"
 				+ "ORDER BY \"EMP_ID\", \"LAST_NAME\";\n");
@@ -67,7 +71,8 @@ public class TextBlockTest{
 		CtTry stmt4 = (CtTry) m3.getBody().getStatement(1);
 		CtStatement stmt5 = stmt4.getBody().getStatement(0);
 		CtInvocation inv = (CtInvocation) stmt5.getDirectChildren().get(1);
-		CtLiteral l3 = (CtLiteral) inv.getArguments().get(0);
+		assertTrue(inv.getArguments().get(0) instanceof CtTextBlock);
+		CtTextBlock l3 = (CtTextBlock) inv.getArguments().get(0);
 		assertEquals(l3.getValue(), "function hello() {\n"
 				+ "    print('\"Hello, world\"');\n"
 				+ "}\n"
@@ -84,7 +89,8 @@ public class TextBlockTest{
 		CtMethod<?> m4 =  allstmt.getMethod("m4");
 		
 		CtStatement stmt1 = m4.getBody().getStatement(0);
-		CtLiteral l1 = (CtLiteral) stmt1.getValueByRole(CtRole.ASSIGNMENT);
+		assertTrue(stmt1.getValueByRole(CtRole.ASSIGNMENT) instanceof CtTextBlock);
+		CtTextBlock l1 = (CtTextBlock) stmt1.getValueByRole(CtRole.ASSIGNMENT);
 		assertEquals(l1.getValue(), "");
 	}
 
