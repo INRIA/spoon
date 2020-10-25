51580e0f9127de129cf30c9d1cdd30f5f1047b5d
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-10-26 00:44:11 +0100

    add existence quantifier for easier meta model reasoning

---
+++ spoon-architecture/src/main/java/spoon/architecture/constraints/Exists.java
@@ -0,0 +1,14 @@
+package spoon.architecture.constraints;
+
+import java.util.function.Predicate;
+/**
+ * This class defines an exists quantifier. For every element it returns true.
+ * This allows checking, that no element of a specific type e.g. lambda is part of the meta model.
+ */
+public class Exists<T> implements Predicate<T> {
+
+	@Override
+	public boolean test(T t) {
+		return true;
+	}
+}
