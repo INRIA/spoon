ca0340576994d53412cd18401e9c446fa3da80cd
Martin Wittlinger <wittlinger.martin@gmail.com>
2020-10-26 17:14:46 +0100

    add documentation for IPrecondition

--- spoon-architecture/src/main/java/spoon/architecture/IPrecondition.java
+++ spoon-architecture/src/main/java/spoon/architecture/IPrecondition.java
@@ -3,6 +3,12 @@ package spoon.architecture;
 import java.util.Collection;
 import java.util.function.Function;
 
+/**
+ * This defines a precondition for a {@link ArchitectureTest} and is a filter for meta model elements.
+ * It's a function from a meta model to a collection of meta model elements.
+ * @param T  the type of meta model elements
+ * @param M  the meta model type
+ */
 public interface IPrecondition<T, M> extends Function<M, Collection<T>> {
 	// Marker interface
 }
