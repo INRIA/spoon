42b2c7b737f6110fd154f765a7f867c2ddc2e0e9
Martin Monperrus <martin.monperrus@gnieh.org>
2020-11-12 11:19:36 +0100

    @monperrus at work

--- chore/travis/travis-extra.sh
+++ chore/travis/travis-extra.sh
@@ -12,6 +12,7 @@ set -e
 pip install --user CommonMark==0.7.5 requests pygithub
 
 # we need Java 8 for the javadoc below
+ls /usr/lib/jvm/
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
 
 mvn -version
