adb7890225e8915470606f88c5ce87f1869e0368
MartinWitt <wittlinger.martin@gmail.com>
2020-11-15 12:55:22 +0100

    chore(jdk15): add jdk15 ci task (#3692)

--- chore/travis/travis-jdk15.sh
+++ chore/travis/travis-jdk15.sh
@@ -7,7 +7,7 @@ wget https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
 chmod +x install-jdk.sh
 
 # Use the default (the one present in the base container) collection of trusted certificate authority (CA) certificates for java
-source ./install-jdk.sh -f 10 -c
+source ./install-jdk.sh -f 15 -c
 
 # install also does verify goals
-mvn  -Djava.src.version=1.10 clean install
+mvn  -Djava.src.version=15 clean install
