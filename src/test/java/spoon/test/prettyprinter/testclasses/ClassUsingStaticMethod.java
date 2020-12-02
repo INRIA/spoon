5d874819399b16dbd72903208ce4743d4df9bf6f
Benjamin DANGLOT <benjamin.danglot@davidson.fr>
2020-12-02 17:53:06 +0100

    test: add test resources

---
+++ src/test/java/spoon/test/prettyprinter/testclasses/ClassUsingStaticMethod.java
@@ -0,0 +1,16 @@
+package spoon.test.prettyprinter.testclasses;
+
+/**
+ * @author Benjamin DANGLOT
+ * benjamin.danglot@davidson.fr
+ * 02/12/2020
+ */
+
+import static spoon.test.prettyprinter.testclasses.ClassWithStaticMethod.findFirst;
+public class ClassUsingStaticMethod {
+
+    public void callFindFirst() {
+        findFirst();
+    }
+
+}
---
+++ src/test/java/spoon/test/prettyprinter/testclasses/ClassWithStaticMethod.java
@@ -0,0 +1,14 @@
+package spoon.test.prettyprinter.testclasses;
+
+/**
+ * @author Benjamin DANGLOT
+ * benjamin.danglot@davidson.fr
+ * 02/12/2020
+ */
+public class ClassWithStaticMethod {
+
+    static void findFirst() {
+        System.out.println("");
+    }
+
+}
