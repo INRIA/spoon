cd6aea7ff2d6d5455aa94d30dd76acb4b86a38ea
Simon Larsén <slarse@slar.se>
2021-01-27 13:46:45 +0100

    Remove unused import

--- src/main/java/spoon/reflect/factory/PackageFactory.java
+++ src/main/java/spoon/reflect/factory/PackageFactory.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.StringTokenizer;
 import spoon.reflect.declaration.CtModule;
 import spoon.reflect.declaration.CtPackage;
