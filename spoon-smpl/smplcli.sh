d4a84099c3eae35a365532da5798065ad98ca809
mkforsb <mkforsb@gmail.com>
2021-01-29 12:16:52 +0100

    delete old tool files

--- spoon-smpl/smplcli.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-java -cp $(for f in $DIR/target/*.jar; do echo -n $f:; done) spoon.smpl.CommandlineApplication $@
