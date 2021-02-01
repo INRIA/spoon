0623ecd8c100134f8f04b947f8328bc1cf002623
mkforsb <mkforsb@gmail.com>
2021-02-01 02:02:07 +0100

    add main doc comment for TriConsumer

--- spoon-smpl/src/main/java/spoon/smpl/TriConsumer.java
+++ spoon-smpl/src/main/java/spoon/smpl/TriConsumer.java
@@ -2,6 +2,13 @@ package spoon.smpl;
 
 import java.util.Objects;
 
+/**
+ * TriConsumer is a functional Consumer interface of three parameters.
+ *
+ * @param <T> Type of first parameter
+ * @param <U> Type of second parameter
+ * @param <V> Type of third parameter
+ */
 @FunctionalInterface
 public interface TriConsumer<T, U, V> {
     void accept(T var1, U var2, V var3);
