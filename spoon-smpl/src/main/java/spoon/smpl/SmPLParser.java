ca5ba7b2656b6233098dac9849035276d444ada8
mkforsb <mkforsb@gmail.com>
2021-02-03 13:30:51 +0100

    remove DeletionAnchorRemover and SelfTypeReferenceNullifier simplify collectStatements and collectStatementLines add and improve comments

--- spoon-smpl/src/main/java/spoon/smpl/SmPLParser.java
+++ spoon-smpl/src/main/java/spoon/smpl/SmPLParser.java
@@ -36,8 +36,7 @@ import spoon.reflect.code.CtTypeAccess;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtMethod;
-import spoon.reflect.reference.CtTypeReference;
-import spoon.reflect.visitor.CtScanner;
+import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.smpl.formula.MetavariableConstraint;
 import spoon.smpl.formula.Not;
 import spoon.smpl.formula.True;
@@ -63,6 +62,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -83,38 +83,12 @@ public class SmPLParser {
 	 * @return SmPLRule instance corresponding to input
 	 */
 	public static SmPLRule parse(String smpl) {
-		class DeletionAnchorRemover extends CtScanner {
-			@Override
-			protected void enter(CtElement e) {
-				if (SmPLJavaDSL.isDeletionAnchor(e)) {
-					e.delete();
-				}
-			}
-		}
-
-		// Nullify type references to self so Spoon doesn't add imports for "RewrittenSmPLRule"
-		class SelfTypeReferenceNullifier extends CtScanner {
-			@Override
-			public void enter(CtElement e) {
-				// TODO: use SmPLJavaDSL as single source of the class/type name
-				if (e.toString().contains("RewrittenSmPLRule")) {
-					if (e instanceof CtTypeReference<?>) {
-						e.replace((CtElement) null);
-					}
-				}
-			}
-		}
-
 		/** the DSL contains two classes, one for all additions, and for all deletions */
 		String javaDSL = rewrite(smpl);
 		List<String> separated = separateAdditionsDeletions(javaDSL);
 
 		CtClass<?> dels = SpoonJavaParser.parseClass(separated.get(0), "RewrittenSmPLRule");
 		CtClass<?> adds = SpoonJavaParser.parseClass(separated.get(1), "RewrittenSmPLRule");
-
-		// some tricks
-		new SelfTypeReferenceNullifier().scan(dels);
-		new SelfTypeReferenceNullifier().scan(adds);
 
 		if (dels.getMethods().size() > 2) {
 			throw new IllegalArgumentException("Referring to multiple methods in match context is not supported");
@@ -127,20 +101,25 @@ public class SmPLParser {
 			throw new IllegalArgumentException("Empty match context");
 		}
 
-		// TODO: why do we need to includeMetaElements in this call to collectStatementLines?
 		// which line numbers are additions and deletions
-		Set<Integer> delsLines = collectStatementLines(delsRuleMethod, true);
+		Set<Integer> delsLines = collectStatementLines(delsRuleMethod);
 
-		// TODO: more comments are needed in the remainder of this method body
-
+		// Find the rule method in the "adds" DSL metamodel. This is needed due to the ability in SmPL to
+		//  add new methods, meaning the "adds" metamodel may contain any number of methods.
 		for (CtMethod<?> method : adds.getMethods()) {
+			// Covers two cases:
+			//  1) Patch does not modify method header, so its signature in "adds" matches the signature in "dels".
+			//  2) Patch does not add any methods, so "adds" contains exactly two methods with one covering metavars.
+			// TODO: write a test that proves case (1) to be unsound (patch modifies header and adds method matching
+			//  the original header) and then remove case (1).
 			if (method.getSignature().equals(delsRuleMethod.getSignature())
 				|| (adds.getMethods().size() == 2 && !method.getSimpleName().equals(SmPLJavaDSL.getMetavarsMethodName()))) {
 				addsRuleMethod = method;
 				break;
 			}
 
-			for (CtStatement stmt : collectStatements(method, true)) {
+			// Hopefully covers other cases by finding a context or deletion statement
+			for (CtStatement stmt : collectStatements(method)) {
 				if (SmPLJavaDSL.isDeletionAnchor(stmt) || delsLines.contains(stmt.getPosition().getLine())) {
 					addsRuleMethod = stmt.getParent(CtMethod.class);
 					break;
@@ -156,17 +135,44 @@ public class SmPLParser {
 			throw new IllegalStateException("Unable to determine rule method of additions AST");
 		}
 
-		// find appropriate anchors for all addition operations
-		Set<Integer> addsLines = collectStatementLines(addsRuleMethod, true);
+		Set<Integer> addsLines = collectStatementLines(addsRuleMethod, e -> !SmPLJavaDSL.isDeletionAnchor(e));
 
 		Set<Integer> commonLines = new HashSet<>(delsLines);
 		commonLines.retainAll(addsLines);
 
+		// find appropriate anchors for all addition operations
 		AnchoredOperationsMap anchoredOperations = anchorAdditions(addsRuleMethod, commonLines);
 
-		Set<Integer> containedCommonLines = findContainedCommonLines(addsRuleMethod, commonLines);
-		commonLines.removeAll(containedCommonLines);
+		// Find the set of context statements enclosed in added statements, and stop treating them as context
+		//  statements. This set of enclosed context statements will then be outfitted with deletion operations
+		//  from the match perspective of the patch (the "dels" perspective), which eventually leads to the
+		//  correct final result once the addition operation for the enclosing statements have been processed,
+		//  since the additions will also add the enclosed statements.
+		//
+		//  Example:
+		//   patch:
+		//    + if (true) {
+		//        int x = 0;
+		//    + }
+		//
+		//   target:
+		//    void m() {
+		//      int x = 0;
+		//    }
+		//
+		//   1) We match "int x = 0;"
+		//   2) We add "if (true) { int x = 0; }", anchored to our match for "int x = 0;"
+		//   3) We delete the "int x = 0;" matched in the first step
+		//
+		//  This trick is essentially the same as if we would rewrite the patch to say
+		//    + if (true) {
+		//    +   int x = 0;
+		//    + }
+		//    - int x = 0;
+		Set<Integer> additionEnclosedContextStatementLines = findContainedCommonLines(addsRuleMethod, commonLines);
+		commonLines.removeAll(additionEnclosedContextStatementLines);
 
+		// Place deletion operations on all deleted statements
 		for (int line : delsLines) {
 			if (!commonLines.contains(line)) {
 				anchoredOperations.addKeyIfNotExists(line);
@@ -174,21 +180,22 @@ public class SmPLParser {
 			}
 		}
 
-		for (int line : anchoredOperations.keySet()) {
-			anchoredOperations.put(line, replaceDeleteXpendOperationPair(anchoredOperations.get(line)));
-		}
+		// Replace delete-append and delete-prepend operation pairs with singular "replacement" operations
+		anchoredOperations.replaceAll((l, v) -> replaceDeleteXpendOperationPair(anchoredOperations.get(l)));
 
 		String delsSignature = delsRuleMethod.getType().toString() + " " + delsRuleMethod.getSignature();
 		String addsSignature = addsRuleMethod.getType().toString() + " " + addsRuleMethod.getSignature();
 
+		// Check for a modified method header, and if so add the appropriate operation
 		if (!delsSignature.equals(addsSignature)) {
 			anchoredOperations.addKeyIfNotExists(AnchoredOperationsMap.methodBodyAnchor);
 			anchoredOperations.get(AnchoredOperationsMap.methodBodyAnchor).add(new MethodHeaderReplaceOperation(addsRuleMethod));
 		}
 
-		new DeletionAnchorRemover().scan(adds);
-		SmPLRuleImpl result = (SmPLRuleImpl) compile(smpl, dels, commonLines, anchoredOperations);
+		// Compile the SmPLRule, which importantly includes compiling the CTL formula that encodes the patch
+		SmPLRuleImpl result = (SmPLRuleImpl) compile(smpl, dels, anchoredOperations);
 
+		// Check for added methods
 		List<String> sigs = new ArrayList<>();
 
 		for (CtMethod<?> method : dels.getMethods()) {
@@ -207,10 +214,12 @@ public class SmPLParser {
 	/**
 	 * Compile a given AST in the SmPL Java DSL, producing an SmPLRule containing a Formula.
 	 *
-	 * @param ast AST to compile
+	 * @param source Plain text SmPL source
+	 * @param ast DSL AST to compile
+	 * @param additions Map of anchored addition operations
 	 * @return SmPLRule instance
 	 */
-	public static SmPLRule compile(String source, CtClass<?> ast, Set<Integer> commonLines, AnchoredOperationsMap additions) {
+	public static SmPLRule compile(String source, CtClass<?> ast, AnchoredOperationsMap additions) {
 		String ruleName = null;
 
 		if (ast.getDeclaredField(SmPLJavaDSL.getRuleNameFieldName()) != null) {
@@ -514,7 +523,7 @@ public class SmPLParser {
 	 * Parse tokens following a dots operator, collecting all constraints and producing the relevant DSL code.
 	 *
 	 * @param tokens Token stream
-	 * @param pos    Position of dots operator in token stream
+	 * @param pos Position of dots operator in token stream
 	 * @param output Output receiver
 	 * @return Position of first unconsumed token in token stream
 	 */
@@ -610,6 +619,7 @@ public class SmPLParser {
 					if (str.contains(SmPLJavaDSL.getDotsStatementElementName() + "();") || methodHeader.test(str.substring(1).strip())) {
 						adds.append("\n");
 					} else {
+						// Add a "deletion anchor" dummy statement which we can use when anchoring addition statements.
 						adds.append(SmPLJavaDSL.getDeletionAnchorName()).append("();\n");
 					}
 				} else if (str.charAt(0) == '+') {
@@ -631,7 +641,7 @@ public class SmPLParser {
 	/**
 	 * Find appropriate anchors for all addition operations.
 	 *
-	 * @param method      SmPL rule method in the SmPL Java DSL
+	 * @param method SmPL rule method in the SmPL Java DSL
 	 * @param commonLines Set of context lines common to both the deletions and the additions ASTs
 	 * @return Map of anchors to lists of operations
 	 */
@@ -642,10 +652,10 @@ public class SmPLParser {
 	/**
 	 * Recursive helper function for anchorAdditions.
 	 *
-	 * @param e           Element to scan
+	 * @param e Element to scan
 	 * @param commonLines Set of context lines common to both the deletions and the additions ASTs
 	 * @param blockAnchor Line number of statement seen as current block-insert anchor.
-	 * @param context     Anchoring context, one of null, "methodHeader", "trueBranch" or "falseBranch"
+	 * @param context Anchoring context, one of null, "methodHeader", "trueBranch" or "falseBranch"
 	 * @return Map of anchors to lists of operations
 	 */
 	private static AnchoredOperationsMap anchorAdditions(CtElement e, Set<Integer> commonLines, int blockAnchor, String context) {
@@ -758,91 +768,72 @@ public class SmPLParser {
 	}
 
 	/**
-	 * Scan a given method in the SmPL Java DSL and collect the line numbers associated with statements in its body.
+	 * Scan a given rule method in the SmPL Java DSL and collect the line numbers associated with statements
+	 * in its body.
 	 *
 	 * @param method Rule method in SmPL Java DSL
 	 * @return Set of line numbers at which statements occur in the rule method
 	 */
-	private static Set<Integer> collectStatementLines(CtMethod<?> method, boolean includeMetaElements) {
-		Set<Integer> result = new HashSet<>();
-
-		for (CtStatement stmt : collectStatements(method, includeMetaElements)) {
-			result.add(stmt.getPosition().getLine());
-		}
-
-		return result;
+	private static Set<Integer> collectStatementLines(CtMethod<?> method) {
+		return collectStatementLines(method, e -> true);
 	}
 
 	/**
-	 * Scan a given method and collect all statement elements found in its body.
+	 * Scan a given rule method in the SmPL Java DSL and collect the line numbers associated with statements
+	 * in its body.
+	 *
+	 * @param method Rule method in SmPL Java DSL
+	 * @param predicate Predicate for filtering statement elements
+	 * @return Set of line numbers at which statements occur in the rule method
+	 */
+	private static Set<Integer> collectStatementLines(CtMethod<?> method, Predicate<CtElement> predicate) {
+		return collectStatements(method, predicate).stream()
+													.map(stmt -> stmt.getPosition().getLine())
+													.collect(Collectors.toSet());
+	}
+
+	/**
+	 * Scan a given method and collect all statement elements (excluding CtBlock) found in its body.
 	 *
 	 * @param method Method to scan
 	 * @return List of statements found in method body
 	 */
-	private static List<CtStatement> collectStatements(CtMethod<?> method, boolean includeMetaElements) {
-		class StatementCollectingScanner extends CtScanner {
-			public List<CtStatement> result = new ArrayList<>();
+	private static List<CtStatement> collectStatements(CtMethod<?> method) {
+		return collectStatements(method, e -> true);
+	}
 
-			@Override
-			protected void enter(CtElement e) {
-				if (!includeMetaElements && !SmPLJavaDSL.isExpressionMatchWrapper(e) && SmPLJavaDSL.isMetaElement(e)) {
-					return;
-				}
-
-				if (!SmPLJavaDSL.isDeletionAnchor(e) && e instanceof CtStatement && !(e instanceof CtBlock)) {
-					result.add((CtStatement) e);
-				}
-			}
-		}
-
-		StatementCollectingScanner statements = new StatementCollectingScanner();
-		statements.scan(method.getBody().getStatements());
-		return statements.result;
+	/**
+	 * Scan a given method and collect all statement elements (excluding CtBlock) found in its body.
+	 *
+	 * @param method Method to scan
+	 * @param predicate Predicate for filtering statement elements
+	 * @return List of statements found in method body
+	 */
+	private static List<CtStatement> collectStatements(CtMethod<?> method, Predicate<CtElement> predicate) {
+		return method.getElements(new TypeFilter<>(CtStatement.class))
+						.stream()
+						.filter(predicate.and(e -> e instanceof CtStatement && !(e instanceof CtBlock)))
+						.collect(Collectors.toList());
 	}
 
 	/**
 	 * Scan the given rule method in the SmPL Java DSL and find the set of statement-associated line numbers that
 	 * are included in a given set of 'common' line numbers, but for which the parent element is a block belonging
-	 * to a statement that does not occur on a line belonging to the set of 'common' line numbers.
-	 * <p>
-	 * i.e the set of context lines enclosed in non-context lines.
+	 * to a statement that does not occur on a line belonging to the set of 'common' line numbers, i.e the set of
+	 * context lines enclosed in non-context lines.
 	 *
-	 * @param method      Method in SmPL Java DSL
+	 * @param method Method in SmPL Java DSL
 	 * @param commonLines Set of 'common' line numbers
 	 * @return Set of line numbers enclosed by statements that are not associated with common lines
 	 */
 	private static Set<Integer> findContainedCommonLines(CtMethod<?> method, Set<Integer> commonLines) {
-		class ContainedCommonLineScanner extends CtScanner {
-			ContainedCommonLineScanner(int rootParent, Set<Integer> commonLines) {
-				this.rootParent = rootParent;
-				this.commonLines = commonLines;
-			}
-
-			private int rootParent;
-			private Set<Integer> commonLines;
-			public Set<Integer> result = new HashSet<>();
-
-			@Override
-			protected void enter(CtElement e) {
-				if (e instanceof CtStatement && !(e instanceof CtBlock)) {
-					int elementPos = e.getPosition().getLine();
-
-					if (!commonLines.contains(elementPos)) {
-						return;
-					}
-
-					int parentStmtPos = e.getParent().getParent().getPosition().getLine();
-
-					if (parentStmtPos != rootParent && !commonLines.contains(parentStmtPos)) {
-						result.add(elementPos);
-					}
-				}
-			}
-		}
-
-		ContainedCommonLineScanner contained = new ContainedCommonLineScanner(method.getPosition().getLine(), commonLines);
-		contained.scan(method.getBody().getStatements());
-		return contained.result;
+		return collectStatements(method)
+				.stream()
+				.filter(e -> commonLines.contains(e.getPosition().getLine()))
+				.filter(e -> e.getParent().getParent().getPosition().getLine() != method.getPosition().getLine())
+				.filter(e -> !commonLines.contains(e.getParent().getParent().getPosition().getLine()))
+				.map(e -> e.getPosition().getLine())
+				.collect(Collectors.toSet());
 	}
 
 	/**
