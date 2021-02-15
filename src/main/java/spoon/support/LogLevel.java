4a7caf241aaf8016ee2a49d66990300030afde09
Simon Larsén <slarse@slar.se>
2021-02-15 17:14:39 +0100

    Define a LogLevel enum

---
+++ src/main/java/spoon/support/LogLevel.java
@@ -0,0 +1,28 @@
+/**
+ * SPDX-License-Identifier: (MIT OR CECILL-C)
+ *
+ * Copyright (C) 2006-2019 INRIA and contributors
+ *
+ * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) of the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
+ */
+package spoon.support;
+
+public enum LogLevel {
+	ERROR(100), WARN(200), INFO(300), DEBUG(400), TRACE(500);
+
+	private final int levelValue;
+
+	LogLevel(int levelValue) {
+		this.levelValue = levelValue;
+	}
+
+	/**
+	 * The ordinal representation of this logging level. The higher this value, the more logging
+	 * output is provided (i.e. the value increases with decreasing specificity).
+	 *
+	 * @return The ordinal logging level.
+	 */
+	public int toInt() {
+		return levelValue;
+	}
+}
