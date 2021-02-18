59f72bc1a68f844ac4f1451d5dc1d859e67b6332
Simon Larsén <slarse@slar.se>
2021-02-18 14:37:00 +0100

    fix: Prevent ImportCleaner from importing inherited static methods (#3798)

--- src/test/java/spoon/reflect/visitor/ImportCleanerTest.java
+++ src/test/java/spoon/reflect/visitor/ImportCleanerTest.java
@@ -18,12 +18,25 @@ public class ImportCleanerTest {
 	@Test
 	public void testDoesNotDuplicateUnresolvedImports() {
 	    // contract: The import cleaner should not duplicate unresolved imports
+		testImportCleanerDoesNotAlterImports("./src/test/resources/unresolved/UnresolvedImport.java", "UnresolvedImport");
+	}
 
+	@Test
+	public void testDoesNotImportInheritedStaticMethod() {
+		// contract: The import cleaner should not import static attributes that are inherited
+		testImportCleanerDoesNotAlterImports("./src/test/resources/inherit-static-method", "Derived");
+	}
+
+	/**
+	 * Test that processing the target class' compilation unit with the import cleaner does not
+	 * alter the imports.
+	 */
+	private static void testImportCleanerDoesNotAlterImports(String source, String targetClassQualname) {
 		// arrange
 		Launcher launcher = new Launcher();
-		launcher.addInputResource("./src/test/resources/unresolved/UnresolvedImport.java");
+		launcher.addInputResource(source);
 		CtModel model = launcher.buildModel();
-		CtType<?> type = model.getAllTypes().stream().findFirst().get();
+		CtType<?> type = model.getUnnamedModule().getFactory().Type().get(targetClassQualname);
 		CtCompilationUnit cu = type.getFactory().CompilationUnit().getOrCreate(type);
 		List<String> importsBefore = getTextualImports(cu);
 
