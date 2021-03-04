6dc3bedb9c84084237496fe736cc532b6870b7f0
Simon Larsén <slarse@slar.se>
2021-03-04 13:32:57 +0100

    Add another expression

--- src/test/java/spoon/reflect/visitor/DefaultJavaPrettyPrinterTest.java
+++ src/test/java/spoon/reflect/visitor/DefaultJavaPrettyPrinterTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import spoon.Launcher;
 import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtStatement;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,13 +16,14 @@ public class DefaultJavaPrettyPrinterTest {
             "1 + 2 + 3",
             "1 + (2 + 3)",
             "\"Sum: \" + (1 + 2)",
+            "\"Sum: \" + 1 + 2",
             "-(1 + 2 + 3)",
             "true || true && false",
             "(true || false) && false"
     })
     public void testParenOptimizationCorrectlyPrintsParenthesesForExpressions(String rawExpression) {
         Launcher launcher = createLauncherWithOptimizeParenthesesPrinter();
-        CtExpression<?> expr =  launcher.getFactory()
+        CtExpression<?> expr = launcher.getFactory()
                 .createCodeSnippetExpression(rawExpression).compile();
         assertThat(expr.toString(), equalTo(rawExpression));
     }
