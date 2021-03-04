41db0391b3d52635d48348f39784a1e1f3573233
Simon Larsén <slarse@slar.se>
2021-03-04 14:52:17 +0100

    Remove unused imports

--- src/test/java/spoon/test/prettyprinter/PrinterTest.java
+++ src/test/java/spoon/test/prettyprinter/PrinterTest.java
@@ -16,8 +16,6 @@
  */
 package spoon.test.prettyprinter;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
