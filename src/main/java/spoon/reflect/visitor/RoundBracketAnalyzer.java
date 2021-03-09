a6a43e9a257d00e73294e5aed3282e32ed422edb
Simon Larsén <slarse@slar.se>
2021-03-09 09:19:20 +0100

    Remove funky header formatting inserted by IntelliJ

--- src/main/java/spoon/reflect/visitor/RoundBracketAnalyzer.java
+++ src/main/java/spoon/reflect/visitor/RoundBracketAnalyzer.java
@@ -1,8 +1,8 @@
 /**
  * SPDX-License-Identifier: (MIT OR CECILL-C)
- * <p>
+ *
  * Copyright (C) 2006-2019 INRIA and contributors
- * <p>
+ *
  * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) of the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
  */
 package spoon.reflect.visitor;
