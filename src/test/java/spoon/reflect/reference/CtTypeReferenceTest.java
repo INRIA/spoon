3ffd123e670093f13e41d29be220017f7d35722a
Rohitesh <thekumarjain@gmail.com>
2021-05-29 19:34:42 +0530

    migrate CtTypeReferenceTest to Junit5

--- src/test/java/spoon/reflect/reference/CtTypeReferenceTest.java
+++ src/test/java/spoon/reflect/reference/CtTypeReferenceTest.java
@@ -1,10 +1,10 @@
 package spoon.reflect.reference;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoRule;
 import org.mockito.stubbing.Answer;
 import spoon.compiler.Environment;
 import spoon.reflect.factory.Factory;
@@ -17,20 +17,20 @@ import java.util.function.Supplier;
 
 import static com.google.common.primitives.Primitives.allPrimitiveTypes;
 import static com.google.common.primitives.Primitives.wrap;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-import static org.mockito.junit.MockitoJUnit.rule;
 import static spoon.testing.utils.Check.assertNotNull;
 
+@ExtendWith(MockitoExtension.class)
 public class CtTypeReferenceTest {
     private final TypeFactory typeFactory = new TypeFactory();
-    @Rule public MockitoRule mockito = rule();
+
     @Mock private Factory factory;
     @Mock private Environment environment;
     @Mock private FineModelChangeListener listener;
     @Mock private ClassLoader classLoader;
 
-    @Before public void setUp() {
+    @BeforeEach public void setUp() {
         when(factory.Type()).thenReturn(typeFactory);
         when(factory.getEnvironment()).thenReturn(environment);
         when(environment.getModelChangeListener()).thenReturn(listener);
