ba184e72dc565f688f9ef55fba7b3f6361372728
Rohitesh Kumar Jain <thekumarjain@gmail.com>
2021-06-01 17:28:20 +0530

    test: migrate CtTypeReferenceTest to Junit5 (#3969)
    Co-authored-by: Simon Larsén <slarse@kth.se>

--- src/test/java/spoon/reflect/reference/CtTypeReferenceTest.java
+++ src/test/java/spoon/reflect/reference/CtTypeReferenceTest.java
@@ -1,10 +1,11 @@
 package spoon.reflect.reference;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoRule;
 import org.mockito.stubbing.Answer;
 import spoon.compiler.Environment;
 import spoon.reflect.factory.Factory;
@@ -17,24 +18,24 @@ import java.util.function.Supplier;
 
 import static com.google.common.primitives.Primitives.allPrimitiveTypes;
 import static com.google.common.primitives.Primitives.wrap;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-import static org.mockito.junit.MockitoJUnit.rule;
 import static spoon.testing.utils.Check.assertNotNull;
 
+@ExtendWith(MockitoExtension.class)
 public class CtTypeReferenceTest {
     private final TypeFactory typeFactory = new TypeFactory();
-    @Rule public MockitoRule mockito = rule();
+
     @Mock private Factory factory;
     @Mock private Environment environment;
     @Mock private FineModelChangeListener listener;
     @Mock private ClassLoader classLoader;
 
-    @Before public void setUp() {
+    @BeforeEach public void setUp() {
         when(factory.Type()).thenReturn(typeFactory);
         when(factory.getEnvironment()).thenReturn(environment);
         when(environment.getModelChangeListener()).thenReturn(listener);
-        when(environment.getInputClassLoader()).thenReturn(classLoader);
+        Mockito.lenient().when(environment.getInputClassLoader()).thenReturn(classLoader);
     }
 
     /**
@@ -77,7 +78,7 @@ public class CtTypeReferenceTest {
         reference.setFactory(factory);
         reference.setSimpleName(inputClass.getName());
         if (mockClassLoader) {
-            when(classLoader.loadClass(inputClass.getName()))
+            Mockito.lenient().when(classLoader.loadClass(inputClass.getName()))
                 .thenAnswer((Answer<Object>) invocationOnMock -> inputClass);
         }
 
