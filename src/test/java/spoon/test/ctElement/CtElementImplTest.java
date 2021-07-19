f5c672c9da727119f5234157bc3db1293e3e4d5f
Rohitesh <thekumarjain@gmail.com>
2021-07-19 23:11:29 +0530

    add test for removeAnnotation method

---
+++ src/test/java/spoon/test/ctElement/CtElementImplTest.java
@@ -0,0 +1,41 @@
+package spoon.test.ctElement;
+
+import org.junit.jupiter.api.Test;
+import spoon.Launcher;
+import spoon.reflect.declaration.CtAnnotation;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.factory.Factory;
+import spoon.support.compiler.FileSystemFile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CtElementImplTest {
+
+    @Test
+    public void testRemoveAnnotation() {
+        // contract: removeAnnotation returns true after removing an annotation of a class containing a single
+        // annotation, and returns false when a non existing annotation is tried to be removed
+
+        // arrange
+        Launcher spoon = new Launcher();
+        spoon.addInputResource(new FileSystemFile("./src/test/java/spoon/test/ctElement/CtElementImplTest.java"));
+        spoon.buildModel();
+        Factory factory = spoon.getFactory();
+
+        CtClass<Annotated> annotatedClass = factory.Class().get(Annotated.class);
+        assertEquals(1, annotatedClass.getAnnotations().size());
+        CtAnnotation annotationToBeRemoved = annotatedClass.getAnnotations().get(0);
+
+        // act
+        Boolean expectedBoolean = annotatedClass.removeAnnotation(annotationToBeRemoved);
+        Boolean expectedBoolean2 = annotatedClass.removeAnnotation(annotationToBeRemoved);
+
+        // assert
+        assertEquals(0, annotatedClass.getAnnotations().size());
+        assertTrue(expectedBoolean);
+        assertFalse(expectedBoolean2);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static class Annotated { }
+}
