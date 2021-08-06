3164c3baa0b5349aaa16b451c28abed32f372ea9
Rohitesh <thekumarjain@gmail.com>
2021-08-06 17:58:39 +0530

    refactor test

--- src/test/java/spoon/test/ctStatementList/CtStatementListTest.java
+++ src/test/java/spoon/test/ctStatementList/CtStatementListTest.java
@@ -6,12 +6,13 @@ import spoon.reflect.code.CtStatement;
 import spoon.reflect.code.CtStatementList;
 import spoon.reflect.factory.Factory;
 import spoon.support.reflect.code.CtStatementListImpl;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CtStatementListImplTest {
+public class CtStatementListTest {
 
     @Test
     void testInsertBeginWithListOfStatements() {
@@ -25,10 +26,10 @@ public class CtStatementListImplTest {
         CtStatement firstStatementToBeInserted = factory.Code().createCodeSnippetStatement("int first = 1;").compile();
         CtStatement secondStatementToBeInserted = factory.Code().createCodeSnippetStatement("int second = 2;").compile();
 
-        CtStatementList mainStatementList =  new CtStatementListImpl<CtStatement>();
+        CtStatementList mainStatementList = new CtStatementListImpl<CtStatement>();
         mainStatementList.addStatement(initialStatement);
 
-        CtStatementList statementListToBeAddedToTheMainList =  new CtStatementListImpl<CtStatement>();
+        CtStatementList statementListToBeAddedToTheMainList = new CtStatementListImpl<CtStatement>();
         statementListToBeAddedToTheMainList.addStatement(firstStatementToBeInserted);
         statementListToBeAddedToTheMainList.addStatement(secondStatementToBeInserted);
 
@@ -38,8 +39,10 @@ public class CtStatementListImplTest {
         // assert
         CtStatement statementAtTheBeginningAfterInsertion = mainStatementList.getStatements().get(0);
         CtStatement secondStatementAtTheBeginningOfTheListAfterInsertion = mainStatementList.getStatements().get(1);
+
         assertEquals(firstStatementToBeInserted, statementAtTheBeginningAfterInsertion);
         assertEquals(secondStatementToBeInserted, secondStatementAtTheBeginningOfTheListAfterInsertion);
+
         assertThat(firstStatementToBeInserted.getParent(), is(mainStatementList));
         assertThat(secondStatementToBeInserted.getParent(), is(mainStatementList));
     }
