55db471f2f06195344389b64b52d601f95fd10f5
andrewbwogi <bwogi.andrew@gmail.com>
2021-08-10 20:05:52 +0200

    fix: get correct descriptions from CtJavaDocImpl (#4061)

---
+++ src/test/java/spoon/test/comment/testclasses/JavaDocWithLink.java
@@ -0,0 +1,17 @@
+package spoon.test.comment.testclasses;
+
+/**
+ * {@link spoon.Launcher Launcher}.
+ */
+public class JavaDocWithLink {
+
+    /**
+     * {@link spoon.Launcher Launcher}
+     */
+    int field1;
+
+    /**
+     * {@link spoon.Launcher Launcher}. Additional text.
+     */
+    int field2;
+}
