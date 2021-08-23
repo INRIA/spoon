dd1d3e0af1e77dde49fadc4058788ff326e0a1db
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-08-24 00:02:32 +0200

    :rocket: fix implicit modifier

--- src/main/java/spoon/support/reflect/declaration/CtRecordComponentImpl.java
+++ src/main/java/spoon/support/reflect/declaration/CtRecordComponentImpl.java
@@ -1,5 +1,8 @@
 package spoon.support.reflect.declaration;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import spoon.reflect.declaration.CtField;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtRecordComponent;
@@ -7,6 +10,7 @@ import spoon.reflect.declaration.CtTypedElement;
 import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.CtVisitor;
+import spoon.support.reflect.CtExtendedModifier;
 
 public class CtRecordComponentImpl<T> extends CtNamedElementImpl implements CtRecordComponent<T> {
 
@@ -17,7 +21,7 @@ public class CtRecordComponentImpl<T> extends CtNamedElementImpl implements CtRe
     CtMethod<?> method = this.getFactory().createMethod();
     method.setSimpleName(getSimpleName());
     method.setType((CtTypeReference) getType());
-    method.addModifier(ModifierKind.PUBLIC);
+    method.setExtendedModifiers(Collections.singleton(new CtExtendedModifier(ModifierKind.PUBLIC, true)));
     method.setImplicit(true);
     method.setBody(getFactory().createCodeSnippetStatement("return " + getSimpleName()));
     return method;
@@ -28,8 +32,10 @@ public class CtRecordComponentImpl<T> extends CtNamedElementImpl implements CtRe
     CtField<?> field = this.getFactory().createField();
     field.setSimpleName(getSimpleName());
     field.setType((CtTypeReference) getType());
-    field.addModifier(ModifierKind.PRIVATE);
-    field.addModifier(ModifierKind.FINAL);
+    Set<CtExtendedModifier> modifiers = new HashSet<>();
+    modifiers.add(new CtExtendedModifier(ModifierKind.PRIVATE, true));
+    modifiers.add(new CtExtendedModifier(ModifierKind.FINAL, true));
+    field.setExtendedModifiers(modifiers);
     field.setImplicit(true);
     return field;
   }
