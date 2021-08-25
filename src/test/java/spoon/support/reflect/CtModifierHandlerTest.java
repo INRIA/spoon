dc74c05750b8419d4ea38209669d0ef4672faff0
Raghav Shankar <raghav@deepsource.io>
2021-08-25 21:38:56 +0530

    refactor tests

--- src/test/java/spoon/support/reflect/CtModifierHandlerTest.java
+++ src/test/java/spoon/support/reflect/CtModifierHandlerTest.java
@@ -1,67 +1,85 @@
 package spoon.support.reflect;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import spoon.Launcher;
 import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.factory.Factory;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CtModifierHandlerTest {
 
-    LinkedHashSet<ModifierKind> inputModifierKinds = new LinkedHashSet<>(Arrays.asList(
+    private final List<ModifierKind> inputModifierKinds = Arrays.asList(
             ModifierKind.STATIC,
             ModifierKind.PUBLIC,
             ModifierKind.ABSTRACT,
             ModifierKind.NATIVE
-    ));
-    LinkedHashSet<CtExtendedModifier> inputExtendedModifiers = inputModifierKinds.stream().map(CtExtendedModifier::new).collect(Collectors.toCollection(LinkedHashSet::new));
+    );
+    private final List<CtExtendedModifier> inputExtendedModifiers = inputModifierKinds
+            .stream()
+            .map(CtExtendedModifier::new)
+            .collect(Collectors.toList());
 
-    // these will be overwritten in each test.
-    Set<ModifierKind> modifiers;
-    Set<CtExtendedModifier> modifiersExt;
-    CtModifierHandler handler;
+    private void assertPass(CtModifierHandler handler) {
+        Set<ModifierKind> outputModifierKinds = handler.getModifiers();
+        Set<CtExtendedModifier> outputExtendedModifiers = handler.getExtendedModifiers();
 
-    @Before
-    public void setUp() {
-        Launcher l = new Launcher();
-        Factory f = l.createFactory();
-        handler = new CtModifierHandler(f.createClass("org.test.Test"));
+        // We iterate over these collections
+        Iterator<ModifierKind> inputIterator = inputModifierKinds.iterator();
+        Iterator<CtExtendedModifier> inputIteratorExt = inputExtendedModifiers.iterator();
+        Iterator<ModifierKind> outputIterator = outputModifierKinds.iterator();
+        Iterator<CtExtendedModifier> outputIteratorExt = outputExtendedModifiers.iterator();
+
+        Supplier<Boolean> hasNext = () -> inputIterator.hasNext()
+                && inputIteratorExt.hasNext()
+                && outputIterator.hasNext()
+                && outputIteratorExt.hasNext();
+
+        while (hasNext.get()) {
+            assertEquals(inputIterator.next(), outputIterator.next());
+            assertEquals(inputIteratorExt.next(), outputIteratorExt.next());
+        }
+
+        // None of these iterators should have any elements left.
+        assertFalse(inputIterator.hasNext()
+                || inputIteratorExt.hasNext()
+                || outputIterator.hasNext()
+                || outputIteratorExt.hasNext()
+        );
     }
 
     @Test
     public void testAddModifier() {
-        inputModifierKinds.forEach(handler::addModifier);
-        modifiersExt = handler.getExtendedModifiers();
-        modifiers = handler.getModifiers();
+        Launcher launcher = new Launcher();
+        Factory factory = launcher.createFactory();
+        CtModifierHandler handler = new CtModifierHandler(factory.createClass("org.test.Test"));
 
-        assertEquals("Order of modifiers was not preserved.", inputModifierKinds, modifiers);
-        assertEquals("Order of extended modifiers was not preserved.", inputExtendedModifiers, modifiersExt);
+        inputModifierKinds.forEach(handler::addModifier);
+        assertPass(handler);
     }
 
     @Test
     public void testSetModifiers() {
-        handler.setModifiers(inputModifierKinds);
-        modifiersExt = handler.getExtendedModifiers();
-        modifiers = handler.getModifiers();
+        Launcher launcher = new Launcher();
+        Factory factory = launcher.createFactory();
+        CtModifierHandler handler = new CtModifierHandler(factory.createClass("org.test.Test"));
 
-        assertEquals("Order of modifiers was not preserved.", inputModifierKinds, modifiers);
-        assertEquals("Order of extended modifiers was not preserved.", inputExtendedModifiers, modifiersExt);
+        handler.setModifiers(new LinkedHashSet<>(inputModifierKinds));
+        assertPass(handler);
     }
 
     @Test
     public void testSetExtendedModifiers() {
-        handler.setExtendedModifiers(inputExtendedModifiers);
-        modifiersExt = handler.getExtendedModifiers();
-        modifiers = handler.getModifiers();
+        Launcher launcher = new Launcher();
+        Factory factory = launcher.createFactory();
+        CtModifierHandler handler = new CtModifierHandler(factory.createClass("org.test.Test"));
 
-        assertEquals("Order of modifiers was not preserved.", inputModifierKinds, modifiers);
-        assertEquals("Order of extended modifiers was not preserved.", inputExtendedModifiers, modifiersExt);
+        handler.setExtendedModifiers(new LinkedHashSet<>(inputExtendedModifiers));
+        assertPass(handler);
     }
 }
