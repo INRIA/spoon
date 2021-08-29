e26497d5a80005fba1e0f4aebeb417a7f8470df8
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-08-30 01:16:27 +0200

    add imports

--- spoon-smpl/src/main/java/spoon/smpl/Substitutor.java
+++ spoon-smpl/src/main/java/spoon/smpl/Substitutor.java
@@ -97,6 +97,8 @@ import spoon.reflect.declaration.CtPackageDeclaration;
 import spoon.reflect.declaration.CtPackageExport;
 import spoon.reflect.declaration.CtParameter;
 import spoon.reflect.declaration.CtProvidedService;
+import spoon.reflect.declaration.CtRecord;
+import spoon.reflect.declaration.CtRecordComponent;
 import spoon.reflect.declaration.CtTypeParameter;
 import spoon.reflect.declaration.CtUsedService;
 import spoon.reflect.reference.CtArrayTypeReference;
