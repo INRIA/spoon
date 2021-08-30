93eeb7c852e375b959b676387baf4af2d3103af4
Wouter Smeenk <wouter.smeenk@gmail.com>
2021-08-30 18:02:55 +0200

    test: Add test for sniper printer issues (#3812)
    Co-authored-by: Martin Monperrus <martin.monperrus@gnieh.org>

---
+++ src/test/java/spoon/test/prettyprinter/testclasses/difftest/ExceptionTest.java
@@ -0,0 +1,11 @@
+package spoon.test.prettyprinter.testclasses.difftest;
+
+public class ExceptionTest {
+    public void test() {
+        try {
+            throw new Exception("test");
+        } catch (Exception ex) {
+
+        }
+    }
+}
