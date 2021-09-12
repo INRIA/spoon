a831b32bda246dec33347b0d513ed34cbefccca0
SirYwell <hannesgreule@outlook.de>
2021-09-12 21:16:58 +0200

    implement asserts in tests

--- src/test/java/spoon/test/sealedclasses/SealedClassesTest.java
+++ src/test/java/spoon/test/sealedclasses/SealedClassesTest.java
@@ -4,31 +4,64 @@ import org.junit.jupiter.api.Test;
 import spoon.Launcher;
 import spoon.reflect.CtModel;
 import spoon.reflect.declaration.CtEnum;
+import spoon.reflect.declaration.CtPackage;
+import spoon.reflect.declaration.CtType;
 import spoon.reflect.declaration.ModifierKind;
+import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.support.reflect.CtExtendedModifier;
 
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class SealedClassesTest {
 
 	@Test
-	void testSealedClassWithInnerSubclassModel() {
+	void testSealedClassWithInnerSubclassModelImplicit() {
+		// contract: inner subtypes are implicit and in the permitted types set
 		Launcher launcher = new Launcher();
 		launcher.getEnvironment().setComplianceLevel(16);
 		launcher.getEnvironment().setPreviewFeaturesEnabled(true);
 
 		launcher.addInputResource("src/test/resources/sealedclasses/SealedClassWithNestedSubclasses.java");
 		CtModel ctModel = launcher.buildModel();
-		ctModel.getAllTypes().forEach(System.out::println);
-		int a = 3;
+		CtType<?> outer = ctModel.getAllTypes().iterator().next();
+
+		for (CtTypeReference<?> permitted : outer.getPermittedTypes()) {
+			assertThat(permitted.isImplicit(), is(true));
+		}
+	}
+
+	@Test
+	void testSealedClassWithInnerSubclassModelModifiers() {
+		// contract: sealed and non-sealed modifiers are present in the model
+		Launcher launcher = new Launcher();
+		launcher.getEnvironment().setComplianceLevel(16);
+		launcher.getEnvironment().setPreviewFeaturesEnabled(true);
+
+		launcher.addInputResource("src/test/resources/sealedclasses/SealedClassWithNestedSubclasses.java");
+		CtModel ctModel = launcher.buildModel();
+		CtType<?> outer = ctModel.getAllTypes().iterator().next();
+		assertThat(outer.getExtendedModifiers(), hasItems(new CtExtendedModifier(ModifierKind.SEALED, false)));
+
+		CtType<?> nestedFinal = outer.getNestedType("NestedFinal");
+		assertThat(nestedFinal.getExtendedModifiers(), hasItem(new CtExtendedModifier(ModifierKind.FINAL, false)));
+		assertThat(outer.getPermittedTypes(), hasItem(nestedFinal.getReference()));
+
+		CtType<?> nestedNonSealed = outer.getNestedType("NestedNonSealed");
+		assertThat(nestedNonSealed.getExtendedModifiers(), hasItem(new CtExtendedModifier(ModifierKind.NON_SEALED, false)));
+		assertThat(outer.getPermittedTypes(), hasItem(nestedNonSealed.getReference()));
 	}
 
 	@Test
 	void testEnumSealed() {
+		// contract: enums with anonymous enum values are sealed and the anonymous types are final
 		Launcher launcher = new Launcher();
 		launcher.getEnvironment().setComplianceLevel(16);
 		launcher.getEnvironment().setPreviewFeaturesEnabled(true);
@@ -37,16 +70,14 @@ public class SealedClassesTest {
 		CtModel ctModel = launcher.buildModel();
 		CtEnum<?> ctEnum = ctModel.getElements(new TypeFilter<CtEnum<?>>(CtEnum.class)).get(0);
 		// not final
-		assertFalse(ctEnum.isFinal());
+		assertThat(ctEnum.isFinal(), is(false));
 		// but (implicitly) sealed
 		assertThat(ctEnum.getExtendedModifiers(), hasItem(new CtExtendedModifier(ModifierKind.SEALED, true)));
-
-		ctModel.getAllTypes().forEach(System.out::println);
-		int a = 3;
 	}
 
 	@Test
 	void testMultiCompilationUnitSealed() {
+		// contract: extending types in other compilation units are present in the permitted types set
 		Launcher launcher = new Launcher();
 		launcher.getEnvironment().setComplianceLevel(16);
 		launcher.getEnvironment().setPreviewFeaturesEnabled(true);
@@ -55,7 +86,17 @@ public class SealedClassesTest {
 		launcher.addInputResource("src/test/resources/sealedclasses/ExtendingClass.java");
 		launcher.addInputResource("src/test/resources/sealedclasses/OtherExtendingClass.java");
 		CtModel ctModel = launcher.buildModel();
-		ctModel.getAllTypes().forEach(System.out::println);
-		int a = 3;
+		CtPackage ctPackage = ctModel.getAllPackages().iterator().next();
+		CtType<?> sealedClassWithPermits = ctPackage.getType("SealedClassWithPermits");
+		CtType<?> extendingClass = ctPackage.getType("ExtendingClass");
+		CtType<?> otherExtendingClass = ctPackage.getType("OtherExtendingClass");
+
+		assertThat(sealedClassWithPermits.getPermittedTypes().size(), is(2));
+		for (CtTypeReference<?> permittedType : sealedClassWithPermits.getPermittedTypes()) {
+			// outer types are always explicit
+			assertThat(permittedType.isImplicit(), is(false));
+		}
+		assertThat(sealedClassWithPermits.getPermittedTypes(), hasItem(extendingClass.getReference()));
+		assertThat(sealedClassWithPermits.getPermittedTypes(), hasItem(otherExtendingClass.getReference()));
 	}
 }
