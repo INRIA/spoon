eb2e9f96d9ca8270b9d091e4064cb650172404e3
SirYwell <hannesgreule@outlook.de>
2021-10-06 11:14:11 +0200

    get rid of unused imports

--- src/main/java/spoon/support/StandardEnvironment.java
+++ src/main/java/spoon/support/StandardEnvironment.java
@@ -46,7 +46,6 @@ import java.net.URLClassLoader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
