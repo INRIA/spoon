e19f82c3795536687e7a68e467717944996acd96
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-10-13 14:29:43 +0200

    fix license header

--- src/main/java/spoon/support/visitor/clone/CloneBuilder.java
+++ src/main/java/spoon/support/visitor/clone/CloneBuilder.java
@@ -5,12 +5,6 @@
  *
  * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) of the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
  */
-/* SPDX-License-Identifier: (MIT OR CECILL-C)
-
-Copyright (C) 2006-2019 INRIA and contributors
-
-Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) of the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
- */
 package spoon.support.visitor.clone;
 /**
  * Used to set all data in the cloned element.
