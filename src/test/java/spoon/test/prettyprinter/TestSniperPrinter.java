73c390c3500d796c8d2b25c038c80a784c1f0579
Aman Sharma <mannu.poski10@gmail.com>
2021-11-11 23:14:53 +0100

    Introduce failing test case for #4218

--- src/test/java/spoon/test/prettyprinter/TestSniperPrinter.java
+++ src/test/java/spoon/test/prettyprinter/TestSniperPrinter.java
@@ -28,6 +28,7 @@ import spoon.reflect.code.CtLocalVariable;
 import spoon.reflect.code.CtStatement;
 import spoon.reflect.code.CtThrow;
 import spoon.reflect.cu.SourcePosition;
+import spoon.reflect.declaration.CtAnnotation;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtCompilationUnit;
 import spoon.reflect.declaration.CtElement;
@@ -39,7 +40,6 @@ import spoon.reflect.declaration.CtPackage;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.factory.Factory;
-import spoon.reflect.path.CtRole;
 import spoon.reflect.reference.CtExecutableReference;
 import spoon.reflect.reference.CtReference;
 import spoon.reflect.reference.CtTypeReference;
@@ -74,7 +74,7 @@ import java.util.stream.Stream;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.anyOf;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -795,6 +795,19 @@ public class TestSniperPrinter {
 				assertThat(result, containsString("super.a(-x);"));
 
 		testSniper("superCall.SuperCallSniperTestClass", deleteForUpdate, assertContainsSuperWithUnaryOperator);
+	}
+
+	@Test
+	@GitHubIssue(issueNumber = 4218)
+	void testSniperDoesNotPrintTheDeletedAnnotation() {
+		Consumer<CtType<?>> deleteAnnotation = type -> {
+			type.getAnnotations().forEach(CtAnnotation::delete);
+		};
+
+		BiConsumer<CtType<?>, String> assertDoesNotContainAnnotation = (type, result) ->
+				assertThat(result, not(containsString("@abc.def.xyz")));
+
+		testSniper("sniperPrinter.DeleteAnnotation", deleteAnnotation, assertDoesNotContainAnnotation);
 	}
 
 	/**
