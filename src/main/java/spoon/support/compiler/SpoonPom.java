282f1f65951b1df2a6c5e013a59df26e0cc6e133
Aman Sharma <mannu.poski10@gmail.com>
2021-11-19 16:13:53 +0100

    Remove unused import

--- src/main/java/spoon/support/compiler/SpoonPom.java
+++ src/main/java/spoon/support/compiler/SpoonPom.java
@@ -34,7 +34,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
