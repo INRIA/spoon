c57d45d3f916ed649af6405b5d6171ca21500108
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-11-20 10:13:10 +0100

    Introduce problem

--- src/main/java/spoon/Launcher.java
+++ src/main/java/spoon/Launcher.java
@@ -101,6 +101,10 @@ public class Launcher implements SpoonAPI {
 	 * arguments and calls {@link #run()}).
 	 */
 	public static void main(String[] args) {
+		String a = null;
+		if (a.isEmpty()) {
+			System.out.println(a.length());
+		}
 		new Launcher().run(args);
 	}
 
