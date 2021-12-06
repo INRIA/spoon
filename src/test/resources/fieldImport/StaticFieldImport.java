6c91c544ee68a18861801f52e6cd06541dc8424e
Aman Sharma <mannu.poski10@gmail.com>
2021-12-06 14:48:03 +0100

    Reproduce SpoonLabs/sorald#602

---
+++ src/test/resources/fieldImport/StaticFieldImport.java
@@ -0,0 +1,10 @@
+package fieldImport;
+
+// this should not be removed
+import static fieldImport.UtilityLibrary.FirstUtility.LIMIT;
+
+public class StaticFieldImport {
+    public static void main(String[] args) {
+        System.out.println(LIMIT.value);
+    }
+}
---
+++ src/test/resources/fieldImport/UtilityLibrary.java
@@ -0,0 +1,11 @@
+package fieldImport;
+
+public class UtilityLibrary {
+    public static class FirstUtility {
+        public static final Parameter LIMIT = new Parameter();
+
+        public class Parameter {
+            public static Integer value;
+        }
+    }
+}
