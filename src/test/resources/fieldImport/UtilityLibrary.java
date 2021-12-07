2a1b2a553b6b99a56cd5f2e72f8c853e916e0cd3
Aman Sharma <mannu.poski10@gmail.com>
2021-12-07 14:23:29 +0100

    Make Parameter inner class as static

--- src/test/resources/fieldImport/UtilityLibrary.java
+++ src/test/resources/fieldImport/UtilityLibrary.java
@@ -4,7 +4,7 @@ public class UtilityLibrary {
     public static class FirstUtility {
         public static final Parameter LIMIT = new Parameter();
 
-        public class Parameter {
+        public static class Parameter {
             public static Integer value;
         }
     }
