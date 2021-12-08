b75b0de4fe32f167499b282fa4efabcfda0efc5f
SirYwell <hannesgreule@outlook.de>
2021-12-08 20:21:16 +0100

    add a few javadocs

--- src/main/java/spoon/reflect/declaration/CtSealable.java
+++ src/main/java/spoon/reflect/declaration/CtSealable.java
@@ -18,17 +18,48 @@ import java.util.Set;
 // TODO docs
 // TODO return type?
 
+/**
+ * This interface represents any type that can be sealed.
+ * See JLS 8.1.1.2
+ */
 public interface CtSealable {
 
+	/**
+	 * Returns the permitted types for this type.
+	 *
+	 * @return an unmodifiable view of the permitted types.
+	 */
 	@PropertyGetter(role = CtRole.PERMITTED_TYPE)
 	Set<CtTypeReference<?>> getPermittedTypes();
 
+	/**
+	 * Sets the permitted types for this type.
+	 * Calling this method does not change the state of the {@link ModifierKind#SEALED} for this type.
+	 * The previously permitted types will be removed.
+	 *
+	 * @param permittedTypes the permitted types to set.
+	 * @return this.
+	 */
 	@PropertySetter(role = CtRole.PERMITTED_TYPE)
 	CtSealable setPermittedTypes(Collection<CtTypeReference<?>> permittedTypes);
 
+	/**
+	 * Adds a permitted type to this type.
+	 * Calling this method does not change the state of the {@link ModifierKind#SEALED} for this type.
+	 *
+	 * @param type the type to add as permitted type.
+	 * @return this.
+	 */
 	@PropertySetter(role = CtRole.PERMITTED_TYPE)
 	CtSealable addPermittedType(CtTypeReference<?> type);
 
+	/**
+	 * Adds a permitted type to this type.
+	 * Calling this method does not change the state of the {@link ModifierKind#SEALED} for this type.
+	 *
+	 * @param type the type to remove from this type's permitted types.
+	 * @return this.
+	 */
 	@PropertySetter(role = CtRole.PERMITTED_TYPE)
 	CtSealable removePermittedType(CtTypeReference<?> type);
 }
