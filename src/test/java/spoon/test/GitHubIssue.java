9cf7f25f10e53a2dddca4083003582155e6cf0b1
Aman Sharma <mannu.poski10@gmail.com>
2021-12-10 15:53:12 +0100

    Use GitHubIssue annotation for types as well

--- src/test/java/spoon/test/GitHubIssue.java
+++ src/test/java/spoon/test/GitHubIssue.java
@@ -14,7 +14,7 @@ import java.lang.annotation.Target;
 
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface GitHubIssue {
 	int issueNumber();
 }
