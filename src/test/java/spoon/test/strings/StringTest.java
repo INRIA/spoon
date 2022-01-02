a449b2c6f810abfc1d8825cb1e5b3bcd4c580a78
MartinWitt <wittlinger.martin@gmail.com>
2022-01-02 10:37:13 +0100

    refactor(*): Convert StringTests to junit5 (#4387)

--- src/test/java/spoon/test/strings/StringTest.java
+++ src/test/java/spoon/test/strings/StringTest.java
@@ -16,15 +16,15 @@
  */
 package spoon.test.strings;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import spoon.reflect.code.BinaryOperatorKind;
 import spoon.reflect.code.CtBinaryOperator;
 import spoon.reflect.code.CtLiteral;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.visitor.filter.TypeFilter;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static spoon.testing.utils.ModelUtils.build;
 
 public class StringTest {
