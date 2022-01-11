a5606982c1b3bf37a3bbaa4c5caf352028169575
MartinWitt <wittlinger.martin@gmail.com>
2022-01-11 08:21:42 +0100

    test: migrate AssignmentsEqualsTest to junit 5 (#4424)

--- src/test/java/spoon/test/visitor/AssignmentsEqualsTest.java
+++ src/test/java/spoon/test/visitor/AssignmentsEqualsTest.java
@@ -1,14 +1,16 @@
 package spoon.test.visitor;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import java.util.List;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import spoon.Launcher;
 import spoon.reflect.code.CtAssignment;
 import spoon.reflect.factory.Factory;
 import spoon.reflect.visitor.Query;
 import spoon.reflect.visitor.filter.TypeFilter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class AssignmentsEqualsTest {
 
@@ -20,12 +22,11 @@ public class AssignmentsEqualsTest {
 
 		Factory factory = launcher.getFactory();
 		List<CtAssignment> assignments = Query.getElements(factory, new TypeFilter<>(CtAssignment.class));
-
-		assertTrue(assignments.size() == 10);
-		assertFalse(assignments.get(0).equals(assignments.get(1)));
-		assertFalse(assignments.get(2).equals(assignments.get(3)));
-		assertFalse(assignments.get(4).equals(assignments.get(5)));
-		assertFalse(assignments.get(6).equals(assignments.get(7)));
-		assertTrue(assignments.get(8).equals(assignments.get(9)));
+		assertEquals(assignments.size(), 10);
+		assertNotEquals(assignments.get(0), assignments.get(1));
+		assertNotEquals(assignments.get(2), assignments.get(3));
+		assertNotEquals(assignments.get(4), assignments.get(5));
+		assertNotEquals(assignments.get(6), assignments.get(7));
+		assertEquals(assignments.get(8), assignments.get(9));
 	}
 }
