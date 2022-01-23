35b4abc934e6cb8f35bb0e2026ce219b5ed09208
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-01-23 22:28:54 +0100

    :rocket:

--- src/test/java/spoon/test/intercession/OneCanCallSetterWithNullParameterizedTest.java
+++ src/test/java/spoon/test/intercession/OneCanCallSetterWithNullParameterizedTest.java
@@ -16,9 +16,8 @@
  */
 package spoon.test.intercession;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import spoon.Launcher;
 import spoon.processing.FactoryAccessor;
 import spoon.reflect.declaration.CtMethod;
@@ -32,54 +31,47 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 import static spoon.test.parent.ContractOnSettersParametrizedTest.createCompatibleObject;
 
 // contract: one can call all setters with null as parameter (no problem with parent)
-@RunWith(Parameterized.class)
 public class OneCanCallSetterWithNullParameterizedTest {
 
-	@Parameterized.Parameters(name = "{1}")
-	public static Collection<Object[]> data() {
-		final Launcher launcher = new Launcher();
-		final Factory factory = launcher.getFactory();
+	@TestFactory
+	public Collection<DynamicTest> createTests() {
+		Launcher launcher = new Launcher();
 		launcher.getEnvironment().setNoClasspath(true);
 		// all metamodel interfaces.
 		launcher.addInputResource("./src/main/java/spoon/reflect/code");
 		launcher.addInputResource("./src/main/java/spoon/reflect/declaration");
 		launcher.addInputResource("./src/main/java/spoon/reflect/reference");
 		launcher.buildModel();
-
-		final List<Object[]> values = new ArrayList<>();
+		List<DynamicTest> tests = new ArrayList<>();
 		new IntercessionScanner(launcher.getFactory()) {
 			@Override
 			protected boolean isToBeProcessed(CtMethod<?> candidate) {
-				return (candidate.getSimpleName().startsWith("set") //
-						|| candidate.getSimpleName().startsWith("add")) //
-						&& takeSetterForCtElement(candidate); //
+				return (candidate.getSimpleName().startsWith("set")
+						|| candidate.getSimpleName().startsWith("add"))
+						&& takeSetterForCtElement(candidate);
 			}
 
 			@Override
 			protected void process(CtMethod<?> element) {
-				values.add(new Object[] { createCompatibleObject(element.getDeclaringType().getReference()), element.getReference().getActualMethod() });
+				tests.add(dynamicTest(element.getReference().getActualMethod().getName(),
+						() -> testNullSafe(createCompatibleObject(element.getDeclaringType().getReference()),
+								element.getReference().getActualMethod())));
 			}
 		}.scan(launcher.getModel().getRootPackage());
-		return values;
+		return tests;
 	}
 
-	@Parameterized.Parameter(0)
-	public Object instance;
 
-	@Parameterized.Parameter(1)
-	public Method toTest;
-
-	@Test
-	public void testContract() throws Throwable {
+	private void testNullSafe(Object instance, Method setter) throws Throwable {
 		Factory factory = new FactoryImpl(new DefaultCoreFactory(),new StandardEnvironment());
-		Object element = instance;
-		if (element instanceof FactoryAccessor) {
-			((FactoryAccessor) element).setFactory(factory);
+		if (instance instanceof FactoryAccessor) {
+			((FactoryAccessor) instance).setFactory(factory);
 		}
 		// we invoke the setter
-		toTest.invoke(element, new Object[] { null });
+		setter.invoke(instance, new Object[] { null });
 	}
 }
