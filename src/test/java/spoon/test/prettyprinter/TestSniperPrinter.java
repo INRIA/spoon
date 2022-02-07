79e7b9f7648bb8f39c71e84ee1addc6cf3e62ceb
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-02-07 12:19:33 +0100

    Revert "showcase that tests are running"
    This reverts commit 003dffb9e309d22b57d9f4949f6698c85c879d46.

--- src/test/java/spoon/test/prettyprinter/TestSniperPrinter.java
+++ src/test/java/spoon/test/prettyprinter/TestSniperPrinter.java
@@ -787,10 +787,7 @@ public class TestSniperPrinter {
 	@GitHubIssue(issueNumber = 4021, open = false)
 	void testSniperRespectsSuperWithUnaryOperator() {
 		// Combining CtSuperAccess and CtUnaryOperator leads to SpoonException with Sniper
-		throw new RuntimeException(
-				"java.lang.IllegalArgumentException: Cannot create a binary operator with a null left hand side"
-		);
-		/*
+
 		// Noop
 		Consumer<CtType<?>> deleteForUpdate = type -> {};
 
@@ -798,7 +795,6 @@ public class TestSniperPrinter {
 				assertThat(result, containsString("super.a(-x);"));
 
 		testSniper("superCall.SuperCallSniperTestClass", deleteForUpdate, assertContainsSuperWithUnaryOperator);
-		*/
 	}
 
 	@GitHubIssue(issueNumber = 3911)
