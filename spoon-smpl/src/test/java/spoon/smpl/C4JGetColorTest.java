dfa18741991bfdd6026370bd466083f437ff91f8
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-02-12 10:25:56 +0100

    more newlines...

--- spoon-smpl/src/test/java/spoon/smpl/C4JGetColorTest.java
+++ spoon-smpl/src/test/java/spoon/smpl/C4JGetColorTest.java
@@ -26,7 +26,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import spoon.reflect.declaration.CtMethod;
 
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
