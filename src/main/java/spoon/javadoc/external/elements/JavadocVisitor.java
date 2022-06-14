9fa8a7c5e91329d1aa7fbbe4834f1679319e1d57
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2022-06-14 18:08:48 +0200

    Return a default value in the visitor
    I am not sure I like forcing users to provide the default value.

--- src/main/java/spoon/javadoc/external/elements/JavadocVisitor.java
+++ src/main/java/spoon/javadoc/external/elements/JavadocVisitor.java
@@ -17,56 +17,59 @@ import spoon.javadoc.external.elements.snippets.JavadocSnippetTag;
 public interface JavadocVisitor<T> {
 
 	/**
+	 * @return the default value to return if a visit method is not overwritten
+	 */
+	T defaultValue();
+
+	/**
 	 * @param tag the inline tag to visit
 	 * @return a return value
-	 * @implNote the default implementation always returns null and visits all arguments
+	 * @implNote the default implementation visits all arguments
 	 */
 	default T visitInlineTag(JavadocInlineTag tag) {
 		for (JavadocElement element : tag.getElements()) {
 			element.accept(this);
 		}
-		return null;
+		return defaultValue();
 	}
 
 	/**
 	 * @param tag the block tag to visit
 	 * @return a return value
-	 * @implNote the default implementation always returns null and visits all elements
+	 * @implNote the default implementation visits all elements
 	 */
 	default T visitBlockTag(JavadocBlockTag tag) {
 		for (JavadocElement element : tag.getElements()) {
 			element.accept(this);
 		}
-		return null;
+		return defaultValue();
 	}
 
 	/**
 	 * @param snippet the snippet tag to visit
 	 * @return a return value
-	 * @implNote the default implementation always returns null and visits all elements
+	 * @implNote the default implementation visits all elements
 	 */
 	default T visitSnippet(JavadocSnippetTag snippet) {
 		for (JavadocElement element : snippet.getElements()) {
 			element.accept(this);
 		}
-		return null;
+		return defaultValue();
 	}
 
 	/**
 	 * @param text the javadoc text to visit
 	 * @return a return value
-	 * @implNote the default implementation always returns null
 	 */
 	default T visitText(JavadocText text) {
-		return null;
+		return defaultValue();
 	}
 
 	/**
 	 * @param reference the javadoc reference to visit
 	 * @return a return value
-	 * @implNote the default implementation always returns null
 	 */
 	default T visitReference(JavadocReference reference) {
-		return null;
+		return defaultValue();
 	}
 }
