4ee020a4b5c50173e642c032f30fa9ae7a7072cd
Simon Larsén <slarse@slar.se>
2022-07-09 12:01:36 +0200

    Replace Test annotation with GithubIssue

--- src/test/java/spoon/reflect/factory/PackageFactoryTest.java
+++ src/test/java/spoon/reflect/factory/PackageFactoryTest.java
@@ -1,17 +1,16 @@
 package spoon.reflect.factory;
 
-import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import spoon.Launcher;
 import spoon.reflect.declaration.CtPackage;
+import spoon.test.GitHubIssue;
 
 class PackageFactoryTest {
 
-	@Test
+	@GitHubIssue(issueNumber = 4764, fixed = true)
 	void getOrCreate_returnsNestedPackageStructure_whenQualifiedNameRepeatsSimpleName() {
 		// contract: A qualified name that is simply repetitions of a single simple name results in the expected
 		// nested package structure
