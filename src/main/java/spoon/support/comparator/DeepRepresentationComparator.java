0defecc13c78e6007527e0656d2f8ed9ec5058f5
MartinWitt <wittlinger.martin@gmail.com>
2022-08-21 10:25:23 +0200

    Update src/main/java/spoon/support/comparator/DeepRepresentationComparator.java
    Co-authored-by: Simon Larsén <slarse@slar.se>

--- src/main/java/spoon/support/comparator/DeepRepresentationComparator.java
+++ src/main/java/spoon/support/comparator/DeepRepresentationComparator.java
@@ -28,7 +28,7 @@ public class DeepRepresentationComparator implements Comparator<CtElement>, Seri
 		}
 		String current = getDeepRepresentation(o1);
 		String other = getDeepRepresentation(o2);
-		if (current.length() == 0 || other.length() == 0) {
+		if (current.isEmpty() || other.isEmpty()) {
 			throw new ClassCastException("Unable to compare elements");
 		}
 		return current.compareTo(other);
