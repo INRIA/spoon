d2e31bad86d4156e3f55a6e855dc3dc13c6d9a9c
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2022-09-08 22:00:11 +0200

    Remove javadoc package from architecture enforcer test

--- src/test/java/spoon/test/architecture/SpoonArchitectureEnforcerTest.java
+++ src/test/java/spoon/test/architecture/SpoonArchitectureEnforcerTest.java
@@ -377,7 +377,6 @@ public class SpoonArchitectureEnforcerTest {
 		officialPackages.add("spoon.compiler");
 		officialPackages.add("spoon.javadoc");
 		officialPackages.add("spoon.javadoc.internal");
-		officialPackages.add("spoon.javadoc.external");
 		officialPackages.add("spoon.support.modelobs.action");
 		officialPackages.add("spoon.support.modelobs.context");
 		officialPackages.add("spoon.support.modelobs");
