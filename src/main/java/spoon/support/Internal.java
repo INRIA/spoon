f3ae9b521aad457e35d3feeede97c1c02fe43597
I-Al-Istannen <I-Al-Istannen@users.noreply.github.com>
2022-09-11 14:48:56 +0200

    fix: Fix getOriginalSourceFragment and getRawJavadoc for zip file inputs (#4850)

--- src/main/java/spoon/support/Internal.java
+++ src/main/java/spoon/support/Internal.java
@@ -13,10 +13,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Tells that a class or method is not in the public API (even if it has Java visibility "public")
+ * Tells that an element is not in the public API (even if it has Java visibility "public")
  * Required because package-visibility is too coarse-grained.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD, ElementType.TYPE })
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.TYPE })
 public @interface Internal {
 }
