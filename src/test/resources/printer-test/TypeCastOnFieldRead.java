5361c2ae9ad5db961d5fc5f280f270337ce234d2
Aman Sharma <mannu.poski10@gmail.com>
2022-11-10 16:39:23 +0100

    Incorporate @SirYwell's test case
    Co-Authored-By: Hannes Greule <SirYwell@users.noreply.github.com>

--- src/test/resources/printer-test/TypeCastOnFieldRead.java
+++ src/test/resources/printer-test/TypeCastOnFieldRead.java
@@ -2,5 +2,6 @@ public class TypeCastOnFieldRead {
     double myDouble = 0.0;
     public void where() {
         int myInt = (int) myDouble;
+        int myInt2 = ((Double) myDouble).intValue();
     }
 }
