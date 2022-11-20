f4c3aeb4068071bc8b4a7fe98d75ed2aa38ba808
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2022-11-20 10:42:12 +0100

    Revert some formatting to minimize diff

--- src/main/java/spoon/support/compiler/jdt/ReferenceBuilder.java
+++ src/main/java/spoon/support/compiler/jdt/ReferenceBuilder.java
@@ -86,6 +86,7 @@ import spoon.reflect.reference.CtWildcardReference;
 import spoon.support.Level;
 import spoon.support.reflect.CtExtendedModifier;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
