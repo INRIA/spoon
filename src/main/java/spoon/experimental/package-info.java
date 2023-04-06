d1f6e1a944665506c3b0c1e28c0201058691dcf8
Philippe Ombredanne <pombredanne@gmail.com>
2023-04-06 16:02:47 +0200

    doc: update license year and text (#5146)
    Co-authored-by: Martin Monperrus <martin.monperrus@gnieh.org>

--- src/main/java/spoon/experimental/package-info.java
+++ src/main/java/spoon/experimental/package-info.java
@@ -1,9 +1,9 @@
 /*
  * SPDX-License-Identifier: (MIT OR CECILL-C)
  *
- * Copyright (C) 2006-2019 INRIA and contributors
+ * Copyright (C) 2006-2023 INRIA and contributors
  *
- * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) of the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
+ * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) or the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
  */
 /**
  * this is unstable code, subject to potential complete redesign, not only in snapshots but also in releases. you can use this code but you'll probably be impacted upon future changes.
