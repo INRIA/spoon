55fee54f3bc3e90e9d9ef29922c890a05df359e1
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2023-08-30 11:38:12 +0200

    chore: Fix release scripts to at least execute jreleaser in dry run

--- chore/release-beta.sh
+++ chore/release-beta.sh
@@ -1,3 +1,4 @@
+CURRENT_VERSION="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | sed 's/-SNAPSHOT//')"
 CURRENT_VERSION_WITH_SNAPSHOT="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)"
 
 if [[ ! $CURRENT_VERSION_WITH_SNAPSHOT =~ .*-SNAPSHOT ]]; then
@@ -7,12 +8,12 @@ fi
 
 # Compute next beta number
 echo "::group::Computing next beta number"
-LAST_BETA_NUMBER="$(curl -L "http://search.maven.org/solrsearch/select?q=a:spoon-core+g:fr.inria.gforge.spoon&rows=40&wt=json&core=gav" | jq -r ".response.docs | map(.v) | map((match(\"$CURRENT_VERSION_NO_SNAPSHOT-beta-(.*)\") | .captures[0].string) // \"0\") | .[0]")"
+LAST_BETA_NUMBER="$(curl -L "http://search.maven.org/solrsearch/select?q=a:spoon-core+g:fr.inria.gforge.spoon&rows=40&wt=json&core=gav" | jq -r ".response.docs | map(.v) | map((match(\"$CURRENT_VERSION-beta-(.*)\") | .captures[0].string) // \"0\") | .[0]")"
 echo "LAST_BETA_NUMBER $LAST_BETA_NUMBER"
 
 NEW_BETA_NUMBER=$((LAST_BETA_NUMBER + 1))
 echo "NEW_BETA_NUMBER $NEW_BETA_NUMBER"
-NEXT_VERSION="$CURRENT_VERSION_NO_SNAPSHOT-beta-$NEW_BETA_NUMBER"
+NEXT_VERSION="$CURRENT_VERSION-beta-$NEW_BETA_NUMBER"
 echo "::endgroup::"
 
 echo "::group::Setting release version"
