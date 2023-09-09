30dadf1c2b614d8428a3080619079643a56b4ddf
I-Al-Istannen <I-Al-Istannen@users.noreply.github.com>
2023-09-09 15:19:15 +0200

    fix: Keep implicit status for more implicit enclosing class references (#5425)

--- src/main/java/spoon/support/compiler/jdt/ReferenceBuilder.java
+++ src/main/java/spoon/support/compiler/jdt/ReferenceBuilder.java
@@ -157,7 +157,17 @@ public class ReferenceBuilder {
 					return true;
 				}
 			});
-			if (ref != null) {
+			// Equality ignores implicit state. The `accessedType` correctly models implicit parts, `ref` resolves a
+			// fully qualified name which might be accessed using an alias, e.g.
+			// class Foo { static class Inner {} }
+			// class Bar extends Foo {}
+			// Use: package.Bar.Inner
+			// Here the `accessedType` is `package.Foo.Inner`, but we want `package.Bar.Inner`, which is what
+			// getQualifiedTypeReference computes.
+			// This is still not perfect, as getQualifiedTypeReference can not resolve accesses like `Bar.Inner` within
+			// Bar.
+			if (ref != null && !ref.equals(accessedType)) {
+				JDTTreeBuilderHelper.handleImplicit(type, ref);
 				accessedType = ref;
 			}
 		}
