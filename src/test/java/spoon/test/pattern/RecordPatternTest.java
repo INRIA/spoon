651b68fd9593de1d30a2a0de059cfa417254905a
SirYwell <hannesgreule@outlook.de>
2024-01-24 19:24:35 +0100

    add mixed record/switch pattern test

--- src/test/java/spoon/test/pattern/RecordPatternTest.java
+++ src/test/java/spoon/test/pattern/RecordPatternTest.java
@@ -1,6 +1,7 @@
 package spoon.test.pattern;
 
-import org.checkerframework.checker.units.qual.N;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -8,9 +9,12 @@ import org.junit.jupiter.params.provider.ValueSource;
 import spoon.Launcher;
 import spoon.reflect.CtModel;
 import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.code.CtCase;
+import spoon.reflect.code.CtCasePattern;
 import spoon.reflect.code.CtLocalVariable;
 import spoon.reflect.code.CtPattern;
 import spoon.reflect.code.CtRecordPattern;
+import spoon.reflect.code.CtSwitch;
 import spoon.reflect.code.CtTypePattern;
 import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.support.compiler.VirtualFile;
@@ -19,12 +23,13 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RecordPatternTest {
 
 	private static CtModel createModelFromString(String code) {
-		System.out.println(code);
 		Launcher launcher = new Launcher();
 		launcher.getEnvironment().setComplianceLevel(21);
 		launcher.getEnvironment().setPreviewFeaturesEnabled(true);
@@ -33,15 +38,33 @@ public class RecordPatternTest {
 	}
 
 	private static CtBinaryOperator<?> createFromInstanceOf(String recordDefinitions, String recordPattern) {
-		return createModelFromString("""
-						class Foo {
-							void foo(Object arg) {
-								%s
-								boolean __ = arg instanceof %s;
-							}
-						}
-			""".formatted(recordDefinitions, recordPattern))
+		return createModelFromString(
+			"""
+				class Foo {
+					void foo(Object arg) {
+						%s
+						boolean __ = arg instanceof %s;
+					}
+				}
+				""".formatted(recordDefinitions, recordPattern))
 			.getElements(new TypeFilter<>(CtBinaryOperator.class)).iterator().next();
+	}
+	private static CtSwitch<?> createFromSwitch(String recordDefinitions, String... cases) {
+		for (int i = 0; i < cases.length; i++) {
+			cases[i] = "case " + cases[i] + " -> {}";
+		}
+		return createModelFromString("""
+			class Foo {
+				void foo(Object arg) {
+					%s
+					switch (arg) {
+						%s
+						default -> {}
+					}
+				}
+			}
+			""".formatted(recordDefinitions, String.join("\n", cases)))
+			.getElements(new TypeFilter<>(CtSwitch.class)).iterator().next();
 	}
 
 	@ParameterizedTest
@@ -113,31 +136,6 @@ public class RecordPatternTest {
 				)
 				"""
 		);
-		interface Node {
-			void assertMatches(CtPattern pattern);
-		}
-		record Inner(Node left, Node right) implements Node {
-			@Override
-			public void assertMatches(CtPattern pattern) {
-				CtRecordPattern recordPattern = assertInstanceOf(CtRecordPattern.class, pattern);
-				List<CtPattern> patternList = recordPattern.getPatternList();
-				assertEquals(2, patternList.size());
-				left().assertMatches(patternList.get(0));
-				right().assertMatches(patternList.get(1));
-
-			}
-		}
-		record Leaf(String name) implements Node {
-			@Override
-			public void assertMatches(CtPattern pattern) {
-				CtRecordPattern recordPattern = assertInstanceOf(CtRecordPattern.class, pattern);
-				List<CtPattern> patternList = recordPattern.getPatternList();
-				assertEquals(1, patternList.size());
-				CtTypePattern ctTypePattern = assertInstanceOf(CtTypePattern.class, patternList.get(0));
-				String simpleName = ctTypePattern.getVariable().getSimpleName();
-				assertEquals(name(), simpleName);
-			}
-		}
 		Node pattern = new Inner(
 			new Inner(
 				new Leaf("a"),
@@ -168,5 +166,73 @@ public class RecordPatternTest {
 		assertTrue(variable.isInferred());
 		assertEquals("String", variable.getType().getSimpleName());
 		assertEquals(pattern.contains("final"), variable.isFinal());
+	}
+
+	@Test
+	void testRecordPatternInSwitch() {
+		CtSwitch<?> ctSwitch = createFromSwitch(
+			"""
+				interface Node {}
+				record Leaf(Object value) implements Node {}
+				record Inner(Node left, Node right) implements Node {}
+				""",
+			"Leaf(String s) when s.length() > 10",
+			"Leaf(var s)",
+			"Inner(Leaf leaf, var right)",
+			"Inner(var left, Node right)"
+		);
+		List<CtCase<?>> cases = (List<CtCase<?>>) ctSwitch.getCases();
+		assertEquals(cases.size(), 5); // includes default
+		CtCasePattern c0 = assertInstanceOf(CtCasePattern.class, cases.get(0).getCaseExpression());
+		new Leaf("s").assertMatches(c0.getPattern());
+		assertNotNull(c0.getGuard());
+
+		CtCasePattern c1 = assertInstanceOf(CtCasePattern.class, cases.get(1).getCaseExpression());
+		new Leaf("s").assertMatches(c1.getPattern());
+		assertNull(c1.getGuard());
+
+		CtCasePattern c2 = assertInstanceOf(CtCasePattern.class, cases.get(2).getCaseExpression());
+		new Inner(new SimpleLeaf("leaf"), new SimpleLeaf("right")).assertMatches(c2.getPattern());
+		assertNull(c2.getGuard());
+
+		CtCasePattern c3 = assertInstanceOf(CtCasePattern.class, cases.get(3).getCaseExpression());
+		new Inner(new SimpleLeaf("left"), new SimpleLeaf("right")).assertMatches(c3.getPattern());
+		assertNull(c3.getGuard());
+	}
+
+	interface Node {
+		void assertMatches(CtPattern pattern);
+	}
+	record Inner(Node left, Node right) implements Node {
+		@Override
+		public void assertMatches(CtPattern pattern) {
+			CtRecordPattern recordPattern = assertInstanceOf(CtRecordPattern.class, pattern);
+			List<CtPattern> patternList = recordPattern.getPatternList();
+			assertEquals(2, patternList.size());
+			left().assertMatches(patternList.get(0));
+			right().assertMatches(patternList.get(1));
+
+		}
+	}
+	record Leaf(String name) implements Node {
+		@Override
+		public void assertMatches(CtPattern pattern) {
+			CtRecordPattern recordPattern = assertInstanceOf(CtRecordPattern.class, pattern);
+			List<CtPattern> patternList = recordPattern.getPatternList();
+			assertEquals(1, patternList.size());
+			CtTypePattern ctTypePattern = assertInstanceOf(CtTypePattern.class, patternList.get(0));
+			String simpleName = ctTypePattern.getVariable().getSimpleName();
+			assertEquals(name(), simpleName);
+		}
+	}
+
+	record SimpleLeaf(String name) implements Node {
+
+		@Override
+		public void assertMatches(CtPattern pattern) {
+			CtTypePattern ctTypePattern = assertInstanceOf(CtTypePattern.class, pattern);
+			String simpleName = ctTypePattern.getVariable().getSimpleName();
+			assertEquals(name(), simpleName);
+		}
 	}
 }
