6f7d663538b1b404ddc78cc0c16b60f8823a40e3
Martin Wittlinger <wittlinger.martin@gmail.com>
2024-04-01 19:42:05 +0200

    to tabs

--- src/test/java/spoon/test/receiverparameter/ReceiverParameterTest.java
+++ src/test/java/spoon/test/receiverparameter/ReceiverParameterTest.java
@@ -16,46 +16,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReceiverParameterTest {
 
-    @ModelTest("src/test/resources/receiver/SimpleReceiverParameter.java")
-    void simpleParameter(CtModel model) {
-        // contract: receiver parameter is correctly parsed and can be accessed from the model
-        CtType<?> targetType = model.getAllTypes().iterator().next();
-        List<CtReceiverParameter> receiverParams = targetType.getElements(new TypeFilter<>(CtReceiverParameter.class));
-        assertEquals(1, receiverParams.size());
-        CtReceiverParameter receiverParam = receiverParams.iterator().next();
-        SpoonAssertions.assertThat(receiverParam.getType()).extracting(CtTypeInformation::getQualifiedName).isEqualTo("receiver.SimpleReceiverParameter");
-        SpoonAssertions.assertThat(receiverParam).extracting(CtElement::toString).isEqualTo("SimpleReceiverParameter this");
+	@ModelTest("src/test/resources/receiver/SimpleReceiverParameter.java")
+	void simpleParameter(CtModel model) {
+		// contract: receiver parameter is correctly parsed and can be accessed from the model
+		CtType<?> targetType = model.getAllTypes().iterator().next();
+		List<CtReceiverParameter> receiverParams = targetType.getElements(new TypeFilter<>(CtReceiverParameter.class));
+		assertEquals(1, receiverParams.size());
+		CtReceiverParameter receiverParam = receiverParams.iterator().next();
+		SpoonAssertions.assertThat(receiverParam.getType()).extracting(CtTypeInformation::getQualifiedName).isEqualTo("receiver.SimpleReceiverParameter");
+		SpoonAssertions.assertThat(receiverParam).extracting(CtElement::toString).isEqualTo("SimpleReceiverParameter this");
 
-    }
+	}
 
 
-    @ModelTest(
-            value = "src/test/resources/receiver/InnerClassCtor.java"
-    )
-    void innerClassCtor(CtModel model) {
-        // contract: constructor of inner class can have receiver parameter with their outer class type
-        CtType<?> targetType = model.getAllTypes().iterator().next();
-        List<CtReceiverParameter> receiverParams = targetType.getElements(new TypeFilter<>(CtReceiverParameter.class));
-        assertEquals(1, receiverParams.size());
-        CtReceiverParameter next1 = receiverParams.iterator().next();
-        SpoonAssertions.assertThat(next1.getType()).extracting(CtTypeInformation::getQualifiedName).isEqualTo("receiver.Outer");
-        CtConstructor<?> ctConstructor = targetType.getElements(new TypeFilter<>(CtConstructor.class)).stream().filter(v -> v.getReceiverParameter() != null).findFirst().get();
-        SpoonAssertions.assertThat(ctConstructor).isNotNull();
-        SpoonAssertions.assertThat(ctConstructor.getReceiverParameter()).isNotNull();
-        SpoonAssertions.assertThat(ctConstructor.getReceiverParameter().getType()).isNotNull();
-        SpoonAssertions.assertThat(ctConstructor.getReceiverParameter()).extracting(CtElement::toString).isEqualTo("Outer Outer.this");
-    }
+	@ModelTest(
+			value = "src/test/resources/receiver/InnerClassCtor.java"
+	)
+	void innerClassCtor(CtModel model) {
+		// contract: constructor of inner class can have receiver parameter with their outer class type
+		CtType<?> targetType = model.getAllTypes().iterator().next();
+		List<CtReceiverParameter> receiverParams = targetType.getElements(new TypeFilter<>(CtReceiverParameter.class));
+		assertEquals(1, receiverParams.size());
+		CtReceiverParameter next1 = receiverParams.iterator().next();
+		SpoonAssertions.assertThat(next1.getType()).extracting(CtTypeInformation::getQualifiedName).isEqualTo("receiver.Outer");
+		CtConstructor<?> ctConstructor = targetType.getElements(new TypeFilter<>(CtConstructor.class)).stream().filter(v -> v.getReceiverParameter() != null).findFirst().get();
+		SpoonAssertions.assertThat(ctConstructor).isNotNull();
+		SpoonAssertions.assertThat(ctConstructor.getReceiverParameter()).isNotNull();
+		SpoonAssertions.assertThat(ctConstructor.getReceiverParameter().getType()).isNotNull();
+		SpoonAssertions.assertThat(ctConstructor.getReceiverParameter()).extracting(CtElement::toString).isEqualTo("Outer Outer.this");
+	}
 
-    @ModelTest("src/test/resources/receiver/Outer.java")
-    void innerClassInnerClass(CtModel model) {
-        // contract: constructor of inner class which is an innerclass can have receiver parameter with their outer class type
-        CtType<?> targetType = model.getAllTypes().iterator().next();
-        List<CtReceiverParameter> receiverParams = targetType.getElements(new TypeFilter<>(CtReceiverParameter.class));
-        assertEquals(1, receiverParams.size());
-        CtConstructor<?> ctConstructor = targetType.getElements(new TypeFilter<>(CtConstructor.class)).stream().filter(v -> v.getReceiverParameter() != null).findFirst().get();
-        SpoonAssertions.assertThat(ctConstructor).isNotNull();
-        SpoonAssertions.assertThat(ctConstructor.getReceiverParameter()).isNotNull();
-        SpoonAssertions.assertThat(ctConstructor.getReceiverParameter().getType()).isNotNull();
-        SpoonAssertions.assertThat(ctConstructor.getReceiverParameter()).extracting(CtElement::toString).isEqualTo("Middle Middle.this");
-    }
+	@ModelTest("src/test/resources/receiver/Outer.java")
+	void innerClassInnerClass(CtModel model) {
+		// contract: constructor of inner class which is an innerclass can have receiver parameter with their outer class type
+		CtType<?> targetType = model.getAllTypes().iterator().next();
+		List<CtReceiverParameter> receiverParams = targetType.getElements(new TypeFilter<>(CtReceiverParameter.class));
+		assertEquals(1, receiverParams.size());
+		CtConstructor<?> ctConstructor = targetType.getElements(new TypeFilter<>(CtConstructor.class)).stream().filter(v -> v.getReceiverParameter() != null).findFirst().get();
+		SpoonAssertions.assertThat(ctConstructor).isNotNull();
+		SpoonAssertions.assertThat(ctConstructor.getReceiverParameter()).isNotNull();
+		SpoonAssertions.assertThat(ctConstructor.getReceiverParameter().getType()).isNotNull();
+		SpoonAssertions.assertThat(ctConstructor.getReceiverParameter()).extracting(CtElement::toString).isEqualTo("Middle Middle.this");
+	}
 }
