4c57655cd1c4ac3fd18f8fd335cd682a3c3e7f3e
Mr-Pine <git@mr-pine.de>
2024-04-04 21:14:18 +0200

    Use Launcher in MetaFactory

--- spoon-control-flow/src/test/java/fr/inria/controlflow/SpoonMetaFactory.java
+++ spoon-control-flow/src/test/java/fr/inria/controlflow/SpoonMetaFactory.java
@@ -22,62 +22,18 @@
 
 package fr.inria.controlflow;
 
-//import fr.inria.diversify.util.Log;
-
+import spoon.Launcher;
 import spoon.reflect.factory.Factory;
-import spoon.reflect.factory.FactoryImpl;
-import spoon.support.DefaultCoreFactory;
-import spoon.support.StandardEnvironment;
-import spoon.support.compiler.jdt.JDTBasedSpoonCompiler;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
-
-/**
- * Created by marodrig on 16/06/2014.
- */
 public class SpoonMetaFactory {
 
-	public Factory buildNewFactory(String srcDirectory, int javaVersion) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-		ArrayList<String> a = new ArrayList<String>();
-		a.add(srcDirectory);
-		return buildNewFactory(a, javaVersion);
+	public Factory buildNewFactory(String sourceDirectory,
+	                               int javaVersion) {
+		Launcher launcher = new Launcher();
+		launcher.getEnvironment().setComplianceLevel(javaVersion);
+		launcher.addInputResource(sourceDirectory);
+		launcher.getModelBuilder().build();
+
+		return launcher.getFactory();
 	}
-
-	public Factory buildNewFactory(Collection<String> srcDirectory,
-	                               int javaVersion) throws
-			ClassNotFoundException, IllegalAccessException, InstantiationException {
-		//String srcDirectory = DiversifyProperties.getProperty("project") + "/" + DiversifyProperties.getProperty("src");
-
-		StandardEnvironment env = new StandardEnvironment();
-		env.setComplianceLevel(javaVersion);
-		env.setVerbose(true);
-		env.setDebug(true);
-
-		DefaultCoreFactory f = new DefaultCoreFactory();
-
-		Factory factory = new FactoryImpl(f, env);
-
-		JDTBasedSpoonCompiler compiler = new JDTBasedSpoonCompiler(factory);
-
-		for (String s : srcDirectory) {
-			for (String dir : s.split(System.getProperty("path.separator"))) {
-				//Log.debug("add {} to classpath", dir);
-				File dirFile = new File(dir);
-				if (dirFile.isDirectory()) {
-					compiler.addInputSource(dirFile);
-				}
-			}
-		}
-		try {
-			compiler.build();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-
-		return factory;
-	}
-
-
 }
