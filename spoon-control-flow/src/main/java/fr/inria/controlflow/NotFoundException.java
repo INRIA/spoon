e3f413a43b44ce6b94c1a6346abc72b9c73067f3
Mr-Pine <git@mr-pine.de>
2024-04-05 13:34:43 +0200

    Remove creation date

--- spoon-control-flow/src/main/java/fr/inria/controlflow/NotFoundException.java
+++ spoon-control-flow/src/main/java/fr/inria/controlflow/NotFoundException.java
@@ -21,9 +21,6 @@
  */
 package fr.inria.controlflow;
 
-/**
- * Created by marodrig on 09/11/2015.
- */
 public class NotFoundException extends Exception {
 	public NotFoundException(String found) {
 		super(found);
