8b7f6092e95fb926d959712829f05a6516a7a315
Mr-Pine <git@mr-pine.de>
2024-04-05 02:09:33 +0200

    Messages for `assertTrue`s

--- spoon-control-flow/src/test/java/fr/inria/controlflow/ForwardFlowBuilderVisitorTest.java
+++ spoon-control-flow/src/test/java/fr/inria/controlflow/ForwardFlowBuilderVisitorTest.java
@@ -182,7 +182,7 @@ public class ForwardFlowBuilderVisitorTest {
 		ControlFlowNode entryNode = pathHelper.findNodeByString(graph, "int b = 0");
 		ControlFlowNode caseNode = pathHelper.findNodeByString(graph, "b = 1");
 		boolean canAvoid = pathHelper.canAvoidNode(entryNode, caseNode);
-		assertTrue(canAvoid);
+		assertTrue(canAvoid, "Path for implicit default case missing");
 	}
 
 	@Test
@@ -192,7 +192,7 @@ public class ForwardFlowBuilderVisitorTest {
 		ControlFlowPathHelper pathHelper = new ControlFlowPathHelper();
 		ControlFlowNode startNode = pathHelper.findNodeByString(graph, "int b = 0");
 		List<List<ControlFlowNode>> paths = pathHelper.paths(startNode);
-		assertTrue(paths.size() > 2);
+		assertTrue(paths.size() > 2, "Not enough paths. Possibly missing different paths from multiple expressions for a case");
 	}
 
 	//Test some mixed conditions
