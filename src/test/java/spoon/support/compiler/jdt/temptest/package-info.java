49cbdbaf49ddcd1eeeffa770cc5ff508a4cc9ca3
Rishi Vijayvargiya <rishivijayv@gmail.com>
2025-02-10 11:37:09 +0100

    test: initial playground testing

---
+++ src/test/java/spoon/support/compiler/jdt/temptest/package-info.java
@@ -0,0 +1,4 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "n", elementFormDefault = XmlNsForm.QUALIFIED)
+package spoon.support.compiler.jdt.temptest;
+
+import javax.xml.bind.annotation.XmlNsForm;
