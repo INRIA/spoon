47f966aa866a73aaec78b7905b9e61fad6ffdefc
Rishi Vijayvargiya <rishivijayv@gmail.com>
2025-02-17 15:52:29 +0100

    chore: delete redundant package-info.java in resources

--- src/test/resources/compilation/package-info.java
+++
@@ -1,4 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "n", elementFormDefault = XmlNsForm.QUALIFIED)
-package spoon.support.compiler.jdt.temptest;
-
-import javax.xml.bind.annotation.XmlNsForm;
