c52cc012c8d93270520bf4d1e472771ecaaadb8a
Thomas M. Schöller <7847075+MaxAtoms@users.noreply.github.com>
2025-10-20 13:48:22 +0200

    doc: Remove remaining references to JUnit 4 (#6487)

--- src/test/java/spoon/test/SpoonTestHelpers.java
+++ src/test/java/spoon/test/SpoonTestHelpers.java
@@ -159,11 +159,6 @@ public class SpoonTestHelpers {
 						isMetamodelRelatedType(m.getType());
 	}
 
-	/** Place at the top of a JUnit4 test method to disable it for Windows. */
-	public static void assumeNotWindows() {
-		assumeFalse(System.getProperty("os.name").toLowerCase().contains("windows"));
-	}
-
 	/** Builds a model from code given as string */
 	public static CtModel createModelFromString(String code, int complianceLevel) {
 		Launcher launcher = new Launcher();
