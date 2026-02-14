05ae5ce0519d01f09d6045253125395bdb0c4bf1
Martin Wittlinger <wittlinger.martin@gmail.com>
2026-02-14 15:19:17 +0100

    spotless

--- src/test/java/spoon/testing/assertions/CtImportAssertInterface.java
+++ src/test/java/spoon/testing/assertions/CtImportAssertInterface.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ObjectAssert;
 import spoon.reflect.declaration.CtImport;
 import spoon.reflect.declaration.CtImportKind;
-
 public interface CtImportAssertInterface<A extends AbstractObjectAssert<A, W>, W extends CtImport> extends CtElementAssertInterface<A, W> , SpoonAssert<A, W> {
 	default CtReferenceAssertInterface<?, ?> getReference() {
 		return SpoonAssertions.assertThat(actual().getReference());
@@ -13,5 +12,4 @@ public interface CtImportAssertInterface<A extends AbstractObjectAssert<A, W>, W
 	default ObjectAssert<CtImportKind> getImportKind() {
 		return Assertions.assertThatObject(actual().getImportKind());
 	}
-
 }
