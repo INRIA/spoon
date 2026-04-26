237fa9a26421b74f1321cd4617e2ae1cc0b8c056
Çağlar Eker <ekercaglar@gmail.com>
2026-04-26 15:01:25 +0300

    fix: allow package-info and module-info as valid identifiers (#6708)
    Fixes #6707

---
+++ src/test/resources/noclasspath/package-info-fqn/demo/pudong/package-info.java
@@ -0,0 +1,11 @@
+@javax.xml.bind.annotation.XmlSchema(
+    namespace = "http://www.example.com/schema",
+    xmlns = {
+        @javax.xml.bind.annotation.XmlNs(
+            prefix = "",
+            namespaceURI = "http://www.example.com/schema"
+        )
+    },
+    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED
+)
+package demo.pudong;
